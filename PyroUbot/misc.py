3757459a8a46761077a923440cbfc4108050bcb3
CyberXGithub <96681219+CyberXGithub@users.noreply.github.com>
2023-02-24 11:57:01 +0700

    Create misc.py

---
+++ PyroUbot/misc.py
@@ -0,0 +1,9 @@
+from PyroUbot.config import OWNER_ID
+from PyroUbot.core.database.premium import add_prem, add_seles, get_prem, get_seles
+
+
+async def premium():
+    if OWNER_ID not in await get_seles():
+        await add_seles(OWNER_ID)
+    if OWNER_ID not in await get_prem():
+        await add_prem(OWNER_ID)
