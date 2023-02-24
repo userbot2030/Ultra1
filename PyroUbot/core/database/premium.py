f6a4c8c4b8e4e7e7cee95954eaa5cee1a10288ec
CyberXGithub <96681219+CyberXGithub@users.noreply.github.com>
2023-02-24 12:32:47 +0700

    Create premium.py

---
+++ PyroUbot/core/database/premium.py
@@ -0,0 +1,29 @@
+from PyroUbot.core.database import mongodb 
+
+
+prem = mongodb.premium
+
+
+async def get_prem():
+    sudoers = await sudoersdb.find_one({"sudo": "sudo"})
+    if not sudoers:
+        return []
+    return sudoers["sudoers"]
+
+
+async def add_prem(user_id):
+    sudoers = await get_prem()
+    sudoers.append(user_id)
+    await sudoersdb.update_one(
+        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+    )
+    return True
+
+
+async def remove_prem(user_id):
+    sudoers = await get_prem()
+    sudoers.remove(user_id)
+    await sudoersdb.update_one(
+        {"sudo": "sudo"}, {"$set": {"sudoers": sudoers}}, upsert=True
+    )
+    return True
