2acd9c635ac96c596aaad250b0a50b8d28a2e4e9
CyberXGithub <96681219+CyberXGithub@users.noreply.github.com>
2023-02-25 07:16:32 +0700

    Create notes.py

---
+++ PyroUbot/core/database/notes.py
@@ -0,0 +1,34 @@
+from PytoUbot.core.database import mongo_client
+
+
+collection = mongo_client["PyroUbot"]["notes"]
+
+
+async def get_note(note_name):
+    result = await collection.find_one({"_id": 1})
+    if result is not None:
+        try:
+            note_id = result["notes"][note_name]
+            return note_id
+        except KeyError:
+            return None
+    else:
+        return None
+
+
+async def rm_note(note_name):
+    await collection.update_one({"_id": 1}, {"$unset": {f"notes.{note_name}": ""}})
+
+
+async def all_notes():
+    results = await collection.find_one({"_id": 1})
+    try:
+        notes_dic = results["notes"]
+        key_list = notes_dic.keys()
+        return key_list
+    except:
+        return None
+
+
+async def rm_all():
+    await collection.update_one({"_id": 1}, {"$unset": {"notes": ""}})
