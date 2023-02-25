90f1d1da2043bd39e541932a8060cc9c4ed496f2
CyberXGithub <96681219+CyberXGithub@users.noreply.github.com>
2023-02-25 07:20:07 +0700

    Update __init__.py

--- PyroUbot/core/database/__init__.py
+++ PyroUbot/core/database/__init__.py
@@ -4,6 +4,8 @@ from PyroUbot.config import MONGO_URL
 mongo_client = AsyncIOMotorClient(MONGO_URL)
 mongodb = mongo_client.pyro_ubot
 
+from PyroUbot.core.database.notes import *
 from PyroUbot.core.database.premium import *
 from PyroUbot.core.database.reseller import *
+from PyroUbot.core.database.saved import *
 from PyroUbot.core.database.userbot import *
