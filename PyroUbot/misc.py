b8d99908663748e7c012920b9a36f43afafc71d2
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-03-09 01:06:12 +0700

    Delete misc.py

--- PyroUbot/misc.py
+++
@@ -1,9 +0,0 @@
-from PyroUbot.config import OWNER_ID
-from PyroUbot.core.database import add_prem, add_seles, get_prem, get_seles
-
-
-async def premium():
-    if OWNER_ID not in await get_seles():
-        await add_seles(OWNER_ID)
-    if OWNER_ID not in await get_prem():
-        await add_prem(OWNER_ID)
