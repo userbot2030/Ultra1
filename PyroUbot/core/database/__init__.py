8979e5c0167d3459ec681861ead42e5de469d3e8
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-12 10:11:14 +0700

    Update __init__.py

--- PyroUbot/core/database/__init__.py
+++ PyroUbot/core/database/__init__.py
@@ -5,7 +5,7 @@ from PyroUbot.config import MONGO_URL
 mongo_client = AsyncIOMotorClient(MONGO_URL)
 mongodb = mongo_client.pyro_ubot
 
-from .core.database.notes import *
+from ..core.database.notes import *
 from ..core.database.premium import *
 from ..core.database.reseller import *
 from ..core.database.saved import *
