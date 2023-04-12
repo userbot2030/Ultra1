50272851130404ff90c61a66da8123434cbc33ae
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-12 21:56:33 +0700

    Update notes.py

--- PyroUbot/core/database/notes.py
+++ PyroUbot/core/database/notes.py
@@ -3,8 +3,8 @@ from PytoUbot.core.database import mongo_client
 collection = mongo_client["PyroUbot"]["notes"]
 
 
-async def get_note(note_name):
-    result = await collection.find_one({"_id": 1})
+async def get_note(user_id, note_name):
+    result = await collection.find_one({"_id": user_id})
     if result is not None:
         try:
             note_id = result["notes"][note_name]
@@ -15,12 +15,12 @@ async def get_note(note_name):
         return None
 
 
-async def rm_note(note_name):
-    await collection.update_one({"_id": 1}, {"$unset": {f"notes.{note_name}": ""}})
+async def rm_note(user_id, note_name):
+    await collection.update_one({"_id": user_id}, {"$unset": {f"notes.{note_name}": ""}})
 
 
-async def all_notes():
-    results = await collection.find_one({"_id": 1})
+async def all_notes(user_id):
+    results = await collection.find_one({"_id": user_id})
     try:
         notes_dic = results["notes"]
         key_list = notes_dic.keys()
