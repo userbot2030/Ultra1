61721bae67ce3b9b85d7eadba0dd9c2d5de74a82
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-12 09:55:41 +0700

    Create expired.py

---
+++ PyroUbot/core/function/expired.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from pytz import timezone
+
+from PyroUbot import bot, ubot
+from PyroUbot.config import OWNER_ID
+from PyroUbot.utils import get_expired_date, rem_expired_date, remove_ubot
+
+
+async def premium():
+    time = datetime.now(timezone("Asia/Jakarta")).strftime("%d-%m-%Y")
+    for X in ubot._ubot:
+        try:
+            exp = (await get_expired_date(X.me.id)).strftime("%d-%m-%Y")
+            if time == exp:
+                await rem_expired_date(X.me.id)
+                await remove_ubot(X.me.id)
+                ubot._ubot.remove(X)
+                await bot.send_message(
+                    OWNER_ID,
+                    f"<b>{X.me.first_name} {X.me.last_name or ''} | <code>{X.me.id}</code> berhasil dihapus</b>",
+                )
+        except:
+            pass
