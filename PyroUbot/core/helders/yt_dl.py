a42fd145d88973f166b243fa7c3e18705ab22038
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-15 11:08:58 +0700

    Create yt_dl.py

---
+++ PyroUbot/core/helders/yt_dl.py
@@ -0,0 +1,38 @@
+from yt_dlp import YoutubeDL
+
+from tomimusic.utils import run_sync
+
+
+async def YoutubeDownload(url, as_video=False):
+    if as_video:
+        ydl = YoutubeDL(
+            {
+                "quiet": True,
+                "no_warnings": True,
+                "format": "(bestvideo[height<=?720][width<=?1280][ext=mp4])+(bestaudio[ext=m4a])",
+                "outtmpl": "downloads/%(id)s.%(ext)s",
+                "nocheckcertificate": True,
+                "geo_bypass": True,
+            }
+        )
+    else:
+        ydl = YoutubeDL(
+            {
+                "quiet": True,
+                "no_warnings": True,
+                "format": "bestaudio[ext=m4a]",
+                "outtmpl": "downloads/%(id)s.%(ext)s",
+                "nocheckcertificate": True,
+                "geo_bypass": True,
+            }
+        )
+    ytdl_data = await run_sync(ydl.extract_info, url, download=True)
+    file_name = ydl.prepare_filename(ytdl_data)
+    videoid = ytdl_data["id"]
+    title = ytdl_data["title"]
+    url = f"https://youtu.be/{videoid}"
+    duration = ytdl_data["duration"]
+    channel = ytdl_data["uploader"]
+    views = f"{ytdl_data['view_count']:,}".replace(",", ".")
+    thumb = f"https://img.youtube.com/vi/{videoid}/hqdefault.jpg"
+    return file_name, title, url, duration, views, channel, thumb
