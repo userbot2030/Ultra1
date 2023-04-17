90006c8a07d83a631ca9c037df76b00b8b31cd3b
XtomiX <XtomiX@users.noreply.github.com>
2023-04-17 06:10:50 +0000

    Auto Fix

--- PyroUbot/core/plugins/alv.py
+++ PyroUbot/core/plugins/alv.py
@@ -4,7 +4,11 @@ from time import time
 
 import psutil
 from pyrogram.raw.functions import Ping
-from pyrogram.types import InputTextMessageContent, InlineKeyboardMarkup, InlineQueryResultArticle
+from pyrogram.types import (
+    InputTextMessageContent,
+    InlineKeyboardMarkup,
+    InlineQueryResultArticle,
+)
 
 from .. import *
 
