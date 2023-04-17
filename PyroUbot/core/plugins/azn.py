a0f385cced7a8460020ac0e71b569e0c43fb735f
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-17 09:09:46 +0700

    Create azn.py

---
+++ PyroUbot/core/plugins/azn.py
@@ -0,0 +1,29 @@
+import json
+
+import requests
+
+
+
+async def jadwal_adzan(client, message):
+    LOKASI = message.text.split(None, 1)[1]
+    if len(message.command) < 2:
+        return await message.reply("<b>Silahkan Masukkan Nama Kota Anda</b>")
+    url = f"http://muslimsalat.com/{LOKASI}.json?key=bd099c5825cbedb9aa934e255a81a5fc"
+    request = requests.get(url)
+    if request.status_code != 200:
+        await message.reply(f"<b>Maaf Tidak Menemukan Kota <code>{LOKASI}</code>")
+    result = json.loads(request.text)
+    catresult = f"""
+Jadwal Shalat Hari Ini
+
+<b>Tanggal</b> <code>{result['items'][0]['date_for']}</code>
+<b>Kota</b> <code>{result['query']} | {result['country']}</code>
+
+<b>Terbit:</b> <code>{result['items'][0]['shurooq']}</code>
+<b>Subuh:</b> <code>{result['items'][0]['fajr']}</code>
+<b>Zuhur:</b> <code>{result['items'][0]['dhuhr']}</code>
+<b>Ashar:</b> <code>{result['items'][0]['asr']}</code>
+<b>Maghrib:</b> <code>{result['items'][0]['maghrib']}</code>
+<b>Isya:</b> <code>{result['items'][0]['isha']}</code>
+"""
+    await message.reply(catresult)
