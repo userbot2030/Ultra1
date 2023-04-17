c92efe813d5546a74cf4d930f2751e6ce19e1d7b
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-17 18:41:20 +0700

    Update plugins.py

--- PyroUbot/core/function/plugins.py
+++ PyroUbot/core/function/plugins.py
@@ -15,7 +15,7 @@ HELP_COMMANDS = {}
 async def loadPlugins():
     modules = loadModule()
     for mod in modules:
-        imported_module = import_module(f"tomimusic.modules.{mod}")
+        imported_module = import_module(f"PyroUbot.modules.{mod}")
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             imported_module.__MODULE__ = imported_module.__MODULE__
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
