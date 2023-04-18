bef202f146c39513b91692e201d73c6883efc500
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-18 10:30:50 +0700

    Update cvr.py

--- PyroUbot/core/plugins/cvr.py
+++ PyroUbot/core/plugins/cvr.py
@@ -88,3 +88,123 @@ async def convert_anime(client, message):
             reply_to_message_id=message.id,
         )
         return await client.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+
+    
+    
+
+async def convert_photo(client, message):
+    try:
+        Tm = await message.reply("Downloading . . .")
+        file_io = await dl_pic(client, message.reply_to_message)
+        file_io.name = "sticker.png"
+        await asyncio.gather(
+            Tm.delete(),
+            client.send_photo(
+                message.chat.id,
+                file_io,
+                reply_to_message_id=message.id,
+            ),
+        )
+    except Exception as e:
+        await Tm.delete()
+        return await client.send_message(
+            message.chat.id,
+            f"INFO: {e}",
+            reply_to_message_id=message.id,
+        )
+    
+    
+
+async def convert_sticker(client, message):
+    try:
+        if not message.reply_to_message or not message.reply_to_message.photo:
+            return await message.reply_text("Reply ke foto untuk mengubah ke sticker")
+        sticker = await client.download_media(
+            message.reply_to_message.photo.file_id,
+            f"sticker_{message.from_user.id}.webp",
+        )
+        await message.reply_sticker(sticker)
+        os.remove(sticker)
+    except Exception as e:
+        await message.reply_text(str(e))
+
+
+async def convert_gif(client, message):
+    TM = await message.reply("<b>Memproses...</b>")
+    if not message.reply_to_message.sticker:
+        return await TM.edit("<b>Balas ke Stiker...</b>")
+    await TM.edit("<b>Downloading Sticker. . .</b>")
+    file = await client.download_media(
+        message.reply_to_message,
+        f"Gift_{message.from_user.id}.mp4",
+    )
+    try:
+        await client.send_animation(
+            message.chat.id, file, reply_to_message_id=message.id
+        )
+        os.remove(file)
+        await TM.delete()
+    except Exception as error:
+        await TM.edit(error)
+
+
+
+async def convert_audio(client, message):
+    replied = message.reply_to_message
+    Tm = await message.reply("<b>Tunggu sebentar</b>")
+    if not replied:
+        return await Tm.edit("<b>Mohon Balas Ke Video</b>")
+    if replied.media == MessageMediaType.VIDEO:
+        await Tm.edit("<b>Downloading Video . . .</b>")
+        file = await client.download_media(
+            message=replied,
+            file_name=f"toaudio_{replied.id}",
+        )
+        out_file = f"{file}.mp3"
+        try:
+            await Tm.edit("<b>Mencoba Ekstrak Audio. . .</b>")
+            cmd = f"ffmpeg -i {file} -q:a 0 -map a {out_file}"
+            await run_cmd(cmd)
+            await Tm.edit("<b>Uploading Audio . . .</b>")
+            await client.send_voice(
+                message.chat.id,
+                voice=out_file,
+                reply_to_message_id=message.id,
+            )
+            os.remove(file)
+            await Tm.delete()
+        except Exception as error:
+            await Tm.edit(error)
+    else:
+        return await Tm.edit("<b>Mohon Balas Ke Video</b>")
+
+
+
+async def convert_efek(client, message):
+    helo = get_arg(message)
+    rep = message.reply_to_message
+    if rep and helo:
+        tau = ["bengek", "robot", "jedug", "fast", "echo"]
+        if helo in tau:
+            Tm = await message.reply(f"Merubah suara menjadi {helo}")
+            indir = await client.download_media(rep)
+            KOMUT = {
+                "bengek": '-filter_complex "rubberband=pitch=1.5"',
+                "robot": "-filter_complex \"afftfilt=real='hypot(re,im)*sin(0)':imag='hypot(re,im)*cos(0)':win_size=512:overlap=0.75\"",
+                "jedug": '-filter_complex "acrusher=level_in=8:level_out=18:bits=8:mode=log:aa=1"',
+                "fast": "-filter_complex \"afftfilt=real='hypot(re,im)*cos((random(0)*2-1)*2*3.14)':imag='hypot(re,im)*sin((random(1)*2-1)*2*3.14)':win_size=128:overlap=0.8\"",
+                "echo": '-filter_complex "aecho=0.8:0.9:500|1000:0.2|0.1"',
+            }
+            ses = await asyncio.create_subprocess_shell(
+                f"ffmpeg -i '{indir}' {KOMUT[helo]} audio.mp3"
+            )
+            await ses.communicate()
+            await Tm.delete()
+            await rep.reply_voice("audio.mp3", caption=f"Efek {helo}")
+            os.remove("audio.mp3")
+        else:
+            await message.reply(f"Silahkan isi sesuai {tau}")
+    else:
+        await Tm.edit(
+            f"Silahkan balas ke audio atau mp3, contoh : <code>!efek bengek</code> sambil balas ke audio atau mp3"
+        )
