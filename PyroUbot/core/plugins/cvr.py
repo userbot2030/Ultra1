5a6235bcad29c829ca2a1731860b759fea4a5070
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-18 10:25:18 +0700

    Create cvr.py

---
+++ PyroUbot/core/plugins/cvr.py
@@ -0,0 +1,93 @@
+import asyncio
+import os
+from io import BytesIO
+
+from pyrogram.enums import MessageMediaType, MessagesFilter
+from pyrogram.raw.functions.messages import DeleteHistory
+from pyrogram.types import InputMediaPhoto
+
+from .. import *
+
+
+
+
+async def dl_pic(client, download):
+    path = await client.download_media(download)
+    with open(path, "rb") as f:
+        content = f.read()
+    os.remove(path)
+    get_photo = BytesIO(content)
+    return get_photo
+
+  
+
+async def convert_anime(client, message):
+    Tm = await message.reply("<b>Tunggu sebentar...</b>")
+    if message.reply_to_message:
+        if len(message.command) < 2:
+            if message.reply_to_message.photo:
+                file = "foto"
+                get_photo = message.reply_to_message.photo.file_id
+            elif message.reply_to_message.sticker:
+                file = "sticker"
+                get_photo = await dl_pic(client, message.reply_to_message)
+            elif message.reply_to_message.animation:
+                file = "gift"
+                get_photo = await dl_pic(client, message.reply_to_message)
+            else:
+                return await Tm.edit(
+                    "<b>mohon balas ke</b> <code>photo/striker/git</code>"
+                )
+        else:
+            if message.command[1] in ["foto", "profil", "photo"]:
+                chat = (
+                    message.reply_to_message.from_user
+                    or message.reply_to_message.sender_chat
+                )
+                file = "foto profil"
+                get = await client.get_chat(chat.id)
+                photo = get.photo.big_file_id
+                get_photo = await dl_pic(client, photo)
+    else:
+        if len(message.command) < 2:
+            return await Tm.edit(
+                "Balas ke foto dan saya akan merubah foto anda menjadi anime"
+            )
+        else:
+            try:
+                file = "foto"
+                get = await client.get_chat(message.command[1])
+                photo = get.photo.big_file_id
+                get_photo = await dl_pic(client, photo)
+            except Exception as error:
+                return await Tm.edit(error)
+    await Tm.edit("<b>Sedang diproses...</b>")
+    await client.unblock_user("@qq_neural_anime_bot")
+    send_photo = await client.send_photo("@qq_neural_anime_bot", get_photo)
+    await asyncio.sleep(30)
+    await send_photo.delete()
+    await Tm.delete()
+    info = await client.resolve_peer("@qq_neural_anime_bot")
+    anime_photo = []
+    try:
+        async for anime in client.search_messages(
+            "@qq_neural_anime_bot", filter=MessagesFilter.PHOTO
+        ):
+            anime_photo.append(
+                InputMediaPhoto(
+                    anime.photo.file_id, caption=f"<b>Powered By: {bot.me.mention}</b>"
+                )
+            )
+        await client.send_media_group(
+            message.chat.id,
+            anime_photo,
+            reply_to_message_id=message.id,
+        )
+        return await client.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
+    except Exception:
+        await client.send_message(
+            message.chat.id,
+            f"<b>Gagal merubah {file} menjadi gambar anime</b>",
+            reply_to_message_id=message.id,
+        )
+        return await client.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
