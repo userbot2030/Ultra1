044537d9864e72eaadd7f1e4f68c482748c3aa0c
XtomiX <XtomiX@users.noreply.github.com>
2023-04-22 12:45:52 +0000

    Auto Fix

--- generate.py
+++ generate.py
@@ -10,7 +10,6 @@ print("\n\n Enter Phone number when asked.\n\n")
 i = c(name="pyrogram", api_id=API_ID, api_hash=API_HASH, in_memory=True)
 
 
-
 async def main():
     await i.start()
     ss = await i.export_session_string()
