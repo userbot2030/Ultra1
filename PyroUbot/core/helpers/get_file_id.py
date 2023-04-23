266dbd39c0319f1ffce738ae034c01da0bea21d5
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-24 00:14:55 +0700

    Update get_file_id.py

--- PyroUbot/core/helpers/get_file_id.py
+++ PyroUbot/core/helpers/get_file_id.py
@@ -19,3 +19,36 @@ def get_file_id(msg):
             if obj:
                 setattr(obj, "message_type", message_type)
                 return obj
+
+def qr_gen(content):
+    return {
+        "data": content,
+        "config": {
+            "body": "circle-zebra",
+            "eye": "frame13",
+            "eyeBall": "ball14",
+            "erf1": [],
+            "erf2": [],
+            "erf3": [],
+            "brf1": [],
+            "brf2": [],
+            "brf3": [],
+            "bodyColor": "#000000",
+            "bgColor": "#FFFFFF",
+            "eye1Color": "#000000",
+            "eye2Color": "#000000",
+            "eye3Color": "#000000",
+            "eyeBall1Color": "#000000",
+            "eyeBall2Color": "#000000",
+            "eyeBall3Color": "#000000",
+            "gradientColor1": "",
+            "gradientColor2": "",
+            "gradientType": "linear",
+            "gradientOnEyes": "true",
+            "logo": "",
+            "logoMode": "default",
+        },
+        "size": 1000,
+        "download": "imageUrl",
+        "file": "png",
+    }
