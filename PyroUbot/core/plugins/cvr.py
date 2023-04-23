1f758b7dbdb10cdd35eccdfe8c3f64ba43f4ab22
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-23 19:04:03 +0700

    Update cvr.py

--- PyroUbot/core/plugins/cvr.py
+++ PyroUbot/core/plugins/cvr.py
@@ -66,22 +66,23 @@ async def convert_anime(client, message):
     await Tm.delete()
     info = await client.resolve_peer("@qq_neural_anime_bot")
     anime_photo = []
-    try:
-        async for anime in client.search_messages(
-            "@qq_neural_anime_bot", filter=MessagesFilter.PHOTO
-        ):
-            anime_photo.append(
-                InputMediaPhoto(
-                    anime.photo.file_id, caption=f"<b>Powered By: {bot.me.mention}</b>"
-                )
+    async for anime in client.search_messages(
+        "@qq_neural_anime_bot", filter=MessagesFilter.PHOTO
+    ):
+        anime_photo.append(
+            InputMediaPhoto(
+                anime.photo.file_id, caption=f"<b>Powered By: {bot.me.mention}</b>"
             )
+        )
+    if anime_photo:
         await client.send_media_group(
             message.chat.id,
             anime_photo,
             reply_to_message_id=message.id,
         )
         return await client.invoke(DeleteHistory(peer=info, max_id=0, revoke=True))
-    except Exception:
+
+    else:
         await client.send_message(
             message.chat.id,
             f"<b>Gagal merubah {file} menjadi gambar anime</b>",
