efa56491f51c50a4ba305ed30408b14ada14f912
XtomiX <XtomiX@users.noreply.github.com>
2023-04-25 09:07:43 +0000

    Auto Fix

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -14,6 +14,7 @@ logging.basicConfig(
     handlers=[logging.StreamHandler()],
 )
 
+
 class Bot(Client):
     def __init__(self, **kwargs):
         super().__init__(**kwargs, parse_mode=ParseMode.HTML)
