2376383b8fced92ad7f1ca50d336a83c4956b28f
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-04-26 11:38:16 +0700

    Update unpack.py

--- PyroUbot/core/helpers/unpack.py
+++ PyroUbot/core/helpers/unpack.py
@@ -2,7 +2,14 @@ from base64 import urlsafe_b64decode
 from struct import unpack
 
 from attrify import Attrify as Atr
+from gc import get_objects
 
+from pykeyboard import InlineKeyboard
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineQueryResultArticle,
+    InputTextMessageContent,
+)
 
 def unpackInlineMessage(inline_message_id: str):
     dc_id, message_id, chat_id, query_id = unpack(
@@ -19,3 +26,32 @@ def unpackInlineMessage(inline_message_id: str):
         "inline_message_id": inline_message_id,
     }
     return Atr(temp)
+
+
+
+async def create_button(m):
+    buttons = InlineKeyboard(row_width=2)
+    keyboard = []
+    if "|" not in m.text.split(None, 1)[1]:
+        msg = []
+        for X in m.text.split(None, 1)[1].split():
+            keyboard.append(
+                InlineKeyboardButton(X.split(":", 1)[0], url=X.split(":", 1)[1])
+            )
+            msg.append(X.split(":")[0])
+        buttons.add(*keyboard)
+        if m.reply_to_message:
+            text_or_rep = m.reply_to_message.text
+        else:
+            msg_text = ""
+            for Z in msg:
+                msg_text += f"{Z} "
+            text_or_rep = msg_text
+    else:
+        for X in m.text.split("|", 1)[1].split():
+            keyboard.append(
+                InlineKeyboardButton(X.split(":", 1)[0], url=X.split(":", 1)[1])
+            )
+        buttons.add(*keyboard)
+        text_or_rep = m.text.split("|", 1)[0].split(None, 1)[1]
+    return buttons, text_or_rep
