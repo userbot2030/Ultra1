6a7adc8ea63b0b402893baec15f25e8574e04cc0
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-01 21:22:42 +0700

    Update ocr.py

--- PyroUbot/core/plugins/ocr.py
+++ PyroUbot/core/plugins/ocr.py
@@ -9,8 +9,8 @@ from .. import *
 async def read_cmd(client, message):
     reply = message.reply_to_message
     if not reply or not reply.photo and not reply.sticker and not reply.animation:
-        return await message.reply_text(f"{message.text} reply media")
-    msg = await message.reply("Membaca pesan media....")
+        return await message.reply_text(f"{message.text} ʀᴇᴘʟʏ ᴍᴇᴅɪᴀ")
+    msg = await message.reply("ᴍᴇᴍʙᴀᴄᴀ ᴘᴇsᴀɴ ᴍᴇᴅɪᴀ....")
     try:
         file_path = await dl_pic(client, reply)
         response = upload_file(file_path)
@@ -18,7 +18,7 @@ async def read_cmd(client, message):
         req = requests.get(
             f"https://script.google.com/macros/s/AKfycbwURISN0wjazeJTMHTPAtxkrZTWTpsWIef5kxqVGoXqnrzdLdIQIfLO7jsR5OQ5GO16/exec?url={url}"
         ).json()
-        await msg.edit(f"<b>Hasil OCR:</b>\n<code>{req['text']}</code>")
+        await msg.edit(f"<code>{req['text']}</code>")
         os.remove(file_path)
     except Exception as e:
         await msg.edit(str(e))
