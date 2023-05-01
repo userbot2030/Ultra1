2a173332363d098b44df9e2efc825fc6fa5d7c29
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-01 21:08:15 +0700

    Update lgo.py

--- PyroUbot/core/plugins/lgo.py
+++ PyroUbot/core/plugins/lgo.py
@@ -6,7 +6,7 @@ from .. import *
 
 
 async def logo_cmd(client, message):
-    Tm = await message.reply("<code>Tunggu sebentar...</code>")
+    Tm = await message.reply("<code>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ...</code>")
     await client.unblock_user("@Yone_Robot")
     if message.reply_to_message:
         reply = message.reply_to_message
@@ -23,28 +23,28 @@ async def logo_cmd(client, message):
     else:
         if len(message.command) < 2:
             return await Tm.edit(
-                f"<code>{message.text} [text]</code> - untuk membuat logo"
+                f"<code>{message.text} [ᴛᴇxᴛ]</code> - ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ ʟᴏɢᴏ"
             )
         else:
             Tm_S = await client.send_message(
                 "@Yone_Robot", f"/{message.command[0]} {message.text.split(None, 1)[1]}"
             )
             await Tm_S.delete()
-    await Tm.edit("<code>Sedang diproses...</code>")
+    await Tm.edit("<code>sᴇᴅᴀɴɢ ᴅɪᴘʀᴏsᴇs...</code>")
     await asyncio.sleep(8)
     async for msg in client.get_chat_history("@Yone_Robot"):
         try:
             if msg.photo:
                 await message.reply_photo(
                     msg.photo.file_id,
-                    caption=f"<b>Logo By:</b> <a href=tg://user?id={client.me.id}>{client.me.first_name} {client.me.last_name or ''}</a>",
+                    caption=f"<b>ʟᴏɢᴏ ʙʏ:</b> <a href=tg://user?id={client.me.id}>{client.me.first_name} {client.me.last_name or ''}</a>",
                 )
                 await Tm.delete()
             elif "Error Report" in msg.text:
                 await Tm.edit(
-                    "<b>Gagal membuat logo silahkan ulangi beberapa saat lagi</b>"
+                    "<b>ɢᴀɢᴀʟ ᴍᴇᴍʙᴜᴀᴛ ʟᴏɢᴏ sɪʟᴀʜᴋᴀɴ ᴜʟᴀɴɢɪ ʙᴇʙᴇʀᴀᴘᴀ sᴀᴀᴛ ʟᴀɢɪ</b>"
                 )
         except:
-            await Tm.edit("<code>Terjadi Kesalahan yang tidak diketahui</code>")
+            await Tm.edit("<code>ᴛᴇʀᴊᴀᴅɪ ᴋᴇsᴀʟᴀʜᴀɴ ʏᴀɴɢ ᴛɪᴅᴀᴋ ᴅɪᴋᴇᴛᴀʜᴜɪ</code>")
         user_info = await client.resolve_peer("@Yone_Robot")
         return await client.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
