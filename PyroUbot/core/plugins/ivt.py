333deb378364e9c0e60b0f133189909ff7807840
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-01 21:03:42 +0700

    Update ivt.py

--- PyroUbot/core/plugins/ivt.py
+++ PyroUbot/core/plugins/ivt.py
@@ -6,29 +6,28 @@ from .. import *
 
 
 async def invite_cmd(client, message):
-    mg = await message.reply("<b>Menambahkan Pengguna!</b>")
+    mg = await message.reply("<b>ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴘᴇɴɢɢᴜɴᴀ!</b>")
     if len(message.command) < 2:
         return await mg.delete()
     user_s_to_add = message.text.split(" ", 1)[1]
     if not user_s_to_add:
         await mg.edit(
-            "<b>Beri Saya Pengguna Untuk Ditambahkan! Periksa Menu Bantuan Untuk Info Lebih Lanjut!</b>"
+            "<b>ʙᴇʀɪ sᴀʏᴀ ᴘᴇɴɢɢᴜɴᴀ ᴜɴᴛᴜᴋ ᴅɪᴛᴀᴍʙᴀʜᴋᴀɴ! ᴘᴇʀɪᴋsᴀ ᴍᴇɴᴜ ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ɪɴꜰᴏ ʟᴇʙɪʜ ʟᴀɴᴊᴜᴛ!</b>"
         )
         return
     user_list = user_s_to_add.split(" ")
     try:
         await client.add_chat_members(message.chat.id, user_list, forward_limit=100)
-    except BaseException as e:
-        await mg.edit(f"<b>Tidak Dapat Menambahkan Pengguna!\nTraceBack:</b> {e}")
-        return
-    await mg.edit(f"<b>berhasil ditambahkan {len(user_list)} Ke Grup Ini</b>")
+    except Exception as e:
+        return await mg.edit(f"{e}")
+    await mg.edit(f"<b>ʙᴇʀʜᴀsɪʟ ᴅɪᴛᴀᴍʙᴀʜᴋᴀɴ {len(user_list)} ᴋᴇ ɢʀᴜᴘ ɪɴɪ</b>")
 
 
 invite_id = []
 
 
 async def inviteall_cmd(client, message):
-    Tm = await message.reply("<b>Processing . . .</b>")
+    Tm = await message.reply("<b>ᴘʀᴏᴄᴇssɪɴɢ . . .</b>")
     if len(message.command) < 3:
         await message.delete()
         return await Tm.delete()
@@ -38,13 +37,13 @@ async def inviteall_cmd(client, message):
         return await Tm.edit(error)
     if message.chat.id in invite_id:
         return await Tm.edit_text(
-            f"sedang menginvite member silahkan coba lagi nanti atau gunakan perintah: <code>{PREFIX[0]}cancel</code>"
+            f"sᴇᴅᴀɴɢ ᴍᴇɴɢɪɴᴠɪᴛᴇ ᴍᴇᴍʙᴇʀ sɪʟᴀʜᴋᴀɴ ᴄᴏʙᴀ ʟᴀɢɪ ɴᴀɴᴛɪ ᴀᴛᴀᴜ ɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ: <code>{PREFIX[0]}cancel</code>"
         )
     else:
         done = 0
         failed = 0
         invite_id.append(message.chat.id)
-        await Tm.edit_text(f"mengundang anggota dari {chat.title}")
+        await Tm.edit_text(f"ᴍᴇɴɢᴜɴᴅᴀɴɢ ᴀɴɢɢᴏᴛᴀ ᴅᴀʀɪ {chat.title}")
         async for member in client.get_chat_members(chat.id):
             stats = [
                 UserStatus.ONLINE,
@@ -64,8 +63,8 @@ async def inviteall_cmd(client, message):
         await Tm.delete()
         return await message.reply(
             f"""
-<b>✅ <code>{done}</code> Anggota Yang Berhasil Diundang</b>
-<b>❌ <code>{failed}</code> Anggota Yang Gagal Diundang</b>
+<b>✅ <code>{done}</code> ᴀɴɢɢᴏᴛᴀ ʏᴀɴɢ ʙᴇʀʜᴀsɪʟ ᴅɪᴜɴᴅᴀɴɢ</b>
+<b>❌ <code>{failed}</code> ᴀɴɢɢᴏᴛᴀ ʏᴀɴɢ ɢᴀɢᴀʟ ᴅɪᴜɴᴅᴀɴɢ</b>
 """
         )
 
@@ -73,7 +72,7 @@ async def inviteall_cmd(client, message):
 async def cancel_cmd(client, message):
     if message.chat.id not in invte_id:
         return await message.reply_text(
-            f"sedang tidak ada perintah: <code>{PREFIX[0]}inviteall</code> yang digunakan"
+            f"sᴇᴅᴀɴɢ ᴛɪᴅᴀᴋ ᴀᴅᴀ ᴘᴇʀɪɴᴛᴀʜ: <code>{PREFIX[0]}inviteall</code> ʏᴀɴɢ ᴅɪɢᴜɴᴀᴋᴀɴ"
         )
     try:
         invite_id.remove(message.chat.id)
