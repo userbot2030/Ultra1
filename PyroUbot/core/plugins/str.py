1f142b1cb5ce8d85064d2bfa9b79ed3bd22dec68
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-02 15:59:18 +0700

    Create str.py

---
+++ PyroUbot/core/plugins/str.py
@@ -0,0 +1,94 @@
+import asyncio
+from datetime import datetime
+from gc import get_objects
+from time import time
+
+from pyrogram.raw.functions import Ping
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from .. import *
+
+
+async def send_msg_to_owner(client, message):
+    if message.from_user.id == OWNER_ID:
+        return
+    else:
+        buttons = [
+            [
+                InlineKeyboardButton(
+                    "👤 ᴘʀᴏꜰɪʟ", callback_data=f"profil {message.from_user.id}"
+                ),
+                InlineKeyboardButton(
+                    "ᴊᴀᴡᴀʙ 💬", callback_data=f"jawab_pesan {message.from_user.id}"
+                ),
+            ],
+        ]
+        await client.send_message(
+            OWNER_ID,
+            f"<a href=tg://user?id={message.from_user.id}>{message.from_user.first_name} {message.from_user.last_name or ''}</a>\n\n<code>{message.text}</code>",
+            reply_markup=InlineKeyboardMarkup(buttons),
+        )
+
+
+async def ping_cmd(client, message):
+    uptime = await get_time((time() - start_time))
+    start = datetime.now()
+    await client.invoke(Ping(ping_id=0))
+    end = datetime.now()
+    delta_ping = (end - start).microseconds / 1000
+    _ping = f"""
+<b>❏ ᴘᴏɴɢ:</b> 
+ <b>├• ᴘɪɴɢᴇʀ:</b> <code>{delta_ping} ms</code>
+ <b>├• ᴜᴘᴛɪᴍᴇ:</b> <code>{uptime}</code>
+ <b>└• ᴏᴡɴᴇʀ:</b> <a href=tg://user?id={client.me.id}>{client.me.first_name} {client.me.last_name or ''}</a>
+"""
+    await message.reply(_ping)
+
+
+async def start_cmd(client, message):
+    await send_msg_to_owner(client, message)
+    if len(message.command) < 2:
+        buttons = Button.start()
+        msg = HELP.START(message)
+        await message.reply(msg, reply_markup=InlineKeyboardMarkup(buttons))
+    else:
+        txt = message.text.split(None, 1)[1]
+        msg_id = txt.split("_", 1)[1]
+        send = await message.reply("<b>ᴛᴜɴɢɢᴜ sᴇʙᴇɴᴛᴀʀ...</b>")
+        if "secretMsg" in txt:
+            try:
+                m = [obj for obj in get_objects() if id(obj) == int(msg_id)][0]
+            except Exception as error:
+                return await send.edit(f"<b>❌ ᴇʀʀᴏʀ:</b> <code>{error}</code>")
+            user_or_me = [m.reply_to_message.from_user.id, m.from_user.id]
+            if message.from_user.id not in user_or_me:
+                return await send.edit(
+                    f"<b>❌ ᴘᴇsᴀɴ ɪɴɪ ʙᴜᴋᴀɴ ᴜɴᴛᴜᴋᴍᴜ <a href=tg://user?id={message.from_user.id}>{message.from_user.first_name} {message.from_user.last_name or ''}</a>"
+                )
+            else:
+                text = await client.send_message(
+                    message.chat.id,
+                    m.text.split(None, 1)[1],
+                    protect_content=True,
+                    reply_to_message_id=message.id,
+                )
+                await send.delete()
+                await asyncio.sleep(120)
+                await message.delete()
+                await text.delete()
+        elif "copyMsg" in txt:
+            try:
+                m = [obj for obj in get_objects() if id(obj) == int(msg_id)][0]
+            except Exception as error:
+                return await send.edit(f"<b>❌ ᴇʀʀᴏʀ:</b> <code>{error}</code>")
+            id_copy = int(m.text.split()[1].split("/")[-1])
+            if "t.me/c/" in m.text.split()[1]:
+                chat = int("-100" + str(m.text.split()[1].split("/")[-2]))
+            else:
+                chat = str(m.text.split()[1].split("/")[-2])
+            try:
+                get = await client.get_messages(chat, id_copy)
+                await get.copy(message.chat.id, reply_to_message_id=message.id)
+                await send.delete()
+            except Exception as error:
+                await send.edit(error)
