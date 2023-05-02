91be772e111ea8c8c87495c6ab85b1faedb1d10b
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-02 09:37:36 +0700

    Create srh.py

---
+++ PyroUbot/core/plugins/srh.py
@@ -0,0 +1,55 @@
+import random
+
+from pyrogram.types import InputMediaPhoto
+
+from .. import *
+
+
+async def pic_bing_cmd(client, message):
+    TM = await message.reply("<b>ᴍᴇᴍᴘʀᴏsᴇs...</b>")
+    if len(message.command) < 2:
+        return await TM.edit(f"<code>{message.text}</code> [ǫᴜᴇʀʏ]")
+    x = await client.get_inline_bot_results(
+        message.command[0], message.text.split(None, 1)[1]
+    )
+    get_media = []
+    for X in range(2):
+        try:
+            saved = await client.send_inline_bot_result(
+                client.me.id, x.query_id, x.results[random.randrange(len(x.results))].id
+            )
+            saved = await client.get_messages(
+                client.me.id, int(saved.updates[1].message.id)
+            )
+            get_media.append(InputMediaPhoto(saved.photo.file_id))
+            await saved.delete()
+        except:
+            await TM.edit(f"<b>❌ ɪᴍᴀɢᴇ ᴘʜᴏᴛᴏ ᴋᴇ {X} ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
+    await client.send_media_group(
+        message.chat.id,
+        get_media,
+        reply_to_message_id=message.id,
+    )
+    await TM.delete()
+
+
+async def gif_cmd(client, message):
+    if len(message.command) < 2:
+        return await message.reply(f"<code>{message.text}</code> [ǫᴜᴇʀʏ]")
+    TM = await message.reply("<b>ᴍᴇᴍᴘʀᴏsᴇs...</b>")
+    x = await client.get_inline_bot_results(
+        message.command[0], message.text.split(None, 1)[1]
+    )
+    try:
+        saved = await client.send_inline_bot_result(
+            client.me.id, x.query_id, x.results[random.randrange(len(x.results))].id
+        )
+    except:
+        await message.reply("<b>❌ ɢɪꜰ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
+        await TM.delete()
+    saved = await client.get_messages(client.me.id, int(saved.updates[1].message.id))
+    await client.send_animation(
+        message.chat.id, saved.animation.file_id, reply_to_message_id=message.id
+    )
+    await TM.delete()
+    await saved.delete()
