1aaf9f3593fe2096a3aab98d49438e02fd504cea
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-02 10:08:54 +0700

    Update sid.py

--- PyroUbot/core/plugins/sid.py
+++ PyroUbot/core/plugins/sid.py
@@ -13,16 +13,16 @@ async def id_cmd(client, message):
             )
         elif chat_type == ChatType.CHANNEL:
             await message.reply(
-                f"<b>ID {message.sender_chat.title} Adalah:</b> <code>{message.sender_chat.id}</code>",
+                f"<b>ɪᴅ {message.sender_chat.title} ᴀᴅᴀʟᴀʜ:</b> <code>{message.sender_chat.id}</code>",
             )
         elif chat_type in [ChatType.GROUP, ChatType.SUPERGROUP]:
             _id = ""
-            _id += f"<b>ɪᴅ {message.from_user.first_name} {message.from_user.last_name or ''} ᴀᴅᴀʟᴀʜ:</b> <code>{message.from_user.id}</code>\n<b>ID {message.chat.title} Adalah:</b> <code>{message.chat.id}</code>\n"
+            _id += f"\n\n<b>ɪᴅ {message.from_user.first_name} {message.from_user.last_name or ''} ᴀᴅᴀʟᴀʜ:</b> <code>{message.from_user.id}</code>\n\n<b>ɪᴅ {message.chat.title} ᴀᴅᴀʟᴀʜ:</b> <code>{message.chat.id}</code>\n"
             if message.reply_to_message:
-                _id += f"\n<b>ID {message.reply_to_message.from_user.first_name} {message.reply_to_message.from_user.last_name or ''} Adalah:</b> <code>{message.reply_to_message.from_user.id}</code>\n"
+                _id += f"\n\n<b>ɪᴅ {message.reply_to_message.from_user.first_name} {message.reply_to_message.from_user.last_name or ''} ᴀᴅᴀʟᴀʜ:</b> <code>{message.reply_to_message.from_user.id}</code>\n"
                 file_info = get_file_id(message.reply_to_message)
                 if file_info:
-                    _id += f"<b>ɪᴅ {file_info.message_type} ᴀᴅᴀʟᴀʜ:</b> <code>{file_info.file_id}</code>\n"
+                    _id += f"\n<b>ɪᴅ {file_info.message_type} ᴀᴅᴀʟᴀʜ:</b> <code>{file_info.file_id}</code>\n"
             m = message.reply_to_message or message
             return await m.reply_text(_id)
     try:
