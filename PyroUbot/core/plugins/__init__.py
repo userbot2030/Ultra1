5af80a0ad8f9880a55a01fc8e0a57c94eed624a0
XtomiX <XtomiX@users.noreply.github.com>
2023-05-02 02:24:19 +0000

    Auto Fix

--- PyroUbot/core/plugins/__init__.py
+++ PyroUbot/core/plugins/__init__.py
@@ -20,6 +20,5 @@ from ..core.plugins.opi import *
 from ..core.plugins.pgr import *
 from ..core.plugins.pyn import *
 from ..core.plugins.qrq import *
-from ..core.plugins.smt import *
 from ..core.plugins.rts import *
-
+from ..core.plugins.smt import *
