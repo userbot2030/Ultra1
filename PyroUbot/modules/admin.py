4d6431c23279c0fe0266b71757384400573491ce
XtomiX <XtomiX@users.noreply.github.com>
2023-05-03 03:15:16 +0000

    Auto Fix

--- PyroUbot/modules/admin.py
+++ PyroUbot/modules/admin.py
@@ -1,11 +1,9 @@
 from .. import *
 
 
-
 @PY.UBOT(["kick", "ban", "mute", "unmute", "unban"], F=FILTERS.ME_GROUP)
 async def _(client, message):
     await admin_bannen(client, message)
-
 
 
 @PY.UBOT(["gban", "ungban"])
