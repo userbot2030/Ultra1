1e6ee16ea567b91ef42be2ed6cb862957323dd7a
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-03 10:28:04 +0700

    Create adzan.py

---
+++ PyroUbot/modules/adzan.py
@@ -0,0 +1,15 @@
+
+from .. import *
+
+__MODULE__ = "ADZAN"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴀᴅᴢᴀɴ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:<b/> <code>{PREFIX[0]}adzan</code> [ɴᴀᴍᴀ ᴋᴏᴛᴀ]y
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴇᴛᴀʜᴜɪ ᴊᴀᴅᴡᴀʟ ᴀᴅᴢᴀɴ ᴅɪ ʟᴏᴋᴀsɪ ᴀɴᴅᴀ
+"""
+
+
+@PY.UBOT("adzan")
+async def _(client, message):
+    await jadwal_adzan(client, message)
