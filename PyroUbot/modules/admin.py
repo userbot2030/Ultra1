c6211be71f12e6cb6fa19790e0e5cd947cd6abcc
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-03 10:16:58 +0700

    Update admin.py

--- PyroUbot/modules/admin.py
+++ PyroUbot/modules/admin.py
@@ -1,6 +1,13 @@
 from .. import *
 
 
+__MODULE__ = "ᴀᴅᴍɪɴ"
+__HELP__ = f"""
+• <code>{PREFIX[0]}help global</code>
+• <code>{PREFIX[0]}help restrict</code>
+"""
+
+
 @PY.UBOT(["kick", "ban", "mute", "unmute", "unban"], F=FILTERS.ME_GROUP)
 async def _(client, message):
     await admin_bannen(client, message)
