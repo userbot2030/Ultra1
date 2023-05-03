74a5610c6d8478144ddca63d7a04d70e0b9f6d52
XtomiX <XtomiX@users.noreply.github.com>
2023-05-03 12:45:52 +0000

    Auto Fix

--- PyroUbot/modules/copy.py
+++ PyroUbot/modules/copy.py
@@ -12,24 +12,20 @@ __HELP__ = f"""
 @PY.BOT("copy", ONLY_UBOT)
 async def _(client, message):
     await copy_bot_msg(client, message)
-    
+
 
 @PY.UBOT("copy")
 async def _(client, message):
     await copy_ubot_msg(client, message)
-    
+
 
 @PY.INLINE("^get_msg")
 @INLINE.QUERY
 async def _(client, inline_query):
     await copy_copy_inline_msginline_msg(client, inline_query)
-  
+
 
 @PY.CALLBACK("^copymsg")
 @INLINE.DATA
 async def _(client, callback_query):
     await copy_callback_msg(client, callback_query)
-  
-  
-  
-  
