f1e9afa893f9747c848bb040cd0a43247fef03c3
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-03 11:47:13 +0700

    Update button.py

--- PyroUbot/modules/button.py
+++ PyroUbot/modules/button.py
@@ -1,6 +1,15 @@
 from .. import *
 
 
+__MODULE__ = "ʙᴜᴛᴛᴏɴ"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ʙᴜᴛᴛᴏɴ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}button</code> ᴛᴇxᴛ | /ʀᴇᴘʟʏ ʙᴜᴛᴛᴏɴ_ᴛᴇxᴛ:ʙᴜᴛᴛᴏɴ_ʟɪɴᴋ
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ ᴛᴏᴍʙᴏʟ ɪɴʟɪɴᴇ - ᴍᴀxɪᴍᴀʟ 100 ʙᴜᴛᴛᴏɴ
+"""
+
+
 @PY.UBOT("button")
 async def _(client, message):
     await cmd_button(client, message)
