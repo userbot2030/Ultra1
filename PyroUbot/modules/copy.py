3d01cfe8f9c15a23c1d7951453726b8ae5b51125
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-03 19:45:25 +0700

    Create copy.py

---
+++ PyroUbot/modules/copy.py
@@ -0,0 +1,35 @@
+from .. import *
+
+__MODULE__ = "ᴄᴏᴘʏ"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴄᴏᴘʏ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}copy</code> [ʟɪɴᴋ_ᴋᴏɴᴛᴇɴ_ᴛᴇʟᴇɢʀᴀᴍ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴀᴍʙɪʟ ᴘᴇsᴀɴ ᴛᴇʟᴇɢʀᴀᴍ ᴍᴇʟᴀʟᴜɪ ʟɪɴᴋ ᴍᴇʀᴇᴋᴀ
+  """
+
+
+@PY.BOT("copy", ONLY_UBOT)
+async def _(client, message):
+    await copy_bot_msg(client, message)
+    
+
+@PY.UBOT("copy")
+async def _(client, message):
+    await copy_ubot_msg(client, message)
+    
+
+@PY.INLINE("^get_msg")
+@INLINE.QUERY
+async def _(client, inline_query):
+    await copy_copy_inline_msginline_msg(client, inline_query)
+  
+
+@PY.CALLBACK("^copymsg")
+@INLINE.DATA
+async def _(client, callback_query):
+    await copy_callback_msg(client, callback_query)
+  
+  
+  
+  
