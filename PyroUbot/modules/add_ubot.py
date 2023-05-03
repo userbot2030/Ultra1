4898ef1dc4a539fd2644b978f4984dd6bfef4a85
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-03 10:07:09 +0700

    Create add_ubot.py

---
+++ PyroUbot/modules/add_ubot.py
@@ -0,0 +1,20 @@
+from .. import *
+
+
+
+@PY.CALLBACK("bahan")
+async def _(client, callback_query):
+    awaut need_api(client, callback_query)
+
+
+@PY.CALLBACK("add_ubot")
+async def _(client, callback_query):
+    await bikin_ubot(client, callback_query)
+
+@PY.BOT("getubot")
+async def _(client, message):
+    await cek_ubot(client, message)
+
+@PY.CALLBACK("del_ubot")
+async def _(client, callback_query):
+    await hapus_ubot(client, callback_query)
