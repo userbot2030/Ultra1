ed5ea0b7ab8910f8b4ae2e655c37365edbfce4c4
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-04 06:19:49 +0700

    Create dbcontrol.py

---
+++ PyroUbot/modules/dbcontrol.py
@@ -0,0 +1,63 @@
+from .. import *
+
+
+@PY.BOT("prem")
+@PY.UBOT("prem")
+async def _(client, message):
+    await prem_user(client, message)
+
+
+@PY.BOT("unprem", FILTERS.OWNER)
+@PY.UBOT("unprem", FILTERS.ME_OWNER)
+async def _(client, message):
+    await unprem_user(client, message)
+
+
+@PY.UBOT("getprem", FILTERS.OWNER)
+@PY.UBOT("getprem", FILTERS.ME_OWNER)
+async def _(cliebt, message):
+    await get_prem_user(client, message)
+
+
+@PY.BOT("seles", FILTERS.OWNER)
+@PY.UBOT("seles", FILTERS.ME_OWNER)
+async def _(client, message):
+    await seles_user(client, message)
+
+
+@PY.BOT("unseles",, FILTERS.OWNER)
+@PY.uBOT("unseles", PREFIX, FILTERS.ME_OWNER)
+async def _(client, message):
+    await unseles_user(client, message)
+
+
+@PY.BOT("getseles", FILTERS.OWNER)
+@PY.UBOT("getseles", FILTERS.ME_OWNER)
+async def _(client, message):
+    await get_seles_user(cliebt, message)
+
+
+@PY.BOT("getseles", FILTERS.OWNER)
+@PY.UBOT("getseles", FILTERS.ME_OWNER)
+async def _(client, message):
+
+
+@PY.BOT("getseles", FILTERS.OWNER)
+@PY.UBOT("getseles", FILTERS.ME_OWNER)
+async def _(client, message):
+@PY.BOT("getseles", FILTERS.OWNER)
+@PY.UBOT("getseles", FILTERS.ME_OWNER)
+async def _(client, message):
+@PY.BOT("time", FILTERS.OWNER)
+@PY.UBOT("time", FILTERS.ME_OWNER)
+async def _(client, message):
+    await expired_add(client, message)
+
+
+@PY.BOT("cek", FILTERS.OWNER)
+@PY.UBOT("time", FILTERS.ME_OWNER)
+async def _(client, message):
+    await expired_cek(client, message)
+
+
+
