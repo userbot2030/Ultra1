cd82dc36cb98651626d7a7e7728f0a3cc7358282
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-03 11:14:15 +0700

    Create asupan.py

---
+++ PyroUbot/modules/asupan.py
@@ -0,0 +1,48 @@
+
+from .. import *
+
+__MODULE__ = "ᴀsᴜᴘᴀɴ"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴀsᴜᴘᴀɴ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}asupan</code>
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴠɪᴅᴇᴏ ᴀsᴜᴘᴀɴ ʀᴀɴᴅᴏᴍ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}ayang</code>
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘʜᴏᴛᴏ ᴄᴇᴡᴇᴋ ʀᴀɴᴅᴏᴍ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}ayang2</code>
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘʜᴏᴛᴏ ᴄᴏᴡᴏᴋ ʀᴀɴᴅᴏᴍ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}anime</code>
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘʜᴏᴛᴏ ᴀɴɪᴍᴇ ʀᴀɴᴅᴏᴍ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}bokep</code>
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴠɪᴅᴇᴏ ʙᴏᴋᴇᴘ ʀᴀɴᴅᴏᴍ
+"""
+
+
+@PY.UBOT("asupan")
+async def _(client, message):
+    await video_asupan(client, message)
+
+
+@PY.UBOT("ayang")
+async def _(client, message):
+    await photo_cewek(client, message)
+
+
+@PY.UBOT("ayang3")
+async def _(client, message):
+    await photo_cowok(client, message)
+
+
+@PY.UBOT("anime")
+async def _(client, message):
+    await photo_anime(client, message)
+
+
+
+@PY.UBOT("bokep")
+async def _(client, message):
+    await video_bokep(client, message)
