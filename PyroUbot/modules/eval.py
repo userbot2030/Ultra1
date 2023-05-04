161f95c6a4a866bc391a9db3c0223731581ce5a5
XtomiX <XtomiX@users.noreply.github.com>
2023-05-04 01:02:34 +0000

    Auto Fix

--- PyroUbot/modules/eval.py
+++ PyroUbot/modules/eval.py
@@ -5,10 +5,8 @@ from .. import *
 @PY.UBOT("sh", FILTERS.ME_OWNER)
 async def _(client, message):
     await shell_cmd(client, message)
-  
-  
-  
-  
+
+
 @PY.BOT("trash", FILTERS.OWNER)
 @PY.UBOT("trash", PREFIX)
 async def _(client, message):
