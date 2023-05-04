8a02b5e1fd09d3d6fd4ace6f6b3a93d91c8a5cf1
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-04 12:35:26 +0700

    Create gcast.py

---
+++ PyroUbot/modules/gcast.py
@@ -0,0 +1,35 @@
+from .. import *
+
+__MODULE__ = "ɢᴄᴀsᴛ"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ɢᴄᴀsᴛ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}ucast</code> [ᴛᴇxᴛ/ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ/ᴍᴇᴅɪᴀ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b>  Untuk mengirim pesan ke semua user 
+
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}gcast</code> [ᴛᴇxᴛ/ʀᴇᴘʟʏ ᴛᴏ ᴛᴇxᴛ/ᴍᴇᴅɪᴀ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘᴇsᴀɴ ᴋᴇ sᴇᴍᴜᴀ ɢʀᴏᴜᴘ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}send</code> [ᴜsᴇʀɪᴅ/ᴜsᴇʀɴᴀᴍᴇ - ᴛᴇxᴛ/ʀᴇᴘʟʏ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘᴇsᴀɴ ᴋᴇ ᴜsᴇʀ/ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ            
+"""
+
+
+@PY.UBOT("gcast")
+async def _(client, message):
+    await broadcast_group_cmd(client, message)
+  
+
+@PY.UBOT("ucast")
+async def _(client, message):
+    await broadcast_users_cmd(client, message)
+  
+@PY.UBOT("send")
+async def _(client, message):
+    await send_msg_cmd(client, message)
+    
+    
+    
+    
+  
