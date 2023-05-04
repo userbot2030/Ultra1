05192944fd75bd8c11efe1e866fa0fdb0a6418c0
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-04 08:02:08 +0700

    Create eval.py

---
+++ PyroUbot/modules/eval.py
@@ -0,0 +1,15 @@
+from .. import *
+
+
+@PY.BOT("sh", FILTERS.OWNER)
+@PY.UBOT("sh", FILTERS.ME_OWNER)
+async def _(client, message):
+    await shell_cmd(client, message)
+  
+  
+  
+  
+@PY.BOT("trash", FILTERS.OWNER)
+@PY.UBOT("trash", PREFIX)
+async def _(client, message):
+    await evalator_cmd(client, message)
