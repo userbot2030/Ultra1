cf5dc25ba8d36ad3d29214934ec622ef2bc9d81a
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-04 18:26:05 +0700

    Create image.py

---
+++ PyroUbot/modules/image.py
@@ -0,0 +1,40 @@
+from .. import *
+
+__MODULE__ = "ɪᴍᴀɢᴇ"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ɪᴍᴀɢᴇ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}rbg</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢʜᴀᴘᴜs ʟᴀᴛᴀʀ ʙᴇʟᴀᴋᴀɴɢ ɢᴀᴍʙᴀʀ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}blur</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴇʀɪᴋᴀ ᴇꜰᴇᴋ ʙʟᴜʀ ᴋᴇ ɢᴀᴍʙᴀʀ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}miror</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴇʀɪᴋᴀɴ ᴇꜰᴇᴋ ᴄᴇʀᴍɪɴ ᴋᴇ ɢᴀᴍʙᴀʀ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}negative</code> [ʀᴇᴘʟʏ ᴛᴏ ᴘʜᴏᴛᴏ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴇʀɪᴋᴀɴ ᴇꜰᴇᴋ ɴᴇɢᴀᴛɪᴠᴇ ᴋᴇ ɢᴀᴍʙᴀʀ
+"""
+
+
+@PY.UBOT("rbg")
+async def _(client, message):
+    await rbg_cmd(client, message)
+
+
+@PY.UBOT("blur")
+async def _(client, message):
+    await blur_cmd(client, message)
+
+
+@PY.UBOT("negative")
+async def _(client, message):
+    await negative_cmd(client, message)
+
+
+@PY.UBOT("miror")
+async def _(client, message):
+    await miror_cmd(client, message)
+
+
