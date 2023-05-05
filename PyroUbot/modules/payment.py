76ed360156068d513985384848fc7ad2a5cbfbc1
XtomiX <XtomiX@users.noreply.github.com>
2023-05-05 03:26:25 +0000

    Auto Fix

--- PyroUbot/modules/payment.py
+++ PyroUbot/modules/payment.py
@@ -9,21 +9,3 @@ async def _(client, callback_query):
 @PY.CALLBACK("^(success|failed|home)")
 async def _(client, callback_query):
     await success_failed_home_callback(client, callback_query)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
