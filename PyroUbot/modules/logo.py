f7983fcfc01e7be27e8018b2692604730665301d
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-05 09:21:21 +0700

    Create logo.py

---
+++ PyroUbot/modules/logo.py
@@ -0,0 +1,17 @@
+from .. import *
+
+__MODULE__ = "ʟᴏɢᴏ"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ʟᴏɢᴏ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}logo</code> [ᴛᴇxᴛ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ sᴇʙᴜᴀʜ ʟᴏɢᴏ ᴅᴇɴɢᴀɴ ʙᴀᴄᴋɢʀᴏᴜɴᴅ ʀᴀɴᴅᴏᴍ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}blogo</code> [ᴛᴇxᴛ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ sᴇʙᴜᴀʜ ʟᴏɢᴏ ᴅᴇɴɢᴀɴ ʙᴀᴄᴋɢʀᴏᴜɴᴅ ʜɪᴛᴀᴍ  
+"""
+
+
+@PY.UBOT(["blogo", "logo"])
+async def _(client, message):
+    await logo_cmd(client, message)
