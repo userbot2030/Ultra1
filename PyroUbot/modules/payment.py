19cf3981c48013dc95293a5f672a6898fe948ddc
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-05 10:26:06 +0700

    Create payment.py

---
+++ PyroUbot/modules/payment.py
@@ -0,0 +1,29 @@
+from .. import *
+
+
+@PY.CALLBACK("^confirm")
+async def _(client, callback_query):
+    await confirm_callback(client, callback_query)
+
+
+@PY.CALLBACK("^(success|failed|home)")
+async def _(client, callback_query):
+    await success_failed_home_callback(client, callback_query)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
