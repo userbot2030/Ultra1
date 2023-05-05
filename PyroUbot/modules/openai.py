648a23f045f28bd0a291d08e810aacaf379ff37b
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-05 10:18:32 +0700

    Create openai.py

---
+++ PyroUbot/modules/openai.py
@@ -0,0 +1,29 @@
+from .. import *
+
+__MODULE__ = "ᴏᴘᴇɴᴀɪ"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴏᴘᴇɴᴀɪ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}ai</code> ᴏʀ <code>{PREFIX[0]}ask</code>  [ǫᴜᴇʀʏ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴀᴊᴜᴋᴀɴ ᴘᴇʀᴛᴀɴʏᴀᴀɴ ᴋᴇ ᴄʜᴀᴛɢᴘᴛ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}dalle</code> ᴏʀ <code>{PREFIX[0]}photo</code> [ǫᴜᴇʀʏ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> Untuk membuat sebuah photo 
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}stt</code> [ʀᴇᴘʟʏ ᴠᴏɪᴄᴇ ɴᴏᴛᴇ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b>ᴜɴᴛᴜᴋ ᴍᴇʀᴜʙᴀʜ ᴘᴇsᴀɴ sᴜᴀʀᴀ ᴋᴇ ᴛᴇxᴛ
+"""
+
+
+@PY.UBOT(["ai", "ask"])
+async def _(client, message):
+    await ai_cmd(client, message)
+  
+@PY.UBOT(["dalle", "photo"])
+async def _(client, message):
+    await dalle_cmd(client, message)
+
+@PY.UBOT("stt", PREFIX)
+async def _(client, message):
+    await stt_cmd(client, message)
+  
