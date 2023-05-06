fe097bb88c99cfcb202aaac3ef78eff7353090d8
XtomiX <XtomiX@users.noreply.github.com>
2023-05-06 00:00:12 +0000

    Auto Fix

--- PyroUbot/core/function/expired.py
+++ PyroUbot/core/function/expired.py
@@ -1,5 +1,6 @@
-from datetime import datetime
 import asyncio
+from datetime import datetime
+
 from pytz import timezone
 
 from PyroUbot import bot, get_my_id, ubot
@@ -7,10 +8,14 @@ from PyroUbot.config import LOGS_MAKER_UBOT
 from PyroUbot.core.database import (get_expired_date, rem_expired_date,
                                     remove_ubot, rm_all)
 
+
 def msg_bot(X, time, clock):
     text_expired = f"<b>❏ ᴘᴇᴍʙᴇʀɪᴛᴀʜᴜᴀɴ\n ├ ᴀᴋᴜɴ <a href=tg://user?id={X.me.id}>{X.me.first_name} {X.me.last_name or ''}</a>\n ╰ ɪᴅ <code>{X.me.id}</code>\n\n► ᴍᴀsᴀ ᴀᴋᴛɪꜰ ᴜsᴇʀʙᴏᴛ ᴛᴇʟᴀʜ ʜᴀʙɪs ᴛᴇʀɪᴍᴀᴋᴀsɪʜ sᴜᴅᴀʜ ᴍᴇᴍᴀᴋᴀɪ ᴜsᴇʀʙᴏᴛ @{bot.me.username} ᴊᴀɴɢᴀɴ ʟᴜᴘᴀ ᴏʀᴅᴇʀ ʟᴀɢɪ ʏᴀ</b>"
-    text_date_msg = f"<b>🗓️ ᴛᴀɴɢɢᴀʟ:</b> <code>{time}</code>\n<b>🕕 ᴊᴀᴍ:</b> <code>{clock}</code>"
+    text_date_msg = (
+        f"<b>🗓️ ᴛᴀɴɢɢᴀʟ:</b> <code>{time}</code>\n<b>🕕 ᴊᴀᴍ:</b> <code>{clock}</code>"
+    )
     return [text_expired, text_date_msg]
+
 
 async def expired_userbot():
     while True:
@@ -27,13 +32,8 @@ async def expired_userbot():
                     get_my_id.remove(X.me.id)
                     await remove_ubot(X.me.id)
                     await rem_expired_date(X.me.id)
-                    await bot.send_message(
-                        LOGS_MAKER_UBOT,
-                        msg_bot(X, time, clock)[0])
+                    await bot.send_message(LOGS_MAKER_UBOT, msg_bot(X, time, clock)[0])
             except:
                 pass
         await asyncio.sleep(3600)
-        await bot.send_message(
-            LOGS_MAKER_UBOT,
-            msg_bot(X, time, clock)[1]
-        )
+        await bot.send_message(LOGS_MAKER_UBOT, msg_bot(X, time, clock)[1])
