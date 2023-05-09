927ab4776411b187c774ed8d32ceb79cfe24fd0d
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-09 09:37:09 +0700

    Create secret.py

---
+++ PyroUbot/modules/secret.py
@@ -0,0 +1,21 @@
+from .. import *
+
+__MODULE__ = "sᴇᴄʀᴇᴛ"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ sᴇᴄʀᴇᴛ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}msg</code> [ʀᴇᴘʟʏ ᴛᴏ ᴜsᴇʀ - ᴛᴇxᴛ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢɪʀɪᴍ ᴘᴇsᴀɴ sᴇᴄᴀʀᴀ ʀᴀʜᴀsɪᴀ
+"""
+
+
+@PY.UBOT("msg")
+async def _(client, message):
+    await msg_cmd(client, message)
+
+
+
+@PY.INLINE("^secret")
+@INLINE.QUERY
+async def _(client, inline_query):
+    await secret_inline(client, inline_query)
