9ce3cad7490c824d7671d25e91b224cb9112ecae
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-09 07:51:51 +0700

    Create qrcode.py

---
+++ PyroUbot/modules/qrcode.py
@@ -0,0 +1,28 @@
+from .. import *
+
+
+
+__MODULE__ = "ǫʀᴄᴏᴅᴇ"
+__HELP__ = f"""
+『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ǫʀᴄᴏᴅᴇ 』
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}qrGen</code> [ᴛᴇxᴛ ǫʀᴄᴏᴅᴇ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇʀᴜʙᴀʜ ǫʀᴄᴏᴅᴇ ᴛᴇxᴛ ᴍᴇɴᴊᴀᴅɪ ɢᴀᴍʙᴀʀ
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}qrRead</code> [ʀᴇᴘʟʏ ᴛᴏ ᴍᴇᴅɪᴀ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇʀᴜʙᴀʜ ǫʀᴄᴏᴅᴇ ᴍᴇɴᴊᴀᴅɪ ᴛᴇxᴛ
+"""
+
+
+@PY.UBOT("qrgen")
+async def _(client, message):
+    await qr_gen_cmd(client, message)
+
+    
+    
+@PY.UBOT("qrread")
+async def _(client, message):
+    await qr_read_cmd(client, message)
+
+
+
