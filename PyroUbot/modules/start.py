246593cd0816d739a43068725a25a8742f686e44
XtomiX <XtomiX@users.noreply.github.com>
2023-05-09 03:51:33 +0000

    Auto Fix

--- PyroUbot/modules/start.py
+++ PyroUbot/modules/start.py
@@ -4,9 +4,8 @@ from .. import *
 @PY.UBOT("ping")
 async def _(client, message):
     await ping_cmd(client, message)
-  
-  
-  
+
+
 @PY.BOT("start")
 async def _(client, message):
     await start_cmd(client, message)
