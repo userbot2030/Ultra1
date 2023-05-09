c4efdb8fcbf54cea67940f18bacd2e78ca9e9207
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-09 11:47:52 +0700

    Create memify.py

---
+++ PyroUbot/modules/memify.py
@@ -0,0 +1,15 @@
+from .. import *
+
+__MODULE__ = "ᴍᴇᴍɪꜰʏ"
+__HELP__ = f"""
+<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴍᴇᴍɪꜰʏ 』</b>
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}mmf</code> [ᴛᴇxᴛ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ʙᴀʟᴀs ᴋᴇ sᴛɪᴄᴋᴇʀ ᴀᴛᴀᴜ ꜰᴏᴛᴏ ᴀᴋᴀɴ ᴅɪ ᴜʙᴀʜ ᴍᴇɴᴊᴀᴅɪ sᴛɪᴄᴋᴇʀ ᴛᴇᴋs ᴍᴇᴍᴇ ʏᴀɴɢ ᴅɪ ᴛᴇɴᴛᴜᴋᴀɴ
+"""
+
+
+
+@PY.UBOT(["mmf", "memify"])
+async def _(client, message):
+    await memify_cmd(client, message)
