95d1ab2360701d3e8ca578e9c03777bd45cbf178
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-09 08:02:10 +0700

    Create restart.py

---
+++ PyroUbot/modules/restart.py
@@ -0,0 +1,12 @@
+from .. import *
+
+
+@PY.BOT("login", FILTERS.OWNER)
+@PY.UBOT("login", FILTERS.ME_OWNER)
+async def _(client, message):
+    await login_cmd(client, message)
+
+
+@PY.BOT("restart", ONLY_UBOT)
+async def _(client, message):
+    await restart_cmd(client, message)
