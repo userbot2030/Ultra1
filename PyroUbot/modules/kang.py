5c23fe877546356b16309c62f3aa096ca33f8f5d
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-09 11:39:13 +0700

    Create kang.py

---
+++ PyroUbot/modules/kang.py
@@ -0,0 +1,15 @@
+
+from .. import *
+
+__MODULE__ = "ᴋᴀɴɢ"
+__HELP__ = f"""
+<ʙ>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴋᴀɴɢ 』</b>
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}kang</code> [ʀᴇᴘʟʏ ᴛᴏ ɪᴍᴀɢᴇ/sᴛɪᴄᴋᴇʀ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴᴀᴍʙᴀʜᴋᴀɴ ᴅᴀɴ ᴄᴏsᴛᴜᴍ ᴇᴍᴏᴊɪ sᴛɪᴄᴋᴇʀ ᴋᴇ sᴛɪᴄᴋᴇʀ ᴘᴀᴄᴋ
+"""
+
+
+@PY.UBOT("kang")
+async def _(client, message):
+    await kang_cmd(client, message)
