ba29fcef8dfb9465664d569289db37b184277293
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-09 19:47:05 +0700

    Create nulis.py

---
+++ PyroUbot/modules/nulis.py
@@ -0,0 +1,16 @@
+from .. import *
+
+__MODULE__ = "ɴᴜʟɪs"
+__HELP__ = f"""
+<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ɴᴜʟɪs 』</b>
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}nulis</code> [ʀᴇᴘʟʏ/ᴛᴇxᴛ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ɴᴜʟɪs sᴇsᴜᴀᴛᴜ ᴋᴀʟɪᴍᴀᴛ/ᴋᴀᴛᴀ ᴅɪ ʙᴜᴋᴜ
+"""
+
+
+
+
+@PY.UBOT("nulis")
+async def _(client, message):
+     await nulis_cmd(client, message)
