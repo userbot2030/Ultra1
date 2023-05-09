1e937096f75f1d3822b379b15b0c4317c8b39da3
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-09 11:45:11 +0700

    Create tiny.py

---
+++ PyroUbot/modules/tiny.py
@@ -0,0 +1,15 @@
+from .. import *
+
+__MODULE__ = "ᴛɪɴʏ"
+__HELP__ = f"""
+<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴛɪɴʏ 』</b>
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}tiny</code> [ʀᴇᴘʟʏ ᴛᴏ sᴛɪᴄᴋᴇʀ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇʀᴜʙᴀʜ sᴛɪᴄᴋᴇʀ ᴍᴇɴᴊᴀᴅɪ ᴋᴇᴄɪʟ
+"""
+
+
+
+@PY.UBOT("tiny")
+async def _(client, message):
+    await tiny_cmd(client, message)
