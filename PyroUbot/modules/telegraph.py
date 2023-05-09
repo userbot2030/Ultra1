2bc379e8141866e864a6471193b386898145c599
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-09 12:22:40 +0700

    Create telegraph.py

---
+++ PyroUbot/modules/telegraph.py
@@ -0,0 +1,15 @@
+from .. import *
+
+__MODULE__ = "ᴛᴇʟᴇɢʀᴀᴘʜ"
+__HELP__ = f"""
+<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴛᴇʟᴇɢʀᴀᴘʜ 』</b>
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}tg</code> [ʀᴇᴘʟʏ ᴍᴇᴅɪᴀ/ᴛᴇxᴛ]
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴀᴘʟᴏᴀᴅ ᴍᴇᴅɪᴀ/ᴛᴇxᴛ ᴋᴇ telegra.ph
+"""
+
+
+
+@PY.UBOT("tg")
+async def _(client, message):
+    await tg_cmd(client, message)
