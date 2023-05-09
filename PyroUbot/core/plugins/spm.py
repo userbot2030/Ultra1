201f3d8141bbb746a86b21bb18c988578453be5a
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-09 10:33:39 +0700

    P

--- PyroUbot/core/plugins/spm.py
+++ PyroUbot/core/plugins/spm.py
@@ -21,7 +21,7 @@ async def spam_cmd(client, message):
                 )
                 await asyncio.sleep(0.3)
         else:
-            if len(message.text.split()) < 3:
+            if len(message.command) < 3:
                 await message.reply_text("⚡ ᴜsᴀɢᴇ:\n.sᴘᴀᴍ ᴊᴜᴍʟᴀʜ sᴘᴀᴍ, ᴛᴇxᴛ sᴘᴀᴍ")
             else:
                 spam = await message.reply("Diproses")
@@ -37,8 +37,25 @@ async def spam_cmd(client, message):
                     await client.send_message(message.chat.id, spam_text)
                     await asyncio.sleep(0.3)
     elif message.command[0] == "dspam":
-        if len(message.text.split()) < 4:
-            await message.reply_text(
+        if message.reply_to_message:
+            if len(message.command) < 3:
+                return await message.reply_text(
+                "⚡ ᴜsᴀɢᴇ:\n.ᴅsᴘᴀᴍ ᴊᴜᴍʟᴀʜ sᴘᴀᴍ, ᴊᴜᴍʟᴀʜ ᴅᴇʟᴀʏ ᴅᴇᴛɪᴋ, ʙᴀʟᴀs ᴘᴇsᴀɴ"
+            )
+            spam = await message.reply("ᴅɪᴘʀᴏsᴇs")
+            try:
+                quantity = int(message.text.split(None, 3)[1])
+                delay_msg = int(message.text.split(None, 3)[2])
+            except Exception as error:
+                return await spam.edit(error)
+            await asyncio.sleep(1)
+            await message.delete()
+            await spam.delete()
+            for i in range(quantity):
+                await message.reply_to_message.copy(message.chat.id)
+                await asyncio.sleep(delay_msg)
+        if len(message.command) < 4:
+            return await message.reply_text(
                 "⚡ ᴜsᴀɢᴇ:\n.ᴅsᴘᴀᴍ ᴊᴜᴍʟᴀʜ sᴘᴀᴍ, ᴊᴜᴍʟᴀʜ ᴅᴇʟᴀʏ ᴅᴇᴛɪᴋ, ᴛᴇxᴛ sᴘᴀᴍ"
             )
         else:
