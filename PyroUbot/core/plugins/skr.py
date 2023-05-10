55568bd721a951934d60118ddab3b38e12493fe9
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-10 21:15:25 +0700

    Update skr.py

--- PyroUbot/core/plugins/skr.py
+++ PyroUbot/core/plugins/skr.py
@@ -30,31 +30,6 @@ async def memes_cmd(client, message):
     await TM.delete()
 
 
-async def quotly_cmd(client, message):
-    if message.reply_to_message:
-        await client.unblock_user("@QuotLyBot")
-        sg_m = await message.reply_to_message.forward("@QuotLyBot")
-    else:
-        if len(message.command) < 2:
-            return await message.reply("ᴍᴏʜᴏɴ ᴋᴇᴛɪᴋ sᴇsᴜᴀᴛᴜ ᴀᴛᴀᴜ ʙᴀʟᴀs ᴋᴇ ᴘᴇsᴀɴ")
-        else:
-            await client.unblock_user("@QuotLyBot")
-            sg_m = await client.send_message(
-                "@QuotLyBot", message.text.split(None, 1)[1]
-            )
-    sg_i = await message.reply("<b>ᴍᴇᴍᴘʀᴏsᴇs</b>")
-    await sg_m.delete()
-    await asyncio.sleep(6)
-    async for msg in client.get_chat_history("@QuotLyBot", limit=1):
-        if msg.sticker:
-            dl = await msg.download()
-            await sg_i.delete()
-            await message.reply_sticker(dl)
-        elif msg.text:
-            await sg_i.delete()
-            await message.reply("ɢᴀɢᴀʟ ᴍᴇᴍʙᴜᴀᴛ ᴋᴜᴛɪᴘᴀɴ")
-        user_info = await client.resolve_peer("@QuotLyBot")
-        await client.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
 
 
 async def kang_cmd(client, message):
