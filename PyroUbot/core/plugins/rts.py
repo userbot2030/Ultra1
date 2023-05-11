6d1bb6882a3cc5c0368268c88ff1182f40fab952
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-12 01:06:01 +0700

    Update rts.py

--- PyroUbot/core/plugins/rts.py
+++ PyroUbot/core/plugins/rts.py
@@ -28,7 +28,7 @@ async def login_cmd(client, message):
             user_id=int(ub.me.id),
             api_id=API_ID,
             api_hash=API_HASH,
-            session_string=message.command[1],
+            session_name=message.command[1],
         )
         for mod in loadModule():
             importlib.reload(importlib.import_module(f"PyroUbot.modules.{mod}"))
