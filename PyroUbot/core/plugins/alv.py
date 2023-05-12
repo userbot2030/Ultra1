bda11f07def93284406b8831b1831732cc232361
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-12 12:13:25 +0700

    Update alv.py

--- PyroUbot/core/plugins/alv.py
+++ PyroUbot/core/plugins/alv.py
@@ -38,13 +38,13 @@ async def alive_query(client, inline_query):
             uptime = await get_time((time() - start_time))
             msg = f"""
 <b><a href=tg://user?id={my.me.id}>{my.me.first_name} {my.me.last_name or ''}</a></b>
-    <b>sᴛᴀᴛᴜs:</b> {status} 
-        <b>ᴇxᴘɪʀᴇᴅ_ᴏɴ:</b> <code>{exp}</code> 
-        <b>ᴅᴄ_ɪᴅ:</b> <code>{my.me.dc_id}</code>
-        <b>ᴘɪɴɢ_ᴅᴄ:</b> <code>{ping} ms</code>
-        <b>ᴘᴇᴇʀ_ᴜsᴇʀs:</b> <code>{peer['users']} users</code>
-        <b>ᴘᴇᴇʀ_ɢʀᴏᴜᴘ:</b> <code>{peer['group']} group</code>
-        <b>sᴛᴀʀᴛ_ᴜᴘᴛɪᴍᴇ:</b> <code>{uptime}</code>
+  <b>sᴛᴀᴛᴜs:</b> {status} 
+    <b>ᴇxᴘɪʀᴇᴅ_ᴏɴ:</b> <code>{exp}</code> 
+    <b>ᴅᴄ_ɪᴅ:</b> <code>{my.me.dc_id}</code>
+    <b>ᴘɪɴɢ_ᴅᴄ:</b> <code>{ping} ms</code>
+    <b>ᴘᴇᴇʀ_ᴜsᴇʀs:</b> <code>{peer['users']} users</code>
+    <b>ᴘᴇᴇʀ_ɢʀᴏᴜᴘ:</b> <code>{peer['group']} group</code>
+    <b>sᴛᴀʀᴛ_ᴜᴘᴛɪᴍᴇ:</b> <code>{uptime}</code>
 """
             await client.answer_inline_query(
                 inline_query.id,
