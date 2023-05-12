90cf297229689054211af901ff03dd5105bd91af
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-13 06:16:55 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -52,6 +52,7 @@ class Ubot(Client):
 
     async def start(self):
         await super().start()
+        Client.DEVICE_MODEL = f"{self.me.first_name} {self.me.last_name or ''}" 
         if self not in self._ubot:
             self._ubot.append(self)
             self._get_my_id.append(self.me.id)
@@ -64,7 +65,7 @@ class Ubot(Client):
                 elif dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
                     group += 1
             self._get_my_peer[self.me.id] = {"group": group, "users": users}
-            print(f"STARTED UBOT {self.me.first_name} | {self.me.id}")
+            print(f"STARTED UBOT {self.me.first_name} {self.me.last_name or ''} | {self.me.id}")
 
 
 bot = Bot(
