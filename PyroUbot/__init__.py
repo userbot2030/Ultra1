b501b5cefb34e9c16b74475bf4772954e4c67a8d
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-13 10:23:25 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -20,8 +20,8 @@ class Bot(Client):
         super().__init__(**kwargs)
 
     def on_message(self):
-        def decorator(func):
-            self.add_handler(MessageHandler(func, filters.Filter), -1)
+        def decorator(func, filters=filters.Filter, group=-1):
+            self.add_handler(MessageHandler(func, filters), group)
             return func
 
         return decorator
@@ -40,9 +40,9 @@ class Ubot(Client):
         self._get_my_peer = {}
 
     def on_message(self):
-        def decorator(func):
+        def decorator(func, filters=filters.Filter, group=-1):
             for ub in self._ubot:
-                ub.add_handler(MessageHandler(func, filters.Filter), -1)
+                ub.add_handler(MessageHandler(func, filters), group)
             return func
 
         return decorator
