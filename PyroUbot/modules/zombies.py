6c743ea08d068792bee83d8f285a5e014750b35a
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-14 06:49:10 +0700

    Create zombies.py

---
+++ PyroUbot/modules/zombies.py
@@ -0,0 +1,15 @@
+from PyroUbot import *
+
+
+__MODULE__ = "ᴢᴏᴍʙɪᴇs"
+__HELP__ = f"""<b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴢᴏᴍʙɪᴇs 』</b>
+
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}zombies</code>
+
+  <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴇʟᴜᴀʀᴋᴀɴ ᴀᴋᴜɴ ᴛᴇʀʜᴀᴘᴜs ᴅɪɢʀᴜᴘ ᴀɴᴅᴀ.
+  """"
+
+
+@PY.UBOT("zombies")
+async def _(client, message):
+    await zombies_cmd(client, message)
