92b3eefca91166297befc08871017d6af2ad8fe6
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-14 06:08:55 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -40,10 +40,10 @@ TEXT_PAYMENT = os.getenv(
 <b>💬 sɪʟᴀʜᴋᴀɴ ʟᴀᴋᴜᴋᴀɴ ᴘᴇᴍʙᴀʏᴀʀᴀɴ sᴇʙᴇsᴀʀ ʀᴘ25.000 = 1 ʙᴜʟᴀɴ</b>
 
 <b>💳 ᴍᴏᴛᴏᴅᴇ ᴘᴇᴍʙᴀʏᴀʀᴀɴ:</b>
-  <b>├ ᴅᴀɴᴀ/ɢᴏᴘᴀʏ/ᴏᴠᴏ/sᴘᴀʏ</b>
-  <b>├────• <code>089525658633</code></b>
-  <b>├ ǫʀɪs</b>
-  <b>└────• <a href=https://api.qrcode-monkey.com/tmp/2274eaefa4ad07bab3a2578ac5c1e000.png>ᴋʟɪᴋ ᴅɪsɪɴɪ</a></b>
+ <b>├ ᴅᴀɴᴀ/ɢᴏᴘᴀʏ/ᴏᴠᴏ/sᴘᴀʏ</b>
+ <b>├────• <code>089525658633</code></b>
+ <b>├ ǫʀɪs</b>
+ <b>└────• <a href=https://api.qrcode-monkey.com/tmp/2274eaefa4ad07bab3a2578ac5c1e000.png>ᴋʟɪᴋ ᴅɪsɪɴɪ</a></b>
 
 <b>✅ ᴋʟɪᴋ ᴛᴏᴍʙᴏʟ ᴋᴏɴꜰɪʀᴍᴀsɪ ᴜɴᴛᴜᴋ ᴋɪʀɪᴍ ʙᴜᴋᴛɪ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ᴀɴᴅᴀ</b>
 """,
