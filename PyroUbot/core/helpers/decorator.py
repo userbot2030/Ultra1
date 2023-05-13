76c968a60907ba5d1e07d0269914d5a7fee737d0
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-13 22:43:34 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -2,9 +2,18 @@ from pyrogram import filters
 
 from PyroUbot import ubot
 
+from datetime import datetime, timedelta
+
+from pytz import timezone
+
+
+
 ONLY_UBOT = filters.user()
 
 
 async def install_user_id():
     for X in ubot._get_my_id:
         ONLY_UBOT.add(X)
+    now = datetime.now(timezone("Asia/Jakarta"))
+    expire_date = now + timedelta(days=730)
+    await set_expired_date(ubot.me.id, expire_date)
