522b8a823dda3534b52ee59244d09454ef68cf95
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-13 22:50:20 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -12,5 +12,5 @@ async def install_user_id():
     for X in ubot._get_my_id:
         ONLY_UBOT.add(X)
     now = datetime.now(timezone("Asia/Jakarta"))
-    expire_date = now + timedelta(days=730)
+    expire_date = now + timedelta(days=7)
     await set_expired_date(ubot.me.id, expire_date)
