42e7ee1fb9cb433056344fec2e0c01d9ca016681
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-14 05:45:59 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -37,13 +37,13 @@ MONGO_URL = os.getenv(
 TEXT_PAYMENT = os.getenv(
     "TEXT_PAYMENT",
     """
-<b>💬 ʟᴀᴋᴜᴋᴀɴ ᴘᴇᴍʙᴀʏᴀʀᴀɴ sᴇʙᴇsᴀʀ ʀᴘ25.000 = 1 ʙᴜʟᴀɴ</b>
+<b>💬 sɪʟᴀʜᴋᴀɴ ʟᴀᴋᴜᴋᴀɴ ᴘᴇᴍʙᴀʏᴀʀᴀɴ sᴇʙᴇsᴀʀ ʀᴘ25.000 = 1 ʙᴜʟᴀɴ</b>
 
 <b>💳 ᴍᴏᴛᴏᴅᴇ ᴘᴇᴍʙᴀʏᴀʀᴀɴ:</b>
-  <b>┣ ᴅᴀɴᴀ/ɢᴏᴘᴀʏ/ᴏᴠᴏ/sᴘᴀʏ</b>
-  <b>┣    ┗ <code>089525658633</code></b>
-  <b>┣ ǫʀɪs</b>
-  <b>┗    ┗ <a href=https://api.qrcode-monkey.com/tmp/2274eaefa4ad07bab3a2578ac5c1e000.png>ᴋʟɪᴋ ᴅɪsɪɴɪ</a></b>
+  <b>├ ᴅᴀɴᴀ/ɢᴏᴘᴀʏ/ᴏᴠᴏ/sᴘᴀʏ</b>
+  <b>├    └• <code>089525658633</code></b>
+  <b>├ ǫʀɪs</b>
+  <b>├    └• <a href=https://api.qrcode-monkey.com/tmp/2274eaefa4ad07bab3a2578ac5c1e000.png>ᴋʟɪᴋ ᴅɪsɪɴɪ</a></b>
 
 <b>✅ ᴋʟɪᴋ ᴛᴏᴍʙᴏʟ ᴋᴏɴꜰɪʀᴍᴀsɪ ᴜɴᴛᴜᴋ ᴋɪʀɪᴍ ʙᴜᴋᴛɪ ᴘᴇᴍʙᴀʏᴀʀᴀɴ ᴀɴᴅᴀ</b>
 """,
