795986e4ddc97d8f1b93ff7662dd7eae3f3f1c3a
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-15 06:43:37 +0700

    Update srh.py

--- PyroUbot/core/plugins/srh.py
+++ PyroUbot/core/plugins/srh.py
@@ -22,32 +22,35 @@ async def pic_bing_cmd(client, message):
             get_media.append(InputMediaPhoto(saved.photo.file_id))
             await saved.delete()
         except:
-            await TM.edit(f"<b>❌ ɪᴍᴀɢᴇ ᴘʜᴏᴛᴏ ᴋᴇ {X} ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
-    await client.send_media_group(
+            pass
+    if len(get_media) > 0:
+        return await TM.edit(f"<b>❌ ɪᴍᴀɢᴇ ᴘʜᴏᴛᴏ ᴋᴇ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
+    else:
+        await client.send_media_group(
         message.chat.id,
         get_media,
         reply_to_message_id=message.id,
     )
-    await TM.delete()
+        return await TM.delete()
 
 
 async def gif_cmd(client, message):
     if len(message.command) < 2:
         return await message.reply(f"<code>{message.text}</code> [ǫᴜᴇʀʏ]")
     TM = await message.reply("<b>ᴍᴇᴍᴘʀᴏsᴇs...</b>")
-    x = await client.get_inline_bot_results(
-        message.command[0], message.text.split(None, 1)[1]
-    )
     try:
+        x = await client.get_inline_bot_results(
+            message.command[0], message.text.split(None, 1)[1]
+        )
         saved = await client.send_inline_bot_result(
             client.me.id, x.query_id, x.results[random.randrange(len(x.results))].id
         )
     except:
         await message.reply("<b>❌ ɢɪꜰ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
-        await TM.delete()
+        return await TM.delete()
     saved = await client.get_messages(client.me.id, int(saved.updates[1].message.id))
     await client.send_animation(
         message.chat.id, saved.animation.file_id, reply_to_message_id=message.id
     )
     await TM.delete()
-    await saved.delete()
+    return await saved.delete()
