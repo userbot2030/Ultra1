65f5b21a290579eaa3d6187600ba93d97393c23d
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-15 10:36:19 +0700

    Update cvr.py

--- PyroUbot/core/plugins/cvr.py
+++ PyroUbot/core/plugins/cvr.py
@@ -202,35 +202,41 @@ async def colong_cmn(client, message):
         return await message.reply("Mohon balas ke media")
     anjing = dia.caption or None
     Tm = await message.reply("Processing...")
-    if dia.text:
-        await dia.copy(client.me.id)
-        await message.delete()
-        return await Tm.delete()
     if dia.photo:
+        if message.reply_to_message.photo.file_size > 10000000:
+            return await Tm.edit("file di atas 10mb tidak di izinkan")
         anu = await client.download_media(dia)
         await client.send_photo(client.me.id, anu, anjing)
         os.remove(anu)
         await message.delete()
         return await Tm.delete()
     if dia.video:
+        if message.reply_to_message.video.file_size > 10000000:
+            return await Tm.edit("file di atas 10mb tidak di izinkan")
         anu = await client.download_media(dia)
         await client.send_video(client.me.id, anu, anjing)
         os.remove(anu)
         await message.delete()
         return await Tm.delete()
     if dia.audio:
+        if message.reply_to_message.audio.file_size > 10000000:
+            return await Tm.edit("file di atas 10mb tidak di izinkan")
         anu = await client.download_media(dia)
         await client.send_audio(client.me.id, anu, anjing)
         os.remove(anu)
         await message.delete()
         return await Tm.delete()
     if dia.voice:
+        if message.reply_to_message.voice.file_size > 10000000:
+            return await Tm.edit("file di atas 10mb tidak di izinkan")
         anu = await client.download_media(dia)
         await client.send_voice(client.me.id, anu, anjing)
         os.remove(anu)
         await message.delete()
         return await Tm.delete()
     if dia.document:
+        if message.reply_to_message.document.file_size > 10000000:
+            return await Tm.edit("file di atas 10mb tidak di izinkan")
         anu = await client.download_media(dia)
         await client.send_document(client.me.id, anu, anjing)
         os.remove(anu)
