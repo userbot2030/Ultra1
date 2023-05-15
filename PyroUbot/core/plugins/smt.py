225f91f1dd46bf0dc98b16bd9da0fef2bb22b4fa
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-15 22:18:01 +0700

    Update smt.py

--- PyroUbot/core/plugins/smt.py
+++ PyroUbot/core/plugins/smt.py
@@ -16,15 +16,17 @@ async def sg_cmd(client, message):
     except Exception as error:
         return await lol.edit(error)
     bot = ["@Sangmata_bot", "@SangMata_beta_bot"]
+    try:
+        await client.join_chat("https://t.me/+QvgWBX23s3RkOWVl")
+    except:
+        pass
     getbot = random.choice(bot)
-    await client.unblock_user(getbot)
-    txt = await client.send_message(getbot, user.id)
+    txt = await client.send_message(-1001835552147, f"{getbot} allhistory {user.id}")
     await asyncio.sleep(4)
     await lol.delete()
     try:
-        sg = await client.get_messages(getbot, txt.id + 1)
-        await sg.copy(message.chat.id, reply_to_message_id=message.id)
+        sg = await client.get_messages(-1001835552147, txt.id + 1)
+        await message.reply(sg.text)
     except:
         await message.reply("❌ ᴀᴘɪ sᴇᴅᴀɴɢ ᴇʀʀᴏʀ sɪʟᴀʜᴋᴀɴ ᴄᴏʙᴀ ʟᴀɢɪ ɴᴀɴᴛɪ")
-    user_info = await client.resolve_peer(getbot)
-    return await client.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
+    await client.leave_chat(-1001835552147)
