2a3eb61d798d29b6237669a8439cc34c0bca990b
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-17 06:19:20 +0700

    Update inline.py

--- PyroUbot/core/helpers/inline.py
+++ PyroUbot/core/helpers/inline.py
@@ -1,6 +1,8 @@
 from pyrogram.errors import MessageNotModified
 from pyrogram.types import (InlineKeyboardButton, InlineQueryResultArticle,
                             InputTextMessageContent)
+from pykeyboard import InlineKeyboard
+
 
 from PyroUbot import *
 
@@ -67,3 +69,43 @@ class INLINE:
                     await callback_query.answer("❌ ERROR")
 
         return wrapper
+
+
+
+async def create_button(m):
+    buttons = InlineKeyboard(row_width=2)
+    keyboard = []
+    if "|" not in m.text.split(None, 1)[1]:
+        msg = []
+        for X in m.text.split(None, 1)[1].split():
+            keyboard.append(
+                InlineKeyboardButton(X.split(":", 1)[0], url=X.split(":", 1)[1])
+            )
+            msg.append(X.split(":")[0])
+        buttons.add(*keyboard)
+        if m.reply_to_message:
+            text = m.reply_to_message.text
+        else:
+            msg_text = ""
+            for Z in msg:
+                msg_text += f"{Z} "
+            text = msg_text
+    else:
+        for X in m.text.split("|", 1)[1].split():
+            keyboard.append(
+                InlineKeyboardButton(X.split(":", 1)[0], url=X.split(":", 1)[1])
+            )
+        buttons.add(*keyboard)
+        text = m.text.split("|", 1)[0].split(None, 1)[1]
+    return buttons, text
+
+async def notes_create_button(text):
+    buttons = InlineKeyboard(row_width=2)
+    keyboard = []
+    for X in text.split("|", 1)[1].split():
+        keyboard.append(
+            InlineKeyboardButton(X.split(":", 1)[0], url=X.split(":", 1)[1])
+        )
+    buttons.add(*keyboard)
+    text_button = text.split("|", 1)[0]
+    return buttons, text_button
