b30800664ab8eb17acae37f771488d3e04acdcd0
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-17 10:08:12 +0700

    Update __init__.py

--- PyroUbot/core/plugins/__init__.py
+++ PyroUbot/core/plugins/__init__.py
@@ -41,3 +41,4 @@ from PyroUbot.core.plugins.whs import *
 from PyroUbot.core.plugins.wrt import *
 from PyroUbot.core.plugins.ytp import *
 from PyroUbot.core.plugins.zbs import *
+from PyroUbot.core.plugins.bls import *
