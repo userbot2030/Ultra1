ac7f1a8238f45d5694abb9bde6ccf6b7c8a9056c
XtomiX <XtomiX@users.noreply.github.com>
2023-05-17 07:56:22 +0000

    Auto Fix

--- PyroUbot/modules/add_ubot.py
+++ PyroUbot/modules/add_ubot.py
@@ -20,6 +20,7 @@ async def _(client, message):
 async def _(client, callback_query):
     await hapus_ubot(client, callback_query)
 
+
 @PY.BOT("restart", ONLY_UBOT)
 async def _(client, message):
     await restart_cmd(client, message)
