46fc1e46e0c003f775bb01ee04a2d99618fff2f1
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-18 12:47:54 +0700

    Update skr.py

--- PyroUbot/core/plugins/skr.py
+++ PyroUbot/core/plugins/skr.py
@@ -10,6 +10,12 @@ from pyrogram.raw.functions.messages import DeleteHistory, GetStickerSet
 from pyrogram.raw.types import InputStickerSetShortName
 
 from PyroUbot import *
+import imghdr
+import os
+
+from pyrogram.errors import *
+
+from PyroUbot import *
 
 
 async def memes_cmd(client, message):
@@ -336,3 +342,98 @@ async def memify_cmd(client, message):
         ),
     )
     os.remove(meme)
+    
+    
+    
+
+async def kang_cmd_bot(client, message):
+    if not message.reply_to_message:
+        return await message.reply_text("Reply to a sticker/image to kang it.")
+    if not message.from_user:
+        return await message.reply_text("You are anon admin, kang stickers in my pm.")
+    msg = await message.reply_text("Kanging Sticker..")
+    args = message.text.split()
+    if len(args) > 1:
+        sticker_emoji = str(args[1])
+    elif message.reply_to_message.sticker and message.reply_to_message.sticker.emoji:
+        sticker_emoji = message.reply_to_message.sticker.emoji
+    else:
+        sticker_emoji = "✨"
+    doc = message.reply_to_message.photo or message.reply_to_message.document
+    try:
+        if message.reply_to_message.sticker:
+            sticker = await create_sticker(
+                await get_document_from_file_id(
+                    message.reply_to_message.sticker.file_id
+                ),
+                sticker_emoji,
+            )
+        elif doc:
+            if doc.file_size > 10000000:
+                return await msg.edit("File size too large.")
+            temp_file_path = await client.download_media(doc)
+            image_type = imghdr.what(temp_file_path)
+            if image_type not in SUPPORTED_TYPES:
+                return await msg.edit("Format not supported! ({})".format(image_type))
+            try:
+                temp_file_path = await resize_file_to_sticker_size(temp_file_path)
+            except Exception as e:
+                await msg.edit_text(f"Something wrong happened.\n{e}")
+            sticker = await create_sticker(
+                await upload_document(client, temp_file_path, message.chat.id),
+                sticker_emoji,
+            )
+            if os.path.isfile(temp_file_path):
+                os.remove(temp_file_path)
+        else:
+            return await msg.edit("Nope, can't kang that.")
+    except ShortnameOccupyFailed:
+        return await message.reply_text("Change Your Name Or Username")
+    except Exception as e:
+        return await message.reply(e)
+    packname = "f" + str(message.from_user.id) + "_by_" + bot.me.username
+    limit = 0
+    try:
+        while True:
+            if limit >= 50:
+                return await msg.delete()
+            stickerset = await get_sticker_set_by_name(client, packname)
+            if not stickerset:
+                stickerset = await create_sticker_set(
+                    client,
+                    message.from_user.id,
+                    f"@{bot.me.username} ᴋᴀɴɢ ᴘᴀᴄᴋ",
+                    packname,
+                    [sticker],
+                )
+            elif stickerset.set.count >= MAX_STICKERS:
+                packnum += 1
+                packname = (
+                    "f"
+                    + str(packnum)
+                    + "_"
+                    + str(message.from_user.id)
+                    + "_by_"
+                    + bot.me.username
+                )
+                limit += 1
+                continue
+            else:
+                try:
+                    await add_sticker_to_set(client, stickerset, sticker)
+                except StickerEmojiInvalid:
+                    return await msg.edit("[ERROR]: INVALID_EMOJI_IN_ARGUMENT")
+            limit += 1
+            break
+
+        await msg.edit(
+            "Sticker Kanged To [Pack](t.me/addstickers/{})\nEmoji: {}".format(
+                packname, sticker_emoji
+            )
+        )
+    except StickerPngNopng:
+        await message.reply_text(
+            "Stickers must be png files but the provided image was not a png"
+        )
+    except StickerPngDimensions:
+        await message.reply_text("The sticker png dimensions are invalid.")
