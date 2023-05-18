cb054061f19d2d305abc318d23eacb05f6f9e6e5
XtomiX <XtomiX@users.noreply.github.com>
2023-05-18 04:10:56 +0000

    Auto Fix

--- PyroUbot/core/plugins/Abc.py
+++ PyroUbot/core/plugins/Abc.py
@@ -1,32 +1,17 @@
 import imghdr
 import os
-from asyncio import gather
 from traceback import format_exc
 
-from pyrogram import filters
-from pyrogram.errors import (
-    PeerIdInvalid,
-    ShortnameOccupyFailed,
-    StickerEmojiInvalid,
-    StickerPngDimensions,
-    StickerPngNopng,
-    UserIsBlocked,
-)
+from pyrogram.errors import (PeerIdInvalid, ShortnameOccupyFailed,
+                             StickerEmojiInvalid, StickerPngDimensions,
+                             StickerPngNopng, UserIsBlocked)
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from wbb import BOT_USERNAME, app
+from wbb.utils.files import (get_document_from_file_id,
+                             resize_file_to_sticker_size, upload_document)
+from wbb.utils.stickerset import (add_sticker_to_set, create_sticker,
+                                  create_sticker_set, get_sticker_set_by_name)
 
-from wbb import BOT_USERNAME, SUDOERS, USERBOT_PREFIX, app, app2, eor
-from wbb.core.decorators.errors import capture_err
-from wbb.utils.files import (
-    get_document_from_file_id,
-    resize_file_to_sticker_size,
-    upload_document,
-)
-from wbb.utils.stickerset import (
-    add_sticker_to_set,
-    create_sticker,
-    create_sticker_set,
-    get_sticker_set_by_name,
-)
 
 async def kang(client, message: Message):
     if not message.reply_to_message:
