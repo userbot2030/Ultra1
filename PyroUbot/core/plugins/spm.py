ffb8ef243b24e60e05b97e50661bb6518f48edc7
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-20 15:44:25 +0700

    Update spm.py

--- PyroUbot/core/plugins/spm.py
+++ PyroUbot/core/plugins/spm.py
@@ -1,75 +1,52 @@
 import asyncio
+from PyroUbot import *
 
 
 async def spam_cmd(client, message):
-    if message.command[0] == "spam":
-        if message.reply_to_message:
-            spam = await message.reply("ᴅɪᴘʀᴏsᴇs")
-            try:
-                quantity = int(message.text.split(None, 2)[1])
-                spam_text = message.text.split(None, 2)[2]
-            except Exception as error:
-                return await spam.edit(error)
-            await asyncio.sleep(1)
-            await message.delete()
-            await spam.delete()
-            for i in range(quantity):
-                await client.send_message(
-                    message.chat.id,
-                    spam_text,
-                    reply_to_message_id=message.reply_to_message.id,
-                )
-                await asyncio.sleep(0.3)
+    reply = message.reply_to_message
+    msg = await message.reply("xsᴇᴅᴀɴɢ ᴅɪᴘʀᴏsᴇs", quote=False)
+    if reply:
+        try:
+            for i range(int(message.command[1])):
+                await reply.copy(message.chat.id)
+                await asyncio.sleep(0.1)
+        except Exception as error:
+            await msg.edit(error)
+    else:
+        if not get_arg(message):
+            return await msg.edit("sɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ <code>help spam</code> ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ᴄᴀʀᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ")
         else:
-            if len(message.command) < 3:
-                await message.reply_text("⚡ ᴜsᴀɢᴇ:\n.spam ᴊᴜᴍʟᴀʜ sᴘᴀᴍ, ᴛᴇxᴛ sᴘᴀᴍ")
-            else:
-                spam = await message.reply("Diproses")
-                try:
-                    quantity = int(message.text.split(None, 2)[1])
-                    spam_text = message.text.split(None, 2)[2]
-                except Exception as error:
-                    return await spam.edit(error)
-                await asyncio.sleep(1)
-                await message.delete()
-                await spam.delete()
-                for i in range(quantity):
-                    await client.send_message(message.chat.id, spam_text)
-                    await asyncio.sleep(0.3)
-    elif message.command[0] == "dspam":
-        if message.reply_to_message:
-            if len(message.command) < 3:
-                return await message.reply_text(
-                    "⚡ ᴜsᴀɢᴇ:\n.dspam ᴊᴜᴍʟᴀʜ sᴘᴀᴍ, ᴊᴜᴍʟᴀʜ ᴅᴇʟᴀʏ ᴅᴇᴛɪᴋ, ʙᴀʟᴀs ᴘᴇsᴀɴ"
-                )
-            spam = await message.reply("ᴅɪᴘʀᴏsᴇs")
             try:
-                quantity = int(message.text.split(None, 3)[1])
-                delay_msg = int(message.text.split(None, 3)[2])
+                for i range(int(message.command[1])):
+                    await message.reply(get_arg(message), quote=False)
+                    await asyncio.sleep(0.1)
             except Exception as error:
-                return await spam.edit(error)
-            await asyncio.sleep(1)
-            await message.delete()
-            await spam.delete()
-            for i in range(quantity):
-                await message.reply_to_message.copy(message.chat.id)
-                await asyncio.sleep(delay_msg)
+                await msg.edit(error)
+    await msg.delete()
+    await message.deletw()
+                            
+
+async def dspam_cmd(client, message):
+    reply = message.reply_to_message
+    msg = await message.reply("xsᴇᴅᴀɴɢ ᴅɪᴘʀᴏsᴇs", quote=False)
+    if reply:
+        try:
+            for i range(int(message.command[1])):
+                await reply.copy(message.chat.id)
+                await asyncio.sleep(int(message.command[2])
+        except Exception as error:
+            await msg.edit(error)
+    else:
+        if not get_arg(message):
+            return await msg.edit("sɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ <code>help spam</code> ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ᴄᴀʀᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ")
         else:
-            if len(message.command) < 4:
-                return await message.reply_text(
-                    "⚡ ᴜsᴀɢᴇ:\n.dspam ᴊᴜᴍʟᴀʜ sᴘᴀᴍ, ᴊᴜᴍʟᴀʜ ᴅᴇʟᴀʏ ᴅᴇᴛɪᴋ, ᴛᴇxᴛ sᴘᴀᴍ"
-                )
-            else:
-                spam = await message.reply("ᴅɪᴘʀᴏsᴇs")
-                try:
-                    quantity = int(message.text.split(None, 3)[1])
-                    delay_msg = int(message.text.split(None, 3)[2])
-                    spam_text = message.text.split(None, 3)[3]
-                except Exception as error:
-                    return await spam.edit(error)
-                await asyncio.sleep(1)
-                await message.delete()
-                await spam.delete()
-                for i in range(quantity):
-                    await client.send_message(message.chat.id, spam_text)
-                    await asyncio.sleep(delay_msg)
+            try:
+                for i range(int(message.command[1])):
+                    await message.reply(get_arg(message), quote=False)
+                    await asyncio.sleep(int(message.command[2])
+            except Exception as error:
+                await msg.edit(error)
+    await msg.delete()
+    await message.deletw()
+    
+    
