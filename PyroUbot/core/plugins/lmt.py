f69879d1d82c1f58a500ad71de3d6ffe64ec3c8f
XtomiX <XtomiX@users.noreply.github.com>
2023-05-25 10:55:39 +0000

    Auto Fix

--- PyroUbot/core/plugins/lmt.py
+++ PyroUbot/core/plugins/lmt.py
@@ -6,7 +6,7 @@ from pyrogram.raw.functions.messages import DeleteHistory, StartBot
 async def limit_cmd(client, message):
     await client.unblock_user("SpamBot")
     bot_info = await client.resolve_peer("SpamBot")
-    msg = await message.reply("<code>ᴘʀᴏᴄᴇssɪɴɢ . . .</code>")
+    await message.reply("<code>ᴘʀᴏᴄᴇssɪɴɢ . . .</code>")
     response = await client.invoke(
         StartBot(
             bot=bot_info,
