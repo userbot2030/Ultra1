5fd16b1f43661af0c1f16efa2cbe0c97ade84409
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-26 18:03:30 +0700

    Update smt.py

--- PyroUbot/core/plugins/smt.py
+++ PyroUbot/core/plugins/smt.py
@@ -7,24 +7,27 @@ from PyroUbot import *
 
 
 async def sg_cmd(client, message):
-    user_id = await extract_user(message)
+    get_user = await extract_user(message)
     lol = await message.reply("</b>ᴍᴇᴍᴘʀᴏsᴇs. . .</b>")
-    if not user_id:
+    if not get_user:
         return await lol.edit("<b>ᴜsᴇʀ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
     try:
-        user = await client.get_users(user_id)
-    except Exception as error:
-        return await lol.edit(error)
+        user_id = (await client.get_users(get_user)).id
+    except Exception:
+        try:
+            user_id = int(message.command[1])
+        except Exception as error:
+            return await lol.edit(error)
     bot = ["@Sangmata_bot", "@SangMata_beta_bot"]
     getbot = random.choice(bot)
     await client.unblock_user(getbot)
-    txt = await client.send_message(getbot, user.id)
+    txt = await client.send_message(getbot, user_id)
     await asyncio.sleep(4)
     await lol.delete()
     try:
         sg = await client.get_messages(getbot, txt.id + 1)
         await sg.copy(message.chat.id, reply_to_message_id=message.id)
-    except:
+    except Exception:
         await message.reply("❌ ᴀᴘɪ sᴇᴅᴀɴɢ ᴇʀʀᴏʀ sɪʟᴀʜᴋᴀɴ ᴄᴏʙᴀ ʟᴀɢɪ ɴᴀɴᴛɪ")
     user_info = await client.resolve_peer(getbot)
     return await client.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
