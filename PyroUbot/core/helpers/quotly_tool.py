7356b92523d0f8dbb161851b290776812e7649b8
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-27 07:26:25 +0700

    Update quotly_tool.py

--- PyroUbot/core/helpers/quotly_tool.py
+++ PyroUbot/core/helpers/quotly_tool.py
@@ -173,7 +173,7 @@ async def pyrogram_to_quotly(messages):
     payload = {
         "type": "quote",
         "format": "png",
-        "backgroundColor": "#333333",
+        "backgroundColor": "#162330",
         "messages": [],
     }
 
