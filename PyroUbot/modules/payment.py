a8bc05613bcb07ef7fd2d646155df31ae0369894
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-27 10:53:58 +0700

    Update payment.py

--- PyroUbot/modules/payment.py
+++ PyroUbot/modules/payment.py
@@ -5,7 +5,11 @@ from PyroUbot import *
 async def _(client, callback_query):
     await confirm_callback(client, callback_query)
 
-
+@PY.CALLBACK("^(kurang|tambah)")
+async def _(client, callback_query):
+    await tambah_or_kurang(client, callback_query)
+    
+    
 @PY.CALLBACK("^(success|failed|home)")
 async def _(client, callback_query):
     await success_failed_home_callback(client, callback_query)
