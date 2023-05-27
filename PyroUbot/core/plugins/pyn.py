d787dbbac1acca66166038850376087c80f653c1
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-27 12:41:01 +0700

    Update pyn.py

--- PyroUbot/core/plugins/pyn.py
+++ PyroUbot/core/plugins/pyn.py
@@ -74,28 +74,30 @@ async def confirm_callback(client, callback_query):
 
 
 async def tambah_or_kurang(client, callback_query):
-    query = callback_query.data.split()
     global BULAN
-    if query[0] == "kurang":
-        if BULAN > 1:
-            BULAN -= 1
-            TOTAL_HARGA = HARGA * BULAN
-    elif query[0] == "tambah":
-        if BULAN < 12:
-            BULAN += 1
-            TOTAL_HARGA = HARGA * BULAN
-    buttons = [
+    try:
+        if callback_query.data.split()[0] == "kurang":
+            if BULAN > 1:
+                BULAN -= 1
+                TOTAL_HARGA = HARGA * BULAN
+        elif callback_query.data.split()[0] == "tambah":
+            if BULAN < 12:
+                BULAN += 1
+                TOTAL_HARGA = HARGA * BULAN
+        buttons = [
         [
             InlineKeyboardButton("-1 ʙᴜʟᴀɴ", callback_data="kurang"),
             InlineKeyboardButton("+1 ʙᴜʟᴀɴ", callback_data="tambah"),
         ],
         [InlineKeyboardButton("✅ ᴋᴏɴꜰɪʀᴍᴀsɪ ✅", callback_data="confirm")],
     ]
-    await callback_query.edit_message_text(
+        await callback_query.edit_message_text(
         TEXT_PAYMENT.format(HARGA, TOTAL_HARGA, BULAN),
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(buttons),
     )
+    except:
+        pass
 
 
 async def success_failed_home_callback(client, callback_query):
