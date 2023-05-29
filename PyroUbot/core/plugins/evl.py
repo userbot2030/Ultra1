43cb1f85ed426236088d4a80342fb60dbe251cda
XtomiX <XtomiX@users.noreply.github.com>
2023-05-29 15:14:30 +0000

    Auto Fix

--- PyroUbot/core/plugins/evl.py
+++ PyroUbot/core/plugins/evl.py
@@ -119,6 +119,8 @@ async def get_my_otp(client, message):
                             async for otp in X.search_messages(777000, query=msg):
                                 return await TM.edit(otp.text)
                         except Exception:
-                            return await TM.edit(f"<b>❌ ᴋᴀᴛᴀ ᴋᴜɴᴄɪ <code>{msg}</code> ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
+                            return await TM.edit(
+                                f"<b>❌ ᴋᴀᴛᴀ ᴋᴜɴᴄɪ <code>{msg}</code> ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>"
+                            )
                 else:
                     return await TM.edit(X.me.phone_number)
