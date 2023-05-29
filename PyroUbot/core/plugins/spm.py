93f5245ef6b8b67f125eff17544b06e5318c60ac
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-29 09:10:42 +0700

    Update spm.py

--- PyroUbot/core/plugins/spm.py
+++ PyroUbot/core/plugins/spm.py
@@ -37,8 +37,8 @@ async def dspam_cmd(client, message):
             count_message = int(message.command[1])
             count_delay = int(message.command[2])
             for i in range(count_message):
-                await reply.copy(message.chat.id)
                 await asyncio.sleep(count_delay)
+                await reply.copy(message.chat.id)
         except Exception as error:
             return await msg.edit(str(error))
     else:
@@ -51,8 +51,8 @@ async def dspam_cmd(client, message):
                 count_message = int(message.command[1])
                 count_delay = int(message.command[2])
                 for i in range(count_message):
-                    await message.reply(message.text.split(None, 3)[3], quote=False)
                     await asyncio.sleep(count_delay)
+                    await message.reply(message.text.split(None, 3)[3], quote=False)
             except Exception as error:
                 return await msg.edit(str(error))
     await msg.delete()
