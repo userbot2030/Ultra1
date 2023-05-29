8704e349c166e49ed6f926ee94204d2e7a110424
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-29 22:56:42 +0700

    Update gpw.py

--- PyroUbot/core/plugins/gpw.py
+++ PyroUbot/core/plugins/gpw.py
@@ -2,10 +2,12 @@ import string, random
 
 
 async def gen_password(client, message):
+    if len(message.command) < 2:
+        return await message.delete()
     try:
        count = int(message.command[1])
-     except Exception as error:
-         return await message.reply(error, quote=True)
-     alphabet = string.ascii_letters + string.digits
-     password = ''.join(random.choice(alphabet) for i in range(count))
-     await message.reply(password, quote=True)
+    except Exception as error:
+        return await message.reply(error, quote=True)
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(random.choice(alphabet) for i in range(count))
+    return await message.reply(password, quote=True)
