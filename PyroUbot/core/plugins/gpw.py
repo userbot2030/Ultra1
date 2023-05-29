4a894bb5e2fbe8bf0899bba3ae067bbfdf372885
XtomiX <XtomiX@users.noreply.github.com>
2023-05-29 15:57:16 +0000

    Auto Fix

--- PyroUbot/core/plugins/gpw.py
+++ PyroUbot/core/plugins/gpw.py
@@ -1,13 +1,14 @@
-import string, random
+import random
+import string
 
 
 async def gen_password(client, message):
     if len(message.command) < 2:
         return await message.delete()
     try:
-       count = int(message.command[1])
+        count = int(message.command[1])
     except Exception as error:
         return await message.reply(error, quote=True)
     alphabet = string.ascii_letters + string.digits
-    password = ''.join(random.choice(alphabet) for i in range(count))
+    password = "".join(random.choice(alphabet) for i in range(count))
     return await message.reply(password, quote=True)
