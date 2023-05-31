23c37d1e6271b401aefe7e9d616a1fa20b20be1b
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-31 23:16:26 +0700

    Update spm.py

--- PyroUbot/core/plugins/spm.py
+++ PyroUbot/core/plugins/spm.py
@@ -31,28 +31,29 @@ async def spam_cmd(client, message):
 
 async def dspam_cmd(client, message):
     reply = message.reply_to_message
+    msg = await message.reply("sᴇᴅᴀɴɢ ᴅɪᴘʀᴏsᴇs", quote=False)
     if reply:
         try:
             count_message = int(message.command[1])
             count_delay = int(message.command[2])
-            await message.delete()
             for i in range(count_message):
-                await asyncio.sleep(count_delay)
                 await reply.copy(message.chat.id)
+                await asyncio.sleep(count_delay)
         except Exception as error:
-            return await message.reply(str(error))
+            return await msg.edit(str(error))
     else:
         if len(message.command) < 4:
-            return await message.reply(
+            return await msg.edit(
                 "sɪʟᴀʜᴋᴀɴ ᴋᴇᴛɪᴋ <code>.help spam</code> ᴜɴᴛᴜᴋ ᴍᴇʟɪʜᴀᴛ ᴄᴀʀᴀ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ"
             )
         else:
             try:
                 count_message = int(message.command[1])
                 count_delay = int(message.command[2])
-                await message.delete()
                 for i in range(count_message):
-                    await asyncio.sleep(count_delay)
                     await message.reply(message.text.split(None, 3)[3], quote=False)
+                    await asyncio.sleep(count_delay)
             except Exception as error:
-                return await message.reply(str(error))
+                return await msg.edit(str(error))
+    await msg.delete()
+    await message.delete()
