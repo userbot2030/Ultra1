6a79545c606061561fc9fe6869338729cdba4374
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-05-31 14:42:40 +0700

    Update mms.py

--- PyroUbot/core/plugins/mms.py
+++ PyroUbot/core/plugins/mms.py
@@ -4,17 +4,15 @@ import random
 async def memes_cmd(client, message):
     if len(message.command) < 2:
         return await message.reply("<code>memes</code> [ᴛᴇxᴛ]")
-    text = f"#{random.randrange(67)} {message.text.split(None, 1)[1]}"
-    TM = await message.reply("<code>ᴍᴇᴍᴘʀᴏsᴇs</code>")
-    x = await client.get_inline_bot_results("StickerizerBot", text)
-    saved = await client.send_inline_bot_result(
-        client.me.id, x.query_id, x.results[0].id
-    )
-    saved = await client.get_messages(client.me.id, int(saved.updates[1].message.id))
     try:
-        await client.send_sticker(
-            message.chat.id, saved.sticker.file_id, reply_to_message_id=message.id
+        text = f"#{random.randrange(67)} {message.text.split(None, 1)[1]}"
+        TM = await message.reply("<code>ᴍᴇᴍᴘʀᴏsᴇs</code>")
+        x = await client.get_inline_bot_results("StickerizerBot", text)
+        saved = await client.send_inline_bot_result(
+             client.me.id, x.query_id, x.results[0].id
         )
+        saved = await client.get_messages(client.me.id, int(saved.updates[1].message.id))
+        await message.reply_sticker(saved.sticker.file_id, quote=True)
     except Exception as error:
         await message.reply(error, quote=True)
     await saved.delete()
