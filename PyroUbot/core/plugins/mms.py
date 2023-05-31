88f3ab285b39ba4ad5a200c90aee963d1409c4a8
XtomiX <XtomiX@users.noreply.github.com>
2023-05-31 07:43:09 +0000

    Auto Fix

--- PyroUbot/core/plugins/mms.py
+++ PyroUbot/core/plugins/mms.py
@@ -9,9 +9,11 @@ async def memes_cmd(client, message):
         TM = await message.reply("<code>ᴍᴇᴍᴘʀᴏsᴇs</code>")
         x = await client.get_inline_bot_results("StickerizerBot", text)
         saved = await client.send_inline_bot_result(
-             client.me.id, x.query_id, x.results[0].id
+            client.me.id, x.query_id, x.results[0].id
         )
-        saved = await client.get_messages(client.me.id, int(saved.updates[1].message.id))
+        saved = await client.get_messages(
+            client.me.id, int(saved.updates[1].message.id)
+        )
         await message.reply_sticker(saved.sticker.file_id, quote=True)
     except Exception as error:
         await message.reply(error, quote=True)
