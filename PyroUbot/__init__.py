75732f6c9aabc5d6f70d8c7914b2556722ac5d27
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-01 08:10:57 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -1,7 +1,6 @@
 import logging
 
 from pyrogram import Client
-from pyrogram.enums import ChatType
 from pyrogram.filters import Filter
 from pyrogram.handlers import MessageHandler
 from pyromod import listen
@@ -72,24 +71,6 @@ ubot = Ubot(
     session_string=SESSION_STRING,
 )
 
-
-get_my_peer = {}
-
-
-async def get_peer_userbot(self):
-    users = 0
-    group = 0
-    async for dialog in self.get_dialogs():
-        if dialog.chat.type == ChatType.PRIVATE:
-            users += 1
-        elif dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
-            group += 1
-    get_my_peer[self.me.id] = {"pm": users, "gc": group}
-
-
-async def install_my_peer():
-    for X in ubot._ubot:
-        await get_peer_userbot(X)
 
 
 from PyroUbot.core.database import *
