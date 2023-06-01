ba537e8350946bbda55621482d97f54bf17ee437
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-01 08:13:01 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -1,10 +1,29 @@
+
 from pyrogram import filters
 
+from pyrogram.enums import ChatType
 from PyroUbot import ubot
 
 ONLY_UBOT = filters.user()
+get_my_peer = {}
 
 
 async def install_user_id():
     for X in ubot._get_my_id:
         ONLY_UBOT.add(X)
+
+
+async def get_peer_userbot(self):
+    users = 0
+    group = 0
+    async for dialog in self.get_dialogs():
+        if dialog.chat.type == ChatType.PRIVATE:
+            users += 1
+        elif dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
+            group += 1
+    get_my_peer[self.me.id] = {"pm": users, "gc": group}
+
+
+async def install_my_peer():
+    for X in ubot._ubot:
+        await get_peer_userbot(X)
