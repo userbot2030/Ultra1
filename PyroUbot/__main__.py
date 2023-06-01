cea0d8f0a1a6ba141df3de0b5fbe5f242d4c42df
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-01 08:27:48 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -24,6 +24,7 @@ async def main():
     await expired_userbot()
     await install_user_id()
     await install_my_peer()
+    await bot.send_message(OWNER_ID, "Install peer id telah selesai")
     await idle()
 
 
