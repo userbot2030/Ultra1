239f4d850d022e1817082d9dedd18f1bd5a39320
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-01 09:11:08 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -13,15 +13,32 @@ async def install_user_id():
         ONLY_UBOT.add(X)
 
 
-async def install_my_peer():
+async def install_all_peer():
     users = 0
     group = 0
-    for X in ubot._ubot:
-        async for dialog in X.get_dialogs():
+    for self in ubot._ubot:
+        async for dialog in self.get_dialogs():
             if dialog.chat.type == ChatType.PRIVATE:
                 users += 1
             elif dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
                 group += 1
-        get_my_peer[X.me.id] = {"pm": users, "gc": group}
-        print(f"{X.me.id} install to get_my_peer")
+        print(f"{self.me.id} install to get_my_peer")
     await bot.send_message(OWNER_ID, "✅ sᴇᴍᴜᴀ ᴘᴇᴇʀɪᴅ ᴛᴇʟᴀʜ ʙᴇʀʜᴀsɪʟ ᴅɪɪɴsᴛᴀʟʟ")
+
+
+async def install_my_peer(self):
+    users = 0
+    group = 0    
+    async for dialog in self.get_dialogs():
+        if dialog.chat.type == ChatType.PRIVATE:
+            users += 1
+        elif dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
+            group += 1
+    get_my_peer[X.me.id] = {"pm": users, "gc": group}
+    print(f"{self.me.id} install to get_my_peer")
+        
+    
+    
+    
+    
+    
