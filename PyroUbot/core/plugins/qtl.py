1f62b7531e7b2a4a15025c4b52e45052b98f7001
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-01 16:41:27 +0700

    Update qtl.py

--- PyroUbot/core/plugins/qtl.py
+++ PyroUbot/core/plugins/qtl.py
@@ -45,3 +45,40 @@ async def quotly_cmd(self, ctx):
         return await ctx.reply_sticker(bio_sticker)
     except Exception as e:
         return await ctx.reply(f"<code>Error : {e}</code>")
+
+    
+async def quotly_cmd_test(client, message):
+    info = await message.reply("<b>ᴍᴇᴍᴘʀᴏsᴇs.....</b>", quote=True)
+    if message.reply_to_message:
+        if len(message.command) < 2:
+            msg = [message.reply_to_message]
+       else:
+            try:
+               count = int(message.command[1])
+            except Exception as error:
+                await info.edit(error)
+            msg = [
+                   i
+                   for i in await user.get_messages(
+                       chat_id=message.chat.id,
+                       message_ids=range(message.reply_to_message.id, message.reply_to_message.id + count),
+                       replies=-1,
+                   )
+               ]
+       await client.unblock_user("@QuotLyBot")
+       for x in msg:
+           await x.forward("@QuotLyBot")
+       await info.delete()
+       async for quotly in client.get_chat_history("@QuotLyBot", limit=1):
+           if not quotly.sticker:
+               await message.reply(
+                f"❌ @QuotLyBot ᴛɪᴅᴀᴋ ᴅᴀᴘᴀᴛ ᴍᴇʀᴇsᴘᴏɴ ᴘᴇʀᴍɪɴᴛᴀᴀɴ", quote=True
+            )
+           else:
+               await message.reply_sticker(quotly.sticker.file_id)
+       user_info = await client.resolve_peer("@QuotLyBot")
+       return await client.invoke(DeleteHistory(peer=user_info, max_id=0, revoke=True))
+
+    
+
+    
