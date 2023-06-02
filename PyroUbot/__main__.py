c3daa92807a90a47e7601ec3ef114303df3aa086
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-03 06:30:53 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -1,4 +1,4 @@
-from asyncio import get_event_loop_policy
+import asyncio 
 
 from pyrogram.errors import RPCError
 from pyrogram.methods.utilities.idle import idle
@@ -7,8 +7,7 @@ from PyroUbot import *
 
 
 async def main():
-    await bot.start()
-    await ubot.start()
+    await asyncio.gather(bot.start(), ubot.start())
     for _ubot in await get_userbots():
         ubot_ = Ubot(**_ubot)
         try:
@@ -21,12 +20,14 @@ async def main():
             for X in await get_chat(user_id):
                 await remove_chat(user_id, X)
             print(f"✅ {user_id} ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs")
-    await loadPlugins()
-    await install_user_id()
-    await install_all_peer()
-    await expired_userbot()
+    await asyncio.gather(
+        loadPlugins(),
+        install_user_id(),
+        install_all_peer(),
+        expired_userbot()
+    )
     await idle()
 
 
 if __name__ == "__main__":
-    get_event_loop_policy().get_event_loop().run_until_complete(main())
+    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(main())
