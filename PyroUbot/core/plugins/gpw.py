88433d90210cc2049c0d57ead9f9e45a9bc3fe8b
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-02 19:31:45 +0700

    Update gpw.py

--- PyroUbot/core/plugins/gpw.py
+++ PyroUbot/core/plugins/gpw.py
@@ -1,6 +1,6 @@
 import random
 import string
-
+from pyrogram.enums import ParseMode
 
 async def gen_password(client, message):
     if len(message.command) < 2:
@@ -10,4 +10,4 @@ async def gen_password(client, message):
     except Exception as error:
         return await message.reply(str(error), quote=True)
     password = "".join(random.choice(string.printable) for _ in range(count))
-    return await message.reply(password, quote=True)
+    return await message.reply(password, parse_mode=ParseMode.MARKDOWN, quote=True)
