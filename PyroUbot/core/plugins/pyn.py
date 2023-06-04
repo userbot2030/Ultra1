a69ce032bfceb105722b45bbe817ae0dcee50870
XtomiX <XtomiX@users.noreply.github.com>
2023-06-04 12:58:08 +0000

    Auto Fix

--- PyroUbot/core/plugins/pyn.py
+++ PyroUbot/core/plugins/pyn.py
@@ -3,7 +3,6 @@ from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
 from pytz import timezone
 
 from PyroUbot import *
