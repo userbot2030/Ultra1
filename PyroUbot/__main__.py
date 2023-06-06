0230b9ee31ed76021d1fd4036f518329a9bb5889
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-06 17:12:29 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -7,7 +7,8 @@ from PyroUbot import *
 
 
 async def main():
-    await asyncio.gather(bot.start(), ubot.start())
+    await bot.start()
+    await ubot.start()
     for _ubot in await get_userbots():
         ubot_ = Ubot(**_ubot)
         try:
@@ -20,9 +21,11 @@ async def main():
             for X in await get_chat(user_id):
                 await remove_chat(user_id, X)
             print(f"✅ {user_id} ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs")
-    await asyncio.gather(
-        loadPlugins(), install_user_id(), install_all_peer(), expired_userbot(), idle()
-    )
+    await loadPlugins()
+    await install_user_id()
+    await install_all_peer()
+    await expired_userbot()
+    await idle()
 
 
 if __name__ == "__main__":
