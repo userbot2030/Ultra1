dfdf8b63347fffc9fc12a8ffb64fc124b2460dce
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-07 21:31:18 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -1,4 +1,4 @@
-import logging
+import logging, importlib
 
 from pyrogram import Client
 from pyrogram.handlers import MessageHandler
@@ -29,6 +29,8 @@ class Bot(Client):
 
     async def start(self):
         await super().start()
+        for mod in loadModule():
+            importlib.reload(importlib.import_module(f"PyroUbot.modules.{mod}"))
         print(f"Robot ({self.me.id}) Started")
 
 
@@ -55,6 +57,9 @@ class Ubot(Client):
             self._ubot.append(self)
             self._get_my_id.append(self.me.id)
             self._translate[self.me.id] = {"negara": "id"}
+            for mod in loadModule():
+                importlib.reload(importlib.import_module(f"PyroUbot.modules.{mod}"))
+            await install_my_peer(self)
             print(f"Userbot ({self.me.id}) Started")
 
 
