1b4c1dee992d1cbc510ce3fd6d6fb28a2f4576c9
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-09 22:14:10 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -1,8 +1,6 @@
 from pyrogram import filters
-from pyrogram.enums import ChatType
 
 from PyroUbot import bot, ubot
-from PyroUbot.config import OWNER_ID
 
 ONLY_UBOT = filters.user()
 
@@ -11,6 +9,9 @@ async def install_user_id():
     for X in ubot._get_my_id:
         ONLY_UBOT.add(X)
 
+"""
+from pyrogram.enums import ChatType
+from PyroUbot.config import OWNER_ID
 
 async def install_my_peer(client):
     users = len(
@@ -35,3 +36,4 @@ async def install_all_peer():
         await install_my_peer(client)
         print(f"peer_id ({client.me.id}) telah diinstal ke get_my_peer")
     await bot.send_message(OWNER_ID, "✅ semua peer_id telah berhasil diinstal")
+"""
