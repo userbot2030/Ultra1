a5c52cd3b6f0c35bee84134ab66c79c34c746609
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-09 21:49:11 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -35,7 +35,6 @@ async def main():
 if __name__ == "__main__":
     if __name__ == "__main__":
         loop = asyncio.get_event_loop()
-        asyncio.run(main())
         try:
             loop.run_until_complete(main())
             loop.run_forever()
