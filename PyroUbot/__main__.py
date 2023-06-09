0cb4de2aaad9e23c31466dbc3bd61e29f8821d94
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-09 15:31:23 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -31,5 +31,5 @@ async def main():
 if __name__ == "__main__":
     try:
         asyncio.get_event_loop_policy().get_event_loop().run_until_complete(main())
-    except (asyncio.ConnectionCancel, asyncio.TimeoutError):
+    except (asyncio.CancelledError, asyncio.TimeoutError):
         os.execl(sys.executable, sys.executable, "-m", "PyroUbot")
