4e57011ab2fb5cad07fc6b9937a9a65a1cacdbe5
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-09 12:57:25 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -1,37 +1,34 @@
 from pyrogram import filters
-from pyrogram.enums import ChatType
+from pyrogram.types import ChatType
 
 from PyroUbot import bot, ubot
 from PyroUbot.config import OWNER_ID
+
 
 ONLY_UBOT = filters.user()
 
 
 async def install_user_id():
-    for X in ubot._get_my_id:
-        ONLY_UBOT.add(X)
+    for X in await ubot.get_me():
+        ONLY_UBOT.add(X.id)
 
 
-async def install_my_peer(self):
-    users = len(
-        [
-            dialog.chat.id
-            async for dialog in self.get_dialogs()
-            if dialog.chat.type == ChatType.PRIVATE
-        ]
-    )
-    group = len(
-        [
-            dialog.chat.id
-            async for dialog in self.get_dialogs()
-            if dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP)
-        ]
-    )
-    self._get_my_peer[self.me.id] = {"pm": users, "gc": group}
+async def install_my_peer(client):
+    users = len([
+        dialog.chat.id
+        async for dialog in client.iter_dialogs()
+        if dialog.chat.type == ChatType.PRIVATE
+    ])
+    group = len([
+        dialog.chat.id
+        async for dialog in client.iter_dialogs()
+        if dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP)
+    ])
+    client._get_my_peer[client.me.id] = {"pm": users, "gc": group}
 
 
 async def install_all_peer():
-    for self in ubot._ubot:
-        await install_my_peer(self)
-        print(f"peer_id ({self.me.id}) telah diinstal ke get_my_peer")
-    await bot.send_message(OWNER_ID, "✅ sᴇᴍᴜᴀ ᴘᴇᴇʀɪᴅ ᴛᴇʟᴀʜ ʙᴇʀʜᴀsɪʟ ᴅɪɪɴsᴛᴀʟʟ")
+    for client in ubot._ubot:
+        await install_my_peer(client)
+        print(f"peer_id ({client.me.id}) telah diinstal ke get_my_peer")
+    await bot.send_message(OWNER_ID, "✅ semua peer_id telah berhasil diinstal")
