f125d77589d83fe25b8c8c618b3fe5b031a9f5a1
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-09 20:23:13 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -1,27 +1,29 @@
 import asyncio
-import os
 
 from pyrogram.errors import RPCError
 from pyrogram.methods.utilities.idle import idle
 
-from PyroUbot import *
+from PyroUbot import bot, ubot, get_userbots, Ubot, remove_ubot, rm_all, rem_expired_date, get_chat, remove_chat, loadPlugins, install_user_id, expired_userbot
+
+
+async def start_client(client):
+    try:
+        await client.start()
+    except RPCError:
+        user_id = int(client["name"])
+        await remove_ubot(user_id)
+        await rm_all(user_id)
+        await rem_expired_date(user_id)
+        for chat_id in await get_chat(user_id):
+            await remove_chat(user_id, chat_id)
+        print(f"✅ {user_id} ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs")
 
 
 async def main():
-    await bot.start()
-    await ubot.start()
+    await asyncio.gather(start_client(bot), start_client(ubot))
     for _ubot in await get_userbots():
         ubot_ = Ubot(**_ubot)
-        try:
-            await ubot_.start()
-        except RPCError:
-            user_id = int(_ubot["name"])
-            await remove_ubot(user_id)
-            await rm_all(user_id)
-            await rem_expired_date(user_id)
-            for X in await get_chat(user_id):
-                await remove_chat(user_id, X)
-            print(f"✅ {user_id} ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs")
+        await start_client(ubot_)
     await loadPlugins()
     await install_user_id()
     await expired_userbot()
@@ -29,7 +31,5 @@ async def main():
 
 
 if __name__ == "__main__":
-    try:
-        asyncio.get_event_loop_policy().get_event_loop().run_until_complete(main())
-    except:
-        os.system(f"kill -9 {os.getpid()} && python3 -m PyroUbot")
+    asyncio.run(main())
+ 
