211225470edf0c41d5cb1f5d1d924660acc46d4f
XtomiX <XtomiX@users.noreply.github.com>
2023-06-10 16:40:39 +0000

    Auto Fix

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -25,7 +25,7 @@ async def main():
         loadPlugins(),
         install_user_id(),
         expired_userbot(),
-        install_all_peer(), 
+        install_all_peer(),
         idle(),
     )
 
