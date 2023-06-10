859d522519af7f61941b4334c91a7571b57919fa
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-10 13:10:58 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -23,8 +23,8 @@ async def main():
             print(f"✅ {user_id} ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs")
     await loadPlugins()
     await install_user_id()
-    await expired_userbot()
     await install_all_peer()
+    await expired_userbot()
     await idle()
 
 
