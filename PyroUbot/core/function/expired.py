039fee0d3dda03c6ea795301239f2b06ed634b8f
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-10 14:27:15 +0700

    Update expired.py

--- PyroUbot/core/function/expired.py
+++ PyroUbot/core/function/expired.py
@@ -18,24 +18,21 @@ async def expired_userbot():
         time = now.strftime("%d-%m-%Y")
         clock = now.strftime("%H:%M:%S")
         for X in ubot._ubot:
-            try:
-                exp = (await get_expired_date(X.me.id)).strftime("%d-%m-%Y")
-                if time == exp:
-                    for chat in await get_chat(X.me.id):
-                        await remove_chat(X.me.id, chat)
-                    await rm_all(X.me.id)
-                    await remove_ubot(X.me.id)
-                    await rem_expired_date(X.me.id)
-                    ubot._get_my_id.remove(X.me.id)
-                    ubot._ubot.remove(X)
-                    await X.log_out()
-                    expired_text = MSG.EXPIRED_MSG_BOT(X)
-                    expired_button = Button.expired_button_bot()
-                    await bot.send_message(
-                        LOGS_MAKER_UBOT,
-                        expired_text,
-                        reply_markup=InlineKeyboardMarkup(expired_button),
-                    )
-            except:
-                pass
+            exp = (await get_expired_date(X.me.id)).strftime("%d-%m-%Y")
+            if time == exp:
+                for chat in await get_chat(X.me.id):
+                    await remove_chat(X.me.id, chat)
+                await rm_all(X.me.id)
+                await remove_ubot(X.me.id)
+                await rem_expired_date(X.me.id)
+                ubot._get_my_id.remove(X.me.id)
+                ubot._ubot.remove(X)
+                await X.log_out()
+                expired_text = MSG.EXPIRED_MSG_BOT(X)
+                expired_button = Button.expired_button_bot()
+                await bot.send_message(
+                    LOGS_MAKER_UBOT,
+                    expired_text,
+                    reply_markup=InlineKeyboardMarkup(expired_button),
+                )
         await asyncio.sleep(300)
