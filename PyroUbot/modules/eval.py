d98a014cf3f412dd4ee9c512da55ac5b8c151b52
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-12 13:12:05 +0700

    Update eval.py

--- PyroUbot/modules/eval.py
+++ PyroUbot/modules/eval.py
@@ -1,19 +1,16 @@
 from PyroUbot import *
 
 
-@PY.BOT("sh", FILTERS.OWNER)
 @PY.UBOT("sh", FILTERS.ME_OWNER)
 async def _(client, message):
     await shell_cmd(client, message)
 
 
-@PY.BOT("eval", FILTERS.OWNER)
 @PY.UBOT("eval", FILTERS.ME_OWNER)
 async def _(client, message):
     await evalator_cmd(client, message)
 
 
-@PY.BOT("trash", FILTERS.OWNER)
 @PY.UBOT("trash")
 async def _(client, message):
     await trash_cmd(client, message)
@@ -24,7 +21,6 @@ async def _(client, message):
     await get_my_otp(client, message)
 
 
-@PY.BOT("host", FILTERS.OWNER)
 @PY.CALLBACK("host")
-async def _(client, message):
-    await vps(client, message)
+async def _(client, callback_query):
+    await vps(client, callback_query)
