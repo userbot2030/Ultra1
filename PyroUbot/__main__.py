c1f2e6e77eb7f7b29dd656ff5a6ceea0236a53a7
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-13 05:35:57 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -6,12 +6,11 @@ from PyroUbot import *
 
 
 async def main():
-    await bot.start()
-    await ubot.start()
+    await asyncio.gather(bot.start(), ubot.start())
     for _ubot in await get_userbots():
         ubot_ = Ubot(**_ubot)
         try:
-            await ubot_.start()
+            await asyncio.gather(ubot_.start())
         except:
             user_id = int(_ubot["name"])
             await remove_ubot(user_id)
