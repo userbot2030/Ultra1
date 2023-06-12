41154b56b11c5178bf2e4290018853e662abe161
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-12 07:43:11 +0700

    Update expired.py

--- PyroUbot/core/function/expired.py
+++ PyroUbot/core/function/expired.py
@@ -16,21 +16,24 @@ async def expired_userbot():
     while True:
         time = datetime.now(timezone("Asia/Jakarta")).strftime("%d-%m-%Y")
         for X in ubot._ubot:
-            exp = (await get_expired_date(X.me.id)).strftime("%d-%m-%Y")
-            if time == exp:
-                for chat in await get_chat(X.me.id):
-                    await remove_chat(X.me.id, chat)
-                await rm_all(X.me.id)
-                await remove_ubot(X.me.id)
-                await rem_expired_date(X.me.id)
-                ubot._get_my_id.remove(X.me.id)
-                ubot._ubot.remove(X)
-                await X.log_out()
-                expired_text = MSG.EXPIRED_MSG_BOT(X)
-                expired_button = Button.expired_button_bot()
-                await bot.send_message(
+            try:
+                exp = (await get_expired_date(X.me.id)).strftime("%d-%m-%Y")
+                if time == exp:
+                    for chat in await get_chat(X.me.id):
+                        await remove_chat(X.me.id, chat)
+                    await rm_all(X.me.id)
+                    await remove_ubot(X.me.id)
+                    await rem_expired_date(X.me.id)
+                    ubot._get_my_id.remove(X.me.id)
+                    ubot._ubot.remove(X)
+                    await X.log_out()
+                    expired_text = MSG.EXPIRED_MSG_BOT(X)
+                    expired_button = Button.expired_button_bot()
+                    await bot.send_message(
                     LOGS_MAKER_UBOT,
                     expired_text,
                     reply_markup=InlineKeyboardMarkup(expired_button),
                 )
+            except:
+                pass 
         await asyncio.sleep(3600)
