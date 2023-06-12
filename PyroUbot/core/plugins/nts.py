b0335c563eea5b449dc0042e0b9e35d83fe70e83
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-12 21:06:37 +0700

    Update nts.py

--- PyroUbot/core/plugins/nts.py
+++ PyroUbot/core/plugins/nts.py
@@ -32,7 +32,7 @@ async def get_cmd(client, message):
     if not note:
         return await message.reply(f"ᴄᴀᴛᴀᴛᴀɴ {note_name} ᴛɪᴅᴀᴋ ᴀᴅᴀ")
     note_id = await client.get_messages(client.me.id, note)
-    if note_id.text
+    if note_id.text:
         if "~>" in note_id.text:
             try:
                 x = await client.get_inline_bot_results(
