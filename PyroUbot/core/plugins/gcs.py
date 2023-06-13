e57f099e84c8c4cb4a98ce6e8e66df7b039747e7
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-13 13:01:20 +0700

    Update gcs.py

--- PyroUbot/core/plugins/gcs.py
+++ PyroUbot/core/plugins/gcs.py
@@ -37,7 +37,7 @@ async def broadcast_group_cmd(client, message):
                         await client.send_inline_bot_result(
                             dialog,
                             x.query_id,
-                            x.results[0].id,
+                            x.results[0].id
                         )
                     sent += 1
                 await asyncio.sleep(5)
