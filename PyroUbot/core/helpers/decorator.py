8e0d6593fdfe3536f3fd58e9a4de80c2e4beb377
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-14 20:36:07 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -3,13 +3,6 @@ from pyrogram.enums import ChatType
 
 from PyroUbot import OWNER_ID, bot, ubot
 
-ONLY_UBOT = filters.user()
-
-
-async def install_user_id():
-    for X in ubot._get_my_id:
-        ONLY_UBOT.add(X)
-
 
 async def install_my_peer(client):
     try:
