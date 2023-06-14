021bd3bb6b146ab97a80871ac4c605abcf28c84d
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-15 00:23:57 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -7,7 +7,7 @@ from pyromod import listen
 from PyroUbot.config import *
 
 logging.basicConfig(
-    level=logging.ERROR,
+    level=logging.INFO,
     format="[%(levelname)s] - %(name)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[logging.NullHandler()],
@@ -51,7 +51,7 @@ class Ubot(Client):
         self._ubot.append(self)
         self._get_my_id.append(self.me.id)
         self._translate[self.me.id] = {"negara": "id"}
-        print(f"[INFO] - ({self.me.id}) - Started")
+        print(f"[INFO] - ({self.me.id}) - [START]")
 
 
 bot = Bot(
