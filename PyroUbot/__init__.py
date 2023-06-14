2e8f553da9c571777b15f216205221873a91a85c
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-14 23:42:51 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -8,7 +8,7 @@ from pyromod import listen
 from PyroUbot.config import *
 
 logging.basicConfig(
-    level=logging.ERROR,
+    level=logging.INFO,
     format="%(filename)s:%(lineno)s %(levelname)s: %(message)s",
     datefmt="%m-%d %H:%M",
     handlers=[logging.StreamHandler()],
