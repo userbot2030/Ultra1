8f74de0885650c451b9c220c5d2ebd593741ad29
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-15 00:50:47 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -9,7 +9,7 @@ from PyroUbot.config import *
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
-    "[%(levelname)s] - %(name)s - %(message)s - %d-%b-%y %H:%M:%S"
+    "[%(levelname)s] - %(name)s - %(message)s"
 )
 handler = logging.StreamHandler()
 handler.setFormatter(formatter)
