718c84a2148ba797c14fc58fa74cbfdf4fd4bda5
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-14 19:25:18 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -6,15 +6,19 @@ from pyrogram.methods.utilities.idle import idle
 from PyroUbot import *
 
 
+"""
+            await asyncio.wait_for(ubot_.start(), timeout=5)
+        except asyncio.TimeoutError:
+            print(f"({user_id}) ᴛɪᴅᴀᴋ ʙɪsᴀ ᴍᴇʀᴇsᴘᴏɴ")
+"""
+
 async def main():
     await asyncio.gather(bot.start(), ubot.start())
     for _ubot in await get_userbots():
         user_id = int(_ubot["name"])
         ubot_ = Ubot(**_ubot)
         try:
-            await asyncio.wait_for(ubot_.start(), timeout=5)
-        except asyncio.TimeoutError:
-            print(f"({user_id}) ᴛɪᴅᴀᴋ ʙɪsᴀ ᴍᴇʀᴇsᴘᴏɴ")
+            await ubot_.start()
         except RPCError:
             await remove_ubot(user_id)
             await rm_all(user_id)
