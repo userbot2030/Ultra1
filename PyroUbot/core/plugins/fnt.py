ef5a9545b900c84a7bcd7596b68abefdac3d9a9b
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-14 12:50:32 +0700

    Update fnt.py

--- PyroUbot/core/plugins/fnt.py
+++ PyroUbot/core/plugins/fnt.py
@@ -7,27 +7,6 @@ from pyrogram.types import (InlineKeyboardButton, InlineQueryResultArticle,
 
 from PyroUbot import *
 
-font = {
-    "sᴍᴀʟʟᴄᴀᴘs": "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘϙʀsᴛᴜᴠᴡxʏᴢABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    "𝚃𝚈𝙿𝙴𝚆𝚁𝙸𝚃𝙴𝚁": "𝚊𝚋𝚌𝚍𝚎𝚏𝚐𝚑𝚒𝚓𝚔𝚕𝚖𝚗𝚘𝚙𝚚𝚛𝚜𝚝𝚞𝚟𝚠𝚡𝚢𝚣𝙰𝙱𝙲𝙳𝙴𝙵𝙶𝙷𝙸𝙹𝙺𝙻𝙼𝙽𝙾𝙿𝚀𝚁𝚂𝚃𝚄𝚅𝚆𝚇𝚈𝚉",
-    "𝕆𝕌𝕋𝕃𝕀ℕ𝔼": "𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏𝕐ℤ",
-    "𝒟ℰℛℐℋℒ": "𝒶𝒷𝒸𝒹𝑒𝒻𝑔𝒽𝒾𝒿𝓀𝓁𝓂𝓃𝑜𝓅𝓆𝓇𝓈𝓉𝓊𝓋𝓌𝓍𝓎𝓏𝒜ℬ𝒞𝒟ℰℱ𝒢ℋℐ𝒥𝒦ℒℳ𝒩𝒪𝒫𝒬ℛ𝒮𝒯𝒰𝒱𝒲𝒳𝒴𝒵",
-    "Ⓥ︎Ⓘ︎Ⓡ︎Ⓒ︎Ⓛ︎Ⓔ︎Ⓢ︎": "ⓐⓑⓒⓓⓔⓕⓖⓗⓘⓙⓚⓛⓜⓝⓞⓟⓠⓡⓢⓣⓤⓥⓦⓧⓨⓩⒶⒷⒸⒹⒺⒻⒼⒽⒾⒿⓀⓁⓂⓃⓄⓅⓆⓇⓈⓉⓊⓋⓌⓍⓎⓏ",
-    "𝗦𝗔𝗡𝗦": "𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭",
-    "ᵗⁱⁿʸ": "ᵃᵇᶜᵈᵉᶠᵍʰⁱʲᵏˡᵐⁿᵒᵖʳˢᵗᵘᵛʷˣʸᶻ",
-    "𝐒𝐄𝐑𝐈𝐅": "𝐚𝐛𝐜𝐝𝐞𝐟𝐠𝐡𝐢𝐣𝐤𝐥𝐦𝐧𝐨𝐩𝐪𝐫𝐬𝐭𝐮𝐯𝐰𝐱𝐲𝐳𝐀𝐁𝐂𝐃𝐄𝐅𝐆𝐇𝐈𝐉𝐊𝐋𝐌𝐍𝐎𝐏𝐑𝐒𝐓𝐔𝐕𝐖𝐗𝐘𝐙",
-}
-basic = string.ascii_letters
-
-
-def gen_font(text, new_font):
-    new_font = " ".join(new_font).split()
-    for q in text:
-        if q in basic:
-            new = new_font[basic.index(q)]
-            text = text.replace(q, new)
-    return text
-
 
 async def font_message(client, message):
     if message.reply_to_message:
@@ -49,10 +28,11 @@ async def font_message(client, message):
 
 async def font_inline(client, inline_query):
     get_id = int(inline_query.query.split(None, 1)[1])
-    buttons = InlineKeyboard(row_width=2)
+    buttons = InlineKeyboard(row_width=4)
     keyboard = []
-    for X in font:
-        keyboard.append(InlineKeyboardButton(X, callback_data=f"get {get_id} {X}"))
+    for X in query_fomts:
+        keyboard.append(
+          InlineKeyboardButton(X, callback_data=f"get {get_id} {X}"))
     buttons.add(*keyboard)
     await client.answer_inline_query(
         inline_query.id,
@@ -80,7 +60,7 @@ async def font_callback(client, callback_query):
             text = m.reply_to_message.text
         else:
             text = m.text.split(None, 1)[1]
-        get_new_font = gen_font(text, font[new])
+        get_new_font = gens_font(query_fomts[new], text)
         return await callback_query.edit_message_text(get_new_font)
     except Exception as error:
         return await callback_query.edit_message_text(f"<code>{error}</code>")
