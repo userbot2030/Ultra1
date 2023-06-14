4340921491bc87195be82aec962749e7199c277d
XtomiX <XtomiX@users.noreply.github.com>
2023-06-14 12:49:34 +0000

    Auto Fix

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -6,7 +6,6 @@ from pyrogram.methods.utilities.idle import idle
 from PyroUbot import *
 
 
-
 async def main():
     await asyncio.gather(bot.start(), ubot.start())
     for _ubot in await get_userbots():
@@ -24,7 +23,11 @@ async def main():
                 await remove_chat(user_id, X)
             print(f"✅ {user_id} ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs")
     await asyncio.gather(
-        loadPlugins(), install_user_id(), expired_userbot(), install_all_peer(), idle(),
+        loadPlugins(),
+        install_user_id(),
+        expired_userbot(),
+        install_all_peer(),
+        idle(),
     )
 
 
