9dd5d29b6da7648bd6f94955aa9a91fc8bf64654
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-14 21:58:08 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -1,20 +1,20 @@
 import asyncio
 
-from pyrogram import compose
 from pyrogram.errors import RPCError
+from pyrogram.methods.utilities.idle import idle
 
 from PyroUbot import *
 
 
 async def main():
-    await bot.start()
-    clients = []
-    clients.append(ubot)
+    await asyncio.gather(bot.start(), ubot.start())
     for _ubot in await get_userbots():
         user_id = int(_ubot["name"])
         ubot_ = Ubot(**_ubot)
         try:
-            clients.append(ubot_)
+            await asyncio.wait_for(ubot_.start(), timeout=5)
+        except asyncio.TimeoutError:
+            print(f"({user_id}) ᴛɪᴅᴀᴋ ʙɪsᴀ ᴍᴇʀᴇsᴘᴏɴ")
         except RPCError:
             await remove_ubot(user_id)
             await rm_all(user_id)
@@ -23,10 +23,10 @@ async def main():
                 await remove_chat(user_id, X)
             print(f"✅ {user_id} ʙᴇʀʜᴀsɪʟ ᴅɪʜᴀᴘᴜs")
     await asyncio.gather(
-        compose(clients),
         loadPlugins(),
         expired_userbot(),
         install_all_peer(),
+        idle(),
     )
 
 
