41b649169b9c4d1dcf7a1d2d8c6fe7980f0835da
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-15 20:29:44 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -10,8 +10,9 @@ from PyroUbot.config import *
 
 
 def handle_connection_lost(record):
-    if "Connection" in record.getMessage():
-        os.system(f"kill -9 {os.getpid()} && rm -rf *.session* && python3 -m PyroUbot")
+    for x in ["Connection", "timed"]:
+        if X in record.getMessage():
+            os.system(f"kill -9 {os.getpid()} && rm -rf *.session* && python3 -m PyroUbot")
 
 
 logging.basicConfig(
