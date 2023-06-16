90e73f499bc5e16027d85ecf645f44409c8aa883
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-16 09:13:00 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -1,24 +1,25 @@
+import asyncio
 from pyrogram.enums import ChatType
-
 from PyroUbot import OWNER_ID, bot, ubot
-
 
 async def install_my_peer(client):
     try:
         users = []
-        group = []
+        groups = []
         async for dialog in client.get_dialogs():
             if dialog.chat.type == ChatType.PRIVATE:
                 users.append(dialog.chat.id)
-            if dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
-                group.append(dialog.chat.id)
-            client._get_my_peer[client.me.id] = {"pm": users, "gc": group}
+            elif dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
+                groups.append(dialog.chat.id)
+        client._get_my_peer[client.me.id] = {"pm": users, "gc": groups}
     except Exception as error:
         print(f"Error occurred: {error}")
 
-
 async def install_all_peer():
+    tasks = []
     for client in ubot._ubot:
-        await install_my_peer(client)
+        tasks.append(install_my_peer(client))
         print(f"[𝗜𝗡𝗙𝗢] - ({client.me.id}) - 𝗜𝗡𝗦𝗧𝗔𝗟𝗟 𝗨𝗕𝗢𝗧._𝗚𝗘𝗧_𝗠𝗬_𝗣𝗘𝗘𝗥")
+    await asyncio.gather(*tasks)
     await bot.send_message(OWNER_ID, "✅ sᴇᴍᴜᴀ ᴘᴇᴇʀ_ɪᴅ ʙᴇʀʜᴀsɪʟ ᴅɪɪɴsᴛᴀʟʟ")
+
