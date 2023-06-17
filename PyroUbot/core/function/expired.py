af980223c84408a0f607dd885c49ce371eef43d7
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-17 07:38:04 +0700

    Update expired.py

--- PyroUbot/core/function/expired.py
+++ PyroUbot/core/function/expired.py
@@ -12,28 +12,34 @@ from PyroUbot.core.database import (get_chat, get_expired_date,
 from PyroUbot.core.helpers import MSG, Button
 
 
-async def expired_userbot():
-    while True:
+async def expired_userbot(X):
+    try:
         time = datetime.now(timezone("Asia/Jakarta")).strftime("%d-%m-%Y")
+        exp = (await get_expired_date(X.me.id)).strftime("%d-%m-%Y")
+        if time == exp:
+            for chat in await get_chat(X.me.id):
+                await remove_chat(X.me.id, chat)
+            await rm_all(X.me.id)
+            await remove_ubot(X.me.id)
+            await rem_expired_date(X.me.id)
+            ubot._get_my_id.remove(X.me.id)
+            ubot._ubot.remove(X)
+            await X.log_out()
+            expired_text = MSG.EXPIRED_MSG_BOT(X)
+            expired_button = Button.expired_button_bot()
+            await bot.send_message(
+                LOGS_MAKER_UBOT,
+                expired_text,
+                reply_markup=InlineKeyboardMarkup(expired_button),
+            )
+    except Exception as e:
+        print(f"Error: {str(e)}")
+
+
+async def check_expired_userbots():
+    while True:
+        tasks = []
         for X in ubot._ubot:
-            try:
-                exp = (await get_expired_date(X.me.id)).strftime("%d-%m-%Y")
-                if time == exp:
-                    for chat in await get_chat(X.me.id):
-                        await remove_chat(X.me.id, chat)
-                    await rm_all(X.me.id)
-                    await remove_ubot(X.me.id)
-                    await rem_expired_date(X.me.id)
-                    ubot._get_my_id.remove(X.me.id)
-                    ubot._ubot.remove(X)
-                    await X.log_out()
-                    expired_text = MSG.EXPIRED_MSG_BOT(X)
-                    expired_button = Button.expired_button_bot()
-                    await bot.send_message(
-                        LOGS_MAKER_UBOT,
-                        expired_text,
-                        reply_markup=InlineKeyboardMarkup(expired_button),
-                    )
-            except:
-                pass
-        await asyncio.sleep(21600)
+            tasks.append(expired_userbot(X))
+        await asyncio.gather(*tasks)
+        await asyncio.sleep{3600)
