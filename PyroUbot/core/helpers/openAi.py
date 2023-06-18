2dbf6c37a29011ac2ec02b3e256dc5acdbbcd6d0
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-18 08:49:59 +0700

    Update openAi.py

--- PyroUbot/core/helpers/openAi.py
+++ PyroUbot/core/helpers/openAi.py
@@ -1,42 +1,40 @@
 import asyncio
 import random
-
 import openai
-
 from PyroUbot import OPENAI_KEY
 
 openai.api_key = random.choice(OPENAI_KEY)
 
-
 class OpenAi:
+    @staticmethod
     async def ChatGPT(question):
-        response = await asyncio.get_event_loop().run_in_executor(
-            None,
-            lambda: openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=[{"role": "user", "content": question}],
-                stop=None,
-                n=1,
-                user="arc",
-            ),
+        response = await asyncio.to_thread(
+            openai.ChatCompletion.create,
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": question}],
+            stop=None,
+            n=1,
+            user="arc",
         )
         return response.choices[0].message["content"].strip()
 
+    @staticmethod
     async def ImageDalle(question):
-        response = await asyncio.get_event_loop().run_in_executor(
-            None,
-            lambda: openai.Image.create(
-                prompt=question,
-                n=1,
-                size="1024x1024",
-                user="arc",
-            ),
+        response = await asyncio.to_thread(
+            openai.Image.create,
+            prompt=question,
+            n=1,
+            size="1024x1024",
+            user="arc",
         )
         return response["data"][0]["url"]
 
+    @staticmethod
     async def SpeechToText(file):
         audio_file = open(file, "rb")
-        response = await asyncio.get_event_loop().run_in_executor(
-            None, lambda: openai.Audio.transcribe("whisper-1", audio_file)
+        response = await asyncio.to_thread(
+            openai.Audio.transcribe,
+            "whisper-1",
+            audio_file
         )
         return response["text"]
