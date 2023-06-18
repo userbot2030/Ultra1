52bbd5ef14d5903eda5e0ae26069e2262359ac96
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-18 15:40:03 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -26,9 +26,8 @@ async def start_ubot(user_id, ubot):
 async def main():
     tasks = [
         asyncio.create_task(
-            start_ubot(int(_ubot["name"]), _ubot) for _ubot in await get_userbots()
-        )
-    ]
+            start_ubot(int(_ubot["name"]), _ubot)) for _ubot in await get_userbots()
+   ]
     await asyncio.gather(bot.start(), ubot.start())
     await asyncio.gather(*tasks)
     await asyncio.gather(
