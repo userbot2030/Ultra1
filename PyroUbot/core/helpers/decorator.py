859700d700608a32cfcc2879487a4d0808385542
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-18 12:34:41 +0700

    Update decorator.py

--- PyroUbot/core/helpers/decorator.py
+++ PyroUbot/core/helpers/decorator.py
@@ -6,17 +6,17 @@ from PyroUbot import OWNER_ID, bot, ubot
 
 
 async def install_my_peer(client):
-    try:
-        users = []
-        groups = []
-        async for dialog in client.get_dialogs():
+    users = []
+    groups = []
+    async for dialog in client.get_dialogs():
+        try:
             if dialog.chat.type == ChatType.PRIVATE:
                 users.append(dialog.chat.id)
             elif dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
                 groups.append(dialog.chat.id)
             client._get_my_peer[client.me.id] = {"pm": users, "gc": groups}
-    except Exception as error:
-        print(f"Error occurred: {error}")
+        except Exception as error:
+            print(f"Error occurred: {error}")
 
 
 async def install_all_peer():
