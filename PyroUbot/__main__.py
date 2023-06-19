fe1a03bb1a690c0c1a28f6d1e3810be482894a4a
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-19 13:04:38 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -3,7 +3,7 @@ import asyncio
 from pyrogram import idle
 from pyrogram.errors import RPCError
 
-from PyroUbot import (Ubot, bot, get_chat, get_userbots, loadPlugins, expiredUserbots, installPeer
+from PyroUbot import (Ubot, bot, get_chat, get_userbots, loadPlugins, expiredUserbots, installPeer,
                       rem_expired_date, remove_chat, remove_ubot, rm_all, ubot)
 
 
