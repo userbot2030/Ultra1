a113915d74764cfde4a397d1d18ede8f0cf0ab48
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-19 12:49:09 +0700

    Update expired.py

--- PyroUbot/core/function/expired.py
+++ PyroUbot/core/function/expired.py
@@ -32,6 +32,7 @@ async def expired_userbot(X):
                 expired_text,
                 reply_markup=InlineKeyboardMarkup(expired_button),
             )
+        print("expired userbot telah di periksa")
     except Exception as e:
         print(f"Error: {str(e)}")
 
