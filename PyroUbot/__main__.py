f37c567e7b3930be1f95bd125bdda156beec5a73
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-20 09:36:39 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -1,33 +1,43 @@
 import asyncio
 
-from pyrogram import idle
 from pyrogram.errors import RPCError
+from pyrogram import idle
 
-from PyroUbot import (Ubot, bot, expiredUserbots, get_chat, get_userbots,
-                      installPeer, loadPlugins, rem_expired_date, remove_chat,
-                      remove_ubot, rm_all, ubot)
+from PyroUbot import (Ubot, bot, check_expired_userbots, get_chat,
+                      get_userbots, install_all_peer, loadPlugins,
+                      rem_expired_date, remove_chat, remove_ubot, rm_all, ubot)
+
+
+async def start_ubot(user_id, _ubot):
+    ubot_ = Ubot(**_ubot)
+    try:
+        await asyncio.wait_for(ubot_.start(), timeout=60)
+    except asyncio.TimeoutError:
+        print(f"[𝗜𝗡𝗙𝗢] - ({user_id}) 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
+    except RPCError:
+        await remove_ubot(user_id)
+        await rm_all(user_id)
+        await rem_expired_date(user_id)
+        for X in await get_chat(user_id):
+            await remove_chat(user_id, X)
+        print(f"✅ {user_id} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦")
 
 
 async def main():
-    await bot.start()
-    await ubot.start()
-    for _ubot in await get_userbots():
-        user_id = int(_ubot["name"])
-        ubot_ = Ubot(**_ubot)
-        try:
-            await asyncio.wait_for(ubot_.start(), timeout=30)
-        except asyncio.TimeoutError:
-            print(f"[𝗜𝗡𝗙𝗢] - ({user_id}) 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
-        except RPCError:
-            await remove_ubot(user_id)
-            await rm_all(user_id)
-            await rem_expired_date(user_id)
-            for X in await get_chat(user_id):
-                await remove_chat(user_id, X)
-            print(f"✅ {user_id} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦")
-    await asyncio.gather(loadPlugins(), expiredUserbots(), installPeer(), idle())
+    tasks = [
+        asyncio.create_task(start_ubot(int(_ubot["name"]), _ubot))
+        for _ubot in await get_userbots()
+    ]
+    await asyncio.gather(*tasks, ubot.start(), bot.start())
+    await asyncio.gather(
+        loadPlugins(),
+        install_all_peer(),
+        check_expired_userbots(),
+        idle(),
+    )
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop_policy().get_event_loop()
     loop.run_until_complete(main())
+    loop.run_forever()
