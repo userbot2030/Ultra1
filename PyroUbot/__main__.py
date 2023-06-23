5320a500c06cb22f2a0c9a9537a7eeb043967a52
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-23 20:21:50 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -50,7 +50,7 @@ async def main():
 
 async def installHelpers():
     await asyncio.sleep(15)
-    await asyncio.gather(loadPlugins(), expiredUserbots())
+    await asyncio.gather(loadPlugins(), installPeer(), expiredUserbots())
 
 
 if __name__ == "__main__":
