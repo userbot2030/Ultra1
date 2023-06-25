3e15450f6e8d740f53a78fc937339846cf0121f6
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-25 23:36:57 +0700

    Update str.py

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -1,4 +1,4 @@
-import asyncio
+import asyncio, time
 from datetime import datetime
 from gc import get_objects
 
@@ -31,7 +31,7 @@ async def send_msg_to_owner(client, message):
 
 async def ping_cmd(client, message):
     ub_uptime = await get_uptime(client.me.id)
-    uptime = await get_time((ub_uptime - start_time))
+    uptime = await get_time((time.time() - ub_uptime))
     start = datetime.now()
     await client.invoke(Ping(ping_id=0))
     end = datetime.now()
