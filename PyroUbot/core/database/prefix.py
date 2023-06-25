55bb4ec09473ba116ac87347ab70ff13d2e4b291
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-25 10:02:39 +0700

    Update prefix.py

--- PyroUbot/core/database/prefix.py
+++ PyroUbot/core/database/prefix.py
@@ -3,19 +3,19 @@ from PyroUbot.core.database import mongo_client
 prefixes = mongo_client["PyroUbot"]["prefixes"]
 
 
-def get_pref(user_id):
-    user = prefixes.users.find_one({"_id": user_id})
+async def get_pref(user_id):
+    user = await prefixes.users.find_one({"_id": user_id})
     if user:
         return user.get("prefix")
     else:
         return None
 
 
-def set_pref(user_id, expire_date):
-    prefixes.users.update_one(
+async def set_pref(user_id, expire_date):
+    await prefixes.users.update_one(
         {"_id": user_id}, {"$set": {"prefix": expire_date}}, upsert=True
     )
 
 
-def rem__pref(user_id):
-    prefixes.users.update_one({"_id": user_id}, {"$unset": {"prefix": ""}}, upsert=True)
+async def rem__pref(user_id):
+    await prefixes.users.update_one({"_id": user_id}, {"$unset": {"prefix": ""}}, upsert=True)
