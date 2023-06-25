1d64c4481e8c416398e72dc6432311511fcebf9f
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-25 15:54:16 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -55,15 +55,10 @@ class PY:
         return wrapper
 
 
-def ubot_prefix(ub_id):
-    for x in ubot._ubot:
-        if ub_id == x.me.id:
-            return x._prefix[x.me.id]["ub"]
-
 
 def TES(command, filter=FILTERS.OWNER):
     def wrapper(func):
-        @ubot.on_message(filters.command(command, "") & filter)
+        @ubot.on_message(filters.command(command, lambda c, m: ubot_prefix(c.me.id) & filter)
         async def wrapped_func(client, message):
             await func(client, message)
 
