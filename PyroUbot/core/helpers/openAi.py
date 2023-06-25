8806f062c3202098f622f4b7988c4189d168a8d8
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-25 22:03:06 +0700

    Update openAi.py

--- PyroUbot/core/helpers/openAi.py
+++ PyroUbot/core/helpers/openAi.py
@@ -15,10 +15,8 @@ class OpenAi:
             openai.ChatCompletion.create,
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": question}],
-            temperature=0.7,
-            stream=True,
         )
-        return response
+        return response.choices[0].message["content"].strip()
 
     @staticmethod
     async def ImageDalle(question):
