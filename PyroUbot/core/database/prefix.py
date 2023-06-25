2b97b7929a600006c3b30344ff3d0d5abc757ee2
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-25 10:00:51 +0700

    Update prefix.py

--- PyroUbot/core/database/prefix.py
+++ PyroUbot/core/database/prefix.py
@@ -6,18 +6,18 @@ prefixes = mongo_client["PyroUbot"]["prefixes"]
 def get_pref(user_id):
     user = prefixes.users.find_one({"_id": user_id})
     if user:
-        return user.get("expire_date")
+        return user.get("prefix")
     else:
         return None
 
 
 def set_pref(user_id, expire_date):
     prefixes.users.update_one(
-        {"_id": user_id}, {"$set": {"expire_date": expire_date}}, upsert=True
+        {"_id": user_id}, {"$set": {"prefix": expire_date}}, upsert=True
     )
 
 
 def rem__pref(user_id):
     prefixes.users.update_one(
-        {"_id": user_id}, {"$unset": {"expire_date": ""}}, upsert=True
+        {"_id": user_id}, {"$unset": {"prefix": ""}}, upsert=True
     )
