b869404e95d9b32377096e995b51a93646efa716
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-25 15:55:24 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -62,6 +62,11 @@ class Ubot(Client):
 
         return decorator
 
+    def ubot_prefix(self):
+        for x in self._ubot:
+            if self.me.id == x.me.id:
+                return x._prefix[x.me.id]["ub"]
+
     async def start(self):
         await super().start()
         handler = await get_pref(self.me.id)
