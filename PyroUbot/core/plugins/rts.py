2e312354aa3a57ca1bc783c50a73bff6541c78b7
XtomiX <XtomiX@users.noreply.github.com>
2023-06-25 10:02:22 +0000

    Auto Fix

--- PyroUbot/core/plugins/rts.py
+++ PyroUbot/core/plugins/rts.py
@@ -74,13 +74,11 @@ async def restart_cmd(client, message):
                         UB = Ubot(**_ubot_)
                         await UB.start()
                         for mod in loadModule():
-                            importlib.reload(importlib.import_module(f"PyroUbot.modules.{mod}"))
+                            importlib.reload(
+                                importlib.import_module(f"PyroUbot.modules.{mod}")
+                            )
                         return await msg.edit(
                             f"<b>✅ ʀᴇsᴛᴀʀᴛ ʙᴇʀʜᴀsɪʟ ᴅɪʟᴀᴋᴜᴋᴀɴ {UB.me.first_name} {UB.me.last_name or ''} | {UB.me.id}</b>"
                         )
                     except Exception as error:
                         return await msg.edit(f"<b>{error}</b>")
-
-
-
-
