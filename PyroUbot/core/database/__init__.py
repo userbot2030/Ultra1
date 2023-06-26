88d72ab42d24f22a6a850920c6131d49707f8017
XtomiX <XtomiX@users.noreply.github.com>
2023-06-26 14:11:00 +0000

    Auto Fix

--- PyroUbot/core/database/__init__.py
+++ PyroUbot/core/database/__init__.py
@@ -8,8 +8,8 @@ mongodb = mongo_client.pyro_ubot
 from PyroUbot.core.database.expired import *
 from PyroUbot.core.database.get_uptime import *
 from PyroUbot.core.database.notes import *
+from PyroUbot.core.database.prefix import *
 from PyroUbot.core.database.premium import *
 from PyroUbot.core.database.reseller import *
 from PyroUbot.core.database.saved import *
 from PyroUbot.core.database.userbot import *
-from PyroUbot.core.database.prefix import *
