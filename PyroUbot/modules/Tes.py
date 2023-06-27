5955e8a169170663ef7d02b5c5df72b4193b065b
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-27 12:45:46 +0700

    Update and rename Tes to Tes.py

--- PyroUbot/modules/Tes.py
+++ PyroUbot/modules/Tes.py
@@ -4,3 +4,8 @@ from .. import *
 @CMD("tes")
 async def _(client, message):
     await message.reply("good")
+
+
+@CMD("prefix")
+async def _(client, message):
+    async def setprefix(client, message)
