0d86d3b85d140765ad6383366a2d61efacf1457d
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-27 08:59:44 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -61,7 +61,6 @@ def ubot_prefix(user_id):
 
 def CMD(command, filter=FILTERS.ME_OWNER):
     def wrapper(func):
-        @ubot.on_message(filters.command(command) & filter)
         async def wrapped_func(client, message):
             prefix = ubot_prefix(client.me.id)
             if message.text.startswith(prefix + command):
