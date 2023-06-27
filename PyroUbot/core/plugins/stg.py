f957cd3f863b612379a8f30d5280200475f25e30
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-27 15:09:35 +0700

    Update stg.py

--- PyroUbot/core/plugins/stg.py
+++ PyroUbot/core/plugins/stg.py
@@ -6,14 +6,15 @@ async def setprefix(client, message):
     if len(message.command) < 2:
         return await Tm.edit(f"<code>{message.text} sɪᴍʙᴏʟ ᴘʀᴇғɪx</code>")
     else:
-        set = await set_pref(client.me.id, message.command[1])
-        if set:
+        try:
+            await set_pref(client.me.id, message.command[1])
             ubot._prefix[client.me.id] = {"prefix": message.command[1]}
             return await Tm.edit(
                 f"<b>ᴘʀᴇғɪx ʙᴇʀʜᴀsɪʟ ᴅɪ sᴇᴛᴛɪɴɢ ᴋᴇ {message.command[1]}"
             )
-        else:
-            return await Tm.edit("ᴛᴇʀJᴀᴅɪ ᴋᴇsᴀʟᴀʜᴀɴ ʏᴀɴɢ ᴛɪᴅᴀᴋ ᴅɪᴋᴇᴛᴀʜᴜɪ")
+        except Exception as error:
+            await Tm.edit(error)
+        
 
 
 async def coba(client, message):
