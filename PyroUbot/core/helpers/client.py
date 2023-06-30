e6353259e7ff838e5ead0a765768758c38b36389
XtomiX <XtomiX@users.noreply.github.com>
2023-06-30 12:37:24 +0000

    Auto Fix

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -119,6 +119,7 @@ def ubot_prefix(user_id):
 def CMD(command, filter=FILTERS.ME_OWNER):
     def wrapper(func):
         for x in ubot._ubot:
+
             @ubot.on_message(filters.command(command, ubot_prefix(x.me.id)) & filter)
             async def wrapped_func(client, message):
                 await func(client, message)
