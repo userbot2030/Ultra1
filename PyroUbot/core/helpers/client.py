a5ed1b26a56039cf67dde9f59b64adce379a20a9
XtomiX <XtomiX@users.noreply.github.com>
2023-06-30 08:45:49 +0000

    Auto Fix

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -67,9 +67,7 @@ def CMD(command, filter=FILTERS.ME_OWNER):
     prefix_handler = PrefixHandler(ubot)
 
     def wrapper(func):
-        @ubot.on_message(
-            filters.command(command, prefix_handler.get_prefix) & filter
-        )
+        @ubot.on_message(filters.command(command, prefix_handler.get_prefix) & filter)
         async def wrapped_func(client, message):
             await func(client, message)
 
