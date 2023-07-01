883a3855c28978c57e3acb302e9f329e38c72444
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-01 23:18:08 +0700

    Create Tes.py

---
+++ PyroUbot/modules/Tes.py
@@ -0,0 +1,11 @@
+from PyroUbot import *
+
+
+@CMD("prefix")
+async def _(client, message):
+    await setprefix(client, message)
+
+
+@CMD("tes")
+async def _(client, message):
+    await coba(client, message)
