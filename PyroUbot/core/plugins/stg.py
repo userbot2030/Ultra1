a58cdd205f946d481e396313ee2ad618b7a1b025
XtomiX <XtomiX@users.noreply.github.com>
2023-07-02 01:35:24 +0000

    Auto Fix

--- PyroUbot/core/plugins/stg.py
+++ PyroUbot/core/plugins/stg.py
@@ -9,9 +9,7 @@ async def setprefix(client, message):
         try:
             await set_pref(client.me.id, message.command[1])
             ubot.set_prefix(client.me.id, message.command[1])
-            return await Tm.edit(
-                f"<b>✅ ᴘʀᴇғɪx ᴛᴇʟᴀʜ ᴅɪᴜʙᴀʜ ᴋᴇ: {message.command[1]}"
-            )
+            return await Tm.edit(f"<b>✅ ᴘʀᴇғɪx ᴛᴇʟᴀʜ ᴅɪᴜʙᴀʜ ᴋᴇ: {message.command[1]}")
         except Exception as error:
             await Tm.edit(error)
 
