5b4351e85172fe91f8e2a3ed8cdfc41e2f246694
XtomiX <XtomiX@users.noreply.github.com>
2023-07-02 07:14:03 +0000

    Auto Fix

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -72,9 +72,7 @@ def filters_command(
         text = message.text or message.caption
         message.command = None
         prefixes = [
-            ubot.get_prefix[client.me.id]
-            for x in ubot._ubot
-            if client.me.id == x.me.id
+            ubot.get_prefix[client.me.id] for x in ubot._ubot if client.me.id == x.me.id
         ]
 
         if not text:
