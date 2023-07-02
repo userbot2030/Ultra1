87604ba5eb70a10aafec69d7fad16ab77c77f3c0
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-02 12:05:47 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -55,16 +55,17 @@ class PY:
         return wrapper
 
 
-def prefix_filter(_, __, m):
+def prefix_filter(_, __, m: Message):
     return ubot.get_prefix(bool(m.from_user.id))
 
 
-get_user_id = filters.create(prefix_filter)
+def get_user_id():
+    return filters.create(prefix_filter)
 
 
 def CMD(command, filter=FILTERS.ME):
     def wrapper(func):
-        @ubot.on_message(filters.command(command) & filter & get_user_id)
+        @ubot.on_message(filters.command(command, get_user_id()) & filter)
         async def wrapped_func(client, message):
             await func(client, message)
 
