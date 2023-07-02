a11b18cf4e442697ba70de8c4888297ad369196e
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-02 13:03:20 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -1,3 +1,11 @@
+
+import inspect
+import re
+from typing import Callable, Union, List, Pattern
+
+import pyrogram
+from pyrogram import enums
+from pyrogram.types import Message, CallbackQuery, InlineQuery, InlineKeyboardMarkup, ReplyKeyboardMarkup, Update
 from pyrogram import filters
 
 from PyroUbot import *
@@ -55,14 +63,64 @@ class PY:
         return wrapper
 
 
+def filtras_command(commands: Union[str, List[str]], case_sensitive: bool = False):
+    command_re = re.compile(r"([\"'])(.*?)(?<!\\)\1|(\S+)")
+
+    async def func(flt, client: pyrogram.Client, message: Message):
+        username = client.me.username or ""
+        text = message.text or message.caption
+        message.command = None
+        prefixes = list(ubot.get_prefix(message.from_user.id))
+
+        if not text:
+            return False
+
+        for prefix in prefixes:
+            if not text.startswith(prefix):
+                continue
+
+            without_prefix = text[len(prefix):]
+
+            for cmd in flt.commands:
+                if not re.match(rf"^(?:{cmd}(?:@?{username})?)(?:\s|$)", without_prefix,
+                                flags=re.IGNORECASE if not flt.case_sensitive else 0):
+                    continue
+
+                without_command = re.sub(rf"{cmd}(?:@?{username})?\s?", "", without_prefix, count=1,
+                                         flags=re.IGNORECASE if not flt.case_sensitive else 0)
+
+                # match.groups are 1-indexed, group(1) is the quote, group(2) is the text
+                # between the quotes, group(3) is unquoted, whitespace-split text
+
+                # Remove the escape character from the arguments
+                message.command = [cmd] + [
+                    re.sub(r"\\([\"'])", r"\1", m.group(2) or m.group(3) or "")
+                    for m in command_re.finditer(without_command)
+                ]
+
+                return True
+
+        return False
+
+    commands = commands if isinstance(commands, list) else [commands]
+    commands = {c if case_sensitive else c.lower() for c in commands}
+
+    prefixes = [] if prefixes is None else prefixes
+    prefixes = prefixes if isinstance(prefixes, list) else [prefixes]
+    prefixes = set(prefixes) if prefixes else {""}
+
+    return filters.create(
+        func,
+        "CommandFilter",
+        commands=commands,
+        prefixes=prefixes,
+        case_sensitive=case_sensitive
+    )
+
+
 def CMD(command, filter=FILTERS.ME):
     def wrapper(func):
-        @ubot.on_message(
-            filters.command(
-                command, lambda _, __, message: ubot.get_prefix(message.from_user.id)
-            )
-            & filter
-        )
+        @ubot.on_message(filtras_command(command) & filter)
         async def wrapped_func(client, message):
             await func(client, message)
 
