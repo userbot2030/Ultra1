09f51bffec4f771d460b3c48bf4ce5c89e63f241
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-03 11:07:11 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -4,7 +4,7 @@ API_ID = int(os.getenv("API_ID", "23944801"))
 
 API_HASH = os.getenv("API_HASH", "b25e021922b0fdfc03150f23b5765001")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "5915868229:AAECSBinHxOYXVQ9a-v7zGgpPXBXeHcPPGw")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6207982225:AAFZlhW_jeUQGhL_-cwPHy_wYt8Q1Ft5AQc")
 
 OWNER_ID = int(os.getenv("OWNER_ID", "1964437366"))
 
