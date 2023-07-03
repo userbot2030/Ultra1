9c00d8ec72bf289e3ac0569936cf0cf08e98d75a
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-03 08:49:00 +0700

    Update Tes.py

--- PyroUbot/modules/Tes.py
+++ PyroUbot/modules/Tes.py
@@ -1,11 +1,13 @@
 from PyroUbot import *
 
 
-@CMD("prefix")
+# @CMD("prefix")
+@ubot.on_message(filters.text & command_filter("prefix"))
 async def _(client, message):
     await setprefix(client, message)
 
 
-@CMD("tes")
+# @CMD("tes")
+@ubot.on_message(filters.text & command_filter("tes))
 async def _(client, message):
     await coba(client, message)
