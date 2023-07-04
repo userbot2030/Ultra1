5fa39a30b1b199515204a13f128797717e765405
XtomiX <XtomiX@users.noreply.github.com>
2023-07-04 11:45:24 +0000

    Auto Fix

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -23,8 +23,7 @@ async def send_msg_to_owner(client, message):
                 ),
             ][
                 InlineKeyboardButton(
-                    "sᴀᴛᴜ ʙᴜʟᴀɴ",
-                    callback_data=f"success {message.from_user.id} 1"
+                    "sᴀᴛᴜ ʙᴜʟᴀɴ", callback_data=f"success {message.from_user.id} 1"
                 )
             ],
         ]
