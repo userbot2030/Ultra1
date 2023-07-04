297147155fff2e754cdaca24b129b9311e779ab7
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-04 20:38:58 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -79,7 +79,7 @@ class Ubot(Client):
                 )
                 if matched_prefix:
                     command = command[len(matched_prefix) :].strip()
-                    if command.split()[0] in cmd:
+                    if command and next((c for c in cmd), None) in command.split()[0]:
                         message.command = command.split()
                         return True
             return False
