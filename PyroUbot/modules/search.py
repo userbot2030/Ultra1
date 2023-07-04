13111db332cfa6cf1e0bf7bd543cae8bd57b3f9c
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-04 07:57:54 +0700

    Update search.py

--- PyroUbot/modules/search.py
+++ PyroUbot/modules/search.py
@@ -1,13 +1,13 @@
 from PyroUbot import *
 
 __MODULE__ = "search"
-__HELP__ = f"""
+__HELP__ = """
 <b>『 ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ sᴇᴀʀᴄʜ 』</b>
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}bing</code> or <code>{PREFIX[0]}pic</code> [ǫᴜᴇʀʏ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}bing</code> or <code>{PREFIX[0]}pic</code> [ǫᴜᴇʀʏ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴᴄᴀʀɪ ᴘʜᴏᴛᴏ ʀᴀɴᴅᴏᴍ ᴅᴀʀɪ ɢᴏᴏɢʟᴇ
 
-  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{PREFIX[0]}gif</code> [ǫᴜᴇʀʏ]
+  <b>• ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}gif</code> [ǫᴜᴇʀʏ]
   <b>• ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴᴄᴀʀɪ ɢɪꜰᴛ/ᴀɴɪᴍᴀᴛɪᴏɴ ʀᴀɴᴅᴏᴍ ᴅᴀʀɪ ɢᴏᴏɢʟᴇ
 """
 
