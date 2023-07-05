a284f629a2523c309e5a6896aaf3b3c608589e2b
XtomiX <XtomiX@users.noreply.github.com>
2023-07-05 00:26:36 +0000

    Auto Fix

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -77,7 +77,9 @@ class Ubot(Client):
                 matched_prefix = next((p for p in prefix if text.startswith(p)), None)
                 if matched_prefix:
                     command = text[len(matched_prefix) :].strip()
-                    if command and (command.split()[0] in cmd or command.split()[0] == cmd):
+                    if command and (
+                        command.split()[0] in cmd or command.split()[0] == cmd
+                    ):
                         message.command = command.split()
                         return True
             return False
