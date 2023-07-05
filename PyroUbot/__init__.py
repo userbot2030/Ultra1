85409091c51a05106dc20aefa260f9ca3ff467c1
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-05 08:33:12 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -77,10 +77,9 @@ class Ubot(Client):
                 matched_prefix = next((p for p in prefix if text.startswith(p)), None)
                 if matched_prefix:
                     command = text[len(matched_prefix) :].strip()
-                    if command:
-                        if command.split()[0] == cmd:
-                            message.command = command.split()
-                            return True
+                    if command.split()[0] == cmd:
+                        message.command = command.split()
+                        return True
             return False
 
         return filters.create(func)
