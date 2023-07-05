bcbab41dd98a0c3a2ee334dbd7f709b061d38544
XtomiX <XtomiX@users.noreply.github.com>
2023-07-05 13:53:51 +0000

    Auto Fix

--- PyroUbot/On.py
+++ PyroUbot/On.py
@@ -8,18 +8,26 @@ class abc:
                 text = message.text.strip()
                 matched_prefix = next((p for p in prefix if text.startswith(p)), None)
                 if matched_prefix:
-                    without_prefix = text[len(matched_prefix):]
-    
+                    without_prefix = text[len(matched_prefix) :]
+
                     commands = commands if isinstance(commands, list) else [commands]
                     commands = {c if case_sensitive else c.lower() for c in commands}
 
                     for cmd in commands:
-                        if not re.match(rf"^(?:{cmd})(?:\s|$)", without_prefix,
-                                    flags=re.IGNORECASE if not case_sensitive else 0):
+                        if not re.match(
+                            rf"^(?:{cmd})(?:\s|$)",
+                            without_prefix,
+                            flags=re.IGNORECASE if not case_sensitive else 0,
+                        ):
                             continue
 
-                        without_command = re.sub(rf"{cmd}\s?", "", without_prefix, count=1,
-                                             flags=re.IGNORECASE if not case_sensitive else 0)
+                        without_command = re.sub(
+                            rf"{cmd}\s?",
+                            "",
+                            without_prefix,
+                            count=1,
+                            flags=re.IGNORECASE if not case_sensitive else 0,
+                        )
                         message.command = [cmd] + [
                             re.sub(r"\\([\"'])", r"\1", m.group(2) or m.group(3) or "")
                             for m in command_re.finditer(without_command)
