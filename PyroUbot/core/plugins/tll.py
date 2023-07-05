358d85738a2d14c20115a905b151489bbb32e05e
XtomiX <XtomiX@users.noreply.github.com>
2023-07-05 01:27:49 +0000

    Auto Fix

--- PyroUbot/core/plugins/tll.py
+++ PyroUbot/core/plugins/tll.py
@@ -10,10 +10,10 @@ async def tagall_cmd(client, message):
     tagallgcid.append(message.chat.id)
     text = message.text.split(None, 1)[1] if len(message.text.split()) != 1 else ""
     users = [
-            member.user.mention
-            async for member in message.chat.get_members()
-            if not (member.user.is_bot or member.user.is_deleted)
-        ]
+        member.user.mention
+        async for member in message.chat.get_members()
+        if not (member.user.is_bot or member.user.is_deleted)
+    ]
     shuffle(users)
     m = message.reply_to_message or message
     for output in [users[i : i + 5] for i in range(0, len(users), 5)]:
@@ -21,20 +21,19 @@ async def tagall_cmd(client, message):
             break
         await asyncio.sleep(1.5)
         await m.reply_text(
-                ", ".join(output) + "\n\n" + text, quote=bool(message.reply_to_message)
-            )
+            ", ".join(output) + "\n\n" + text, quote=bool(message.reply_to_message)
+        )
     try:
         tagallgcid.remove(message.chat.id)
     except Exception:
         pass
 
 
-
 async def batal_cmd(client, message):
     if message.chat.id not in tagallgcid:
         return await message.reply_text(
-                "sedang tidak ada perintah: <code>tagall</code> yang digunakan"
-            )
+            "sedang tidak ada perintah: <code>tagall</code> yang digunakan"
+        )
     try:
         tagallgcid.remove(message.chat.id)
     except Exception:
