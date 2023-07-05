d157afd5e3629651c9343da89bd94cf41a2fc269
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-05 15:30:14 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -71,12 +71,7 @@ class Ubot(Client):
 
     def command_filter(self, cmd):
         async def func(_, __, message):
-            if (
-                message.text
-                and message.caption
-                and message.from_user
-                and message.sender_chat
-            ):
+            if message.text and message.from_user:
                 prefix = await self.get_prefix(message.from_user.id)
                 text = message.text.strip()
                 matched_prefix = next((p for p in prefix if text.startswith(p)), None)
