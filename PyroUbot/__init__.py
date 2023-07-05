0d9fe679fac909274c4fde9f50d0a43713fe77ef
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-05 22:47:25 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -1,6 +1,5 @@
 import logging
 import os
-import re
 
 from pyrogram import Client, filters
 from pyrogram.enums import ParseMode
@@ -70,42 +69,16 @@ class Ubot(Client):
     async def get_prefix(self, user_id):
         return self._prefix.get(user_id, ".")
 
-    def command_filter(self, command, case_sensitive=False):
-        command_re = re.compile(r"([\"'])(.*?)(?<!\\)\1|(\S+)")
-
+    def command_filter(self, cmd):
         async def func(_, __, message):
             if message.text and message.from_user:
                 prefix = await self.get_prefix(message.from_user.id)
                 text = message.text.strip()
                 matched_prefix = next((p for p in prefix if text.startswith(p)), None)
                 if matched_prefix:
-                    without_prefix = text[len(matched_prefix) :]
-
-                    if not isinstance(command, list):
-                        commands = [command]
-
-                    command_set = {c if case_sensitive else c.lower() for c in commands}
-
-                    for cmd in command_set:
-                        if not re.match(
-                            rf"^(?:{cmd})(?:\s|$)",
-                            without_prefix,
-                            flags=re.IGNORECASE if not case_sensitive else 0,
-                        ):
-                            continue
-
-                        without_command = re.sub(
-                            rf"{cmd}\s?",
-                            "",
-                            without_prefix,
-                            count=1,
-                            flags=re.IGNORECASE if not case_sensitive else 0,
-                        )
-                        message.command = [cmd] + [
-                            re.sub(r"\\([\"'])", r"\1", m.group(2) or m.group(3) or "")
-                            for m in command_re.finditer(without_command)
-                        ]
-
+                    command = text[len(matched_prefix) :].strip()
+                    if command and command.split()[0] == cmd:
+                        message.command = command.split()
                         return True
             return False
 
