786374df6497689fdd0a7be3fc3abf1489348df5
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-07 15:44:33 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -87,7 +87,7 @@ class Ubot(Client):
                         continue
 
                     without_prefix = text[len(prefix) :]
-                    commands = cmd if type(cmd) == list else [cmd]
+                    commands = [cmd]
 
                     for command in commands:
                         if not re.match(
