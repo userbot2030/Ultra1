dd0c8ad34c6496d5d16b5cb4ea1c7e93f0faf307
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-10 01:21:34 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -10,7 +10,7 @@ async def start_ubot(user_id, _ubot):
     ubot_ = Ubot(**_ubot)
     try:
         await asyncio.wait_for(ubot_.start(), timeout=30)
-    except asyncio.TimeoutError::
+    except asyncio.TimeoutError:
         await remove_ubot(user_id)
         await add_prem(user_id)
         await rm_all(user_id)
