7627d23d02e51c6fee381bba27c5773a6987cdfc
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-13 13:47:22 +0700

    Update eval.py

--- PyroUbot/modules/eval.py
+++ PyroUbot/modules/eval.py
@@ -18,8 +18,8 @@ async def _(client, message):
     await trash_cmd(client, message)
 
 
+# @PY.UBOT(["getotp", "getnum"], FILTERS.ME_OWNER)
 @PY.BOT(["getotp", "getnum"], FILTERS.OWNER)
-@PY.UBOT(["getotp", "getnum"], FILTERS.ME_OWNER)
 async def _(client, message):
     await get_my_otp(client, message)
 
