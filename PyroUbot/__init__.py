a63417ac3bdf857d3bff917199e5739b7438bd16
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-14 22:57:37 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -33,7 +33,7 @@ logger.addHandler(connection_handler)
 
 class Bot(Client):
     def __init__(self, **kwargs):
-        super().__init__(**kwargs, parse_mode=ParseMode.HTML)
+        super().__init__(**kwargs, parse_mode=ParseMode.DEFAULT)
 
     def on_message(self, filters=None, group=-1):
         def decorator(func):
@@ -54,7 +54,7 @@ class Ubot(Client):
     _get_my_peer = {}
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs, device_model="V1PremUbot", parse_mode=ParseMode.HTML)
+        super().__init__(**kwargs, device_model="V1PremUbot", parse_mode=ParseMode.DEFAULT)
 
     def on_message(self, filters=None, group=-1):
         def decorator(func):
