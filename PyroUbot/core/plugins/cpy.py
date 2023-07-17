36bd6e988d34b007ea4b09e5b8d18ced43182242
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-17 07:49:03 +0700

    Update cpy.py

--- PyroUbot/core/plugins/cpy.py
+++ PyroUbot/core/plugins/cpy.py
@@ -117,10 +117,11 @@ async def copy_callback_msg(client, callback_query):
             copy = await m._client.send_message(
                 bot.me.username, f"/copy {m.text.split()[1]}"
             )
+            msg = m.reply_to_message or m
             await asyncio.sleep(1.5)
             await copy.delete()
             async for get in m._client.search_messages(bot.me.username, limit=1):
-                await m._client.copy_message(m.chat.id, bot.me.username, get.id)
+                await m._client.copy_message(m.chat.id, bot.me.username, get.id, reply_to_message_id=msg.id)
                 await m._client.delete_messages(m.chat.id, COPY_ID[m._client.me.id])
                 await get.delete()
     except Exception as error:
