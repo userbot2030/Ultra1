d80d2165dbf18062860e2bae00409c50fe31cb40
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-17 21:00:39 +0700

    Update tools.py

--- PyroUbot/core/helpers/tools.py
+++ PyroUbot/core/helpers/tools.py
@@ -8,6 +8,12 @@ from PIL import Image, ImageDraw, ImageFont
 from pymediainfo import MediaInfo
 
 
+import asyncio
+import math
+import os
+from gc import get_objects
+from time import time
+
 class Media_Info:
     def data(media):
         found = False
@@ -224,10 +230,74 @@ async def dl_pic(client, download):
     return get_photo
 
 
-async def edit_or_reply(message, text):
-    msg = (
-        message.edit_text
-        if bool(message.from_user and message.from_user.is_self or message.outgoing)
-        else (message.reply_to_message or message).reply_text
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "ᴋʙ", 2: "ᴍʙ", 3: "ɢʙ", 4: "ᴛʙ"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return f"{str(round(size, 2))} {dict_power_n[raised_to_pow]}"
+
+
+def time_formatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(milliseconds, 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        (f"{str(days)} ʜᴀʀɪ, " if days else "")
+        + (f"{str(hours)} ᴊᴀᴍ, " if hours else "")
+        + (f"{str(minutes)} ᴍᴇɴɪᴛ, " if minutes else "")
+        + (f"{str(seconds)} ᴅᴇᴛɪᴋ, " if seconds else "")
+        + (f"{str(milliseconds)} ᴍɪᴋʀᴏᴅᴇᴛɪᴋ, " if milliseconds else "")
     )
-    return await msg(text)
+    return tmp[:-2]
+
+
+async def progress(current, total, message, start, type_of_ps, file_name=None):
+    now = time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        if elapsed_time == 0:
+            return
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "💢 ᴘᴇʀsᴇɴᴛᴀsᴇ: {0}{1} {2}%\n".format(
+            "".join("°" for _ in range(math.floor(percentage / 10))),
+            "".join("-" for _ in range(10 - math.floor(percentage / 10))),
+            round(percentage, 2),
+        )
+        tmp = "📂 ғɪʟᴇ_sɪᴢᴇ: {0} - {1}\n{3}\n⏳ ᴇsᴛɪᴍᴀsɪ: {2}\n".format(
+            humanbytes(current),
+            humanbytes(total),
+            time_formatter(estimated_total_time),
+            progress_str,
+        )
+        if file_name:
+            try:
+                await message.edit(
+                    f"""
+<b>📥 {type_of_ps}</b>
+
+<b>🆔 ғɪʟᴇ_ɪᴅ:</b> <code>{file_name}</code>
+<b>{tmp}</b>
+"""
+                )
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+        else:
+            try:
+                await message.edit(f"{type_of_ps}\n{tmp}")
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+
