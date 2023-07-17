c5b554979f61193e739b25c038b3e72da4f3ff74
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-17 10:28:25 +0700

    Update cpy.py

--- PyroUbot/core/plugins/cpy.py
+++ PyroUbot/core/plugins/cpy.py
@@ -5,8 +5,11 @@ from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
                             InlineQueryResultArticle, InputTextMessageContent)
 
 from PyroUbot import *
+import asyncio
+import math
+import os
+from time import time
 
-COPY_ID = {}
 
 
 async def copy_bot_msg(client, message):
@@ -28,6 +31,8 @@ async def copy_bot_msg(client, message):
         await Tm.edit(error)
 
 
+COPY_ID = {}
+
 async def copy_ubot_msg(client, message):
     msg = message.reply_to_message or message
     Tm = await message.reply("<b>sᴇᴅᴀɴɢ ᴍᴇᴍᴘʀᴏsᴇs ᴄᴏᴘʏ ᴍᴏʜᴏɴ ʙᴇʀsᴀʙᴀʀ</b>")
@@ -43,7 +48,7 @@ async def copy_ubot_msg(client, message):
             get = await client.get_messages(chat, msg_id)
             text = get.caption or ""
             if get.photo:
-                await client.download_media(get)
+                await client.download_media(get, progress=progress, progress_args=(Tm, time(), "Photo"))
                 await client.send_photo(
                     message.chat.id, media, text, reply_to_message_id=msg.id
                 )
@@ -51,7 +56,7 @@ async def copy_ubot_msg(client, message):
                 os.remove(media)
 
             elif get.video:
-                media = await client.download_media(get)
+                media = await client.download_media(get, progress=progress, progress_args=(Tm, time(), "Video"))
                 await app.send_video(
                     message.chat.id, media, text, reply_to_message_id=msg.id
                 )
@@ -59,7 +64,7 @@ async def copy_ubot_msg(client, message):
                 os.remove(media)
 
             elif get.audio:
-                media = await client.download_media(get)
+                media = await client.download_media(get, progress=progress, progress_args=(Tm, time(), "Audio"))
                 await client.send_audio(
                     message.chat.id, media, text, reply_to_message_id=msg.id
                 )
@@ -67,7 +72,7 @@ async def copy_ubot_msg(client, message):
                 os.remove(media)
 
             elif get.voice:
-                media = await client.download_media(get)
+                media = await client.download_media(get, progress=progress, progress_args=(Tm, time(), "Voice"))
                 await client.send_voice(
                     message.chat.id, media, text, reply_to_message_id=msg.id
                 )
@@ -75,7 +80,7 @@ async def copy_ubot_msg(client, message):
                 os.remove(media)
 
             elif get.document:
-                media = await client.download_media(get)
+                media = await client.download_media(get, progress=progress, progress_args=(Tm, time(), "Document"))
                 await client.send_document(
                     message.chat.id, media, text, reply_to_message_id=msg.id
                 )
@@ -83,7 +88,7 @@ async def copy_ubot_msg(client, message):
                 os.remove(media)
 
             elif get.animation:
-                media = await client.download_media(get)
+                media = await client.download_media(get, progress=progress, progress_args=(Tm, time(), "Animation"))
                 await client.send_animation(
                     message.chat.id, media, text, reply_to_message_id=msg.id
                 )
@@ -112,6 +117,76 @@ async def copy_ubot_msg(client, message):
                     return await Tm.edit(error)
     else:
         await Tm.edit("ᴍᴀsᴜᴋᴋɪɴ ʟɪɴᴋ ʏᴀɴɢ ᴠᴀʟɪᴅ")
+
+
+def humanbytes(size):
+    if not size:
+        return ""
+    power = 2**10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "ᴋʙ", 2: "ᴍʙ", 3: "ɢʙ", 4: "ᴛʙ"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return f"{str(round(size, 2))} {dict_power_n[raised_to_pow]}"
+
+
+def time_formatter(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(milliseconds, 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        (f"{str(days)} ʜᴀʀɪ, " if days else "")
+        + (f"{str(hours)} ᴊᴀᴍ, " if hours else "")
+        + (f"{str(minutes)} ᴍᴇɴɪᴛ, " if minutes else "")
+        + (f"{str(seconds)} ᴅᴇᴛɪᴋ, " if seconds else "")
+        + (f"{str(milliseconds)} ᴍɪᴋʀᴏᴅᴇᴛɪᴋ, " if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+async def progress(current, total, message, start, type_of_ps, file_name=None):
+    now = time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        elapsed_time = round(diff) * 1000
+        if elapsed_time == 0:
+            return
+        time_to_completion = round((total - current) / speed) * 1000
+        estimated_total_time = elapsed_time + time_to_completion
+        progress_str = "{0}{1} {2}%\n".format(
+            "".join("•" for _ in range(math.floor(percentage / 10))),
+            "".join("~" for _ in range(10 - math.floor(percentage / 10))),
+            round(percentage, 2),
+        )
+        tmp = progress_str + "{0} of {1}\nᴇsᴛɪᴍᴀsɪ: {2}".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        )
+        if file_name:
+            try:
+                await message.edit(
+                    f"""
+<b>{type_of_ps}</b>
+
+<b>ғɪʟᴇ_ɪᴅ:</b> <code>{file_name}</code>
+
+<b>{tmp}</b>
+"""
+                )
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
+        else:
+            try:
+                await message.edit(f"{type_of_ps}\n{tmp}")
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageNotModified:
+                pass
 
 
 async def copy_inline_msg(client, inline_query):
