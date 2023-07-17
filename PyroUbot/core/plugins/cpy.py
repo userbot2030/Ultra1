70cf0bd0146f34bc3d3fa3795637ee377d674666
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-17 14:20:00 +0700

    Update cpy.py

--- PyroUbot/core/plugins/cpy.py
+++ PyroUbot/core/plugins/cpy.py
@@ -220,13 +220,13 @@ async def progress(current, total, message, start, type_of_ps, file_name=None):
             return
         time_to_completion = round((total - current) / speed) * 1000
         estimated_total_time = elapsed_time + time_to_completion
-        progress_str = "💯 ᴘᴇʀsᴇɴᴛᴀsᴇ {0}{1} {2}%\n".format(
+        progress_str = "💯 ᴘᴇʀsᴇɴᴛᴀsᴇ: {0}{1} {2}%\n".format(
             "".join("°" for _ in range(math.floor(percentage / 10))),
             "".join("-" for _ in range(10 - math.floor(percentage / 10))),
             round(percentage, 2),
         )
-        tmp = progress_str + "⏳ {0} ᴏғ {1}\n📍 ᴇsᴛɪᴍᴀsɪ: {2}\n".format(
-            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
+        tmp = "⏳ ғɪʟᴇ_sɪᴢᴇ: {0} ᴏғ {1}\n{3}\n\n📍 ᴇsᴛɪᴍᴀsɪ: {2}\n".format(
+            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time), progress_str
         )
         if file_name:
             try:
@@ -235,7 +235,6 @@ async def progress(current, total, message, start, type_of_ps, file_name=None):
 <b>📥 {type_of_ps}</b>
 
 <b>📂 ғɪʟᴇ_ɪᴅ:</b> <code>{file_name}</code>
-
 <b>{tmp}</b>
 """
                 )
