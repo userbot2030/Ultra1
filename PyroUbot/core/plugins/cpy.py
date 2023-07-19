dd11aac8cba1f9718da9a8d60521903dc6da2077
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-19 12:44:35 +0700

    Update cpy.py

--- PyroUbot/core/plugins/cpy.py
+++ PyroUbot/core/plugins/cpy.py
@@ -38,13 +38,7 @@ async def download_media_copy(get, client, infomsg, message):
         media = await client.download_media(
             get,
             progress=progress,
-            progress_args=(
-                infomsg,
-                time(),
-                "ᴅᴏᴡɴʟᴏᴀᴅ ᴘʜᴏᴛᴏ",
-                get.photo.file_id,
-            ),
-        )
+            progress_args=(infomsg, time(),"ᴅᴏᴡɴʟᴏᴀᴅ ᴘʜᴏᴛᴏ", get.photo.file_id))
         await client.send_photo(
             message.chat.id,
             media,
