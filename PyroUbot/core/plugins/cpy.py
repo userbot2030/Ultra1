03aa011a542796ac5e063cedda605f5e0ec0553f
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-19 12:50:17 +0700

    Update cpy.py

--- PyroUbot/core/plugins/cpy.py
+++ PyroUbot/core/plugins/cpy.py
@@ -57,8 +57,7 @@ async def download_media_copy(get, client, infomsg, message):
                 infomsg,
                 time(),
                 "ᴅᴏᴡɴʟᴏᴀᴅ ᴀɴɪᴍᴀᴛɪᴏɴ",
-                get.animation.file_id,
-            ),
+                get.animation.file_id)
         )
         await client.send_animation(
             message.chat.id,
