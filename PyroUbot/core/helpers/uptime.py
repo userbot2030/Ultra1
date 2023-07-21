21f7e1d2d55e25aaa69e6610110807bcf687e484
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-21 13:41:11 +0700

    Update uptime.py

--- PyroUbot/core/helpers/uptime.py
+++ PyroUbot/core/helpers/uptime.py
@@ -2,24 +2,33 @@ from time import time as waktunya
 
 start_time = waktunya()
 
-
 async def get_time(seconds):
     count = 0
     up_time = ""
     time_list = []
-    time_suffix_list = ["s", "ᴍ", "ʜ", "ᴅ"]
-
-    while count < 4:
+    time_suffix_list = ["s", "ᴍ", "ʜ", "ᴅ", "ᴡ", "ᴡᴍᴏ"]
+    
+    while count < 6:
         count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        elif count < 4:
+            remainder, result = divmod(seconds, 24)
+        elif count < 5:
+            remainder, result = divmod(seconds, 7) 
+        else:
+            remainder, result = divmod(seconds, 30 * 24 * 60 * 60)
+
         if seconds == 0 and remainder == 0:
             break
+
         time_list.append(int(result))
         seconds = int(remainder)
 
     for i in range(len(time_list)):
         time_list[i] = str(time_list[i]) + time_suffix_list[i]
-    if len(time_list) == 4:
+
+    if len(time_list) >= 4:
         up_time += time_list.pop() + ":"
 
     time_list.reverse()
