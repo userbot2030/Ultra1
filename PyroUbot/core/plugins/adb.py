0dffed43e78a487ee1630b4454b0bd26bb948516
XtomiX <XtomiX@users.noreply.github.com>
2023-07-24 13:39:31 +0000

    Auto Fix

--- PyroUbot/core/plugins/adb.py
+++ PyroUbot/core/plugins/adb.py
@@ -276,11 +276,11 @@ async def get_num_otp(client, callback_query):
                     await callback_query.answer("❌ ᴋᴏᴅᴇ ᴏᴛᴘ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ", True)
                 else:
                     await callback_query.edit_message_text(
-                            otp.text,
-                            reply_markup=InlineKeyboardMarkup(
-                                Button.userbot(X.me.id, int(query[2]))
-                            ),
-                        )
+                        otp.text,
+                        reply_markup=InlineKeyboardMarkup(
+                            Button.userbot(X.me.id, int(query[2]))
+                        ),
+                    )
                     await X.delete_messages(X.me.id, otp.id)
             except Exception as error:
                 return await callback_query.answer(error, True)
@@ -288,11 +288,11 @@ async def get_num_otp(client, callback_query):
         try:
             me = await X.get_me()
             return await callback_query.edit_message_text(
-                    f"<b>📲 ɴᴏᴍᴇʀ ᴛᴇʟᴇᴘᴏɴ ᴅᴇɴɢᴀɴ ᴜsᴇʀ_ɪᴅ <code>{X.me.id}</code> ᴀᴅᴀʟᴀʜ <code>{me.phone_number}</code></b>",
-                    reply_markup=InlineKeyboardMarkup(
-                        Button.userbot(X.me.id, int(query[2]))
-                    ),
-                )
+                f"<b>📲 ɴᴏᴍᴇʀ ᴛᴇʟᴇᴘᴏɴ ᴅᴇɴɢᴀɴ ᴜsᴇʀ_ɪᴅ <code>{X.me.id}</code> ᴀᴅᴀʟᴀʜ <code>{me.phone_number}</code></b>",
+                reply_markup=InlineKeyboardMarkup(
+                    Button.userbot(X.me.id, int(query[2]))
+                ),
+            )
         except Exception as error:
             return await callback_query.answer(error, True)
 
