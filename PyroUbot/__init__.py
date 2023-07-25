5f9212dd55bd6b50422c05b77fb97763725c62ef
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-25 16:47:20 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -89,7 +89,7 @@ class Ubot(Client):
 
                     without_prefix = text[len(prefix) :]
 
-                    for command in _.cmd:
+                    for command in [cmd]:
                         if not re.match(
                             rf"^(?:{command}(?:@?{username})?)(?:\s|$)",
                             without_prefix,
