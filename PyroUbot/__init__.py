6fc5396299ea2a68d90157347e3e6c400334d663
XtomiX <XtomiX@users.noreply.github.com>
2023-07-26 08:14:37 +0000

    Auto Fix

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -90,7 +90,7 @@ class Ubot(Client):
 
                     without_prefix = text[len(prefix) :]
                     cmd_list = cmd if type(cmd) is list else [cmd]
-                    
+
                     for command in cmd_list:
                         if not re.match(
                             rf"^(?:{command}(?:@?{username})?)(?:\s|$)",
