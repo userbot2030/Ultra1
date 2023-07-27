f3d1298987deadc216f02a6601a1cbc754f53ede
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-27 12:18:52 +0700

    Update stg.py

--- PyroUbot/core/plugins/stg.py
+++ PyroUbot/core/plugins/stg.py
@@ -13,14 +13,10 @@ async def setprefix(client, message):
             else:
                 set_prefix_ub.append(prefix)
         try:
-            set_prefix_ubot = [
-                emoji if len(emoji) == 1 else f"\\u{ord(emoji):X}"
-                for emoji in set_prefix_ub
-            ]
-            ubot.set_prefix(message.from_user.id, set_prefix_ubot)
-            await set_pref(message.from_user.id, set_prefix_ubot)
+            ubot.set_prefix(message.from_user.id, set_prefix_ub)
+            await set_pref(message.from_user.id, set_prefix_ub)
             return await Tm.edit(
-                f"<b>✅ ᴘʀᴇғɪx ᴛᴇʟᴀʜ ᴅɪᴜʙᴀʜ ᴋᴇ: {' '.join(set_prefix_ubot)}"
+                f"<b>✅ ᴘʀᴇғɪx ᴛᴇʟᴀʜ ᴅɪᴜʙᴀʜ ᴋᴇ: {' '.join(set_prefix_ub).encode('utf-8').decode('utf-8')}"
             )
         except Exception as error:
-            await Tm.edit(str(error))
+            await Tm.edit(error)
