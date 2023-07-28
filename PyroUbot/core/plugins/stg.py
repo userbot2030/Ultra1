439d64dd23c797120176e3b0f8838bd451875afc
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-28 10:56:40 +0700

    Update stg.py

--- PyroUbot/core/plugins/stg.py
+++ PyroUbot/core/plugins/stg.py
@@ -9,7 +9,7 @@ async def setprefix(client, message):
         return await Tm.edit(f"<code>{message.text}</code> sɪᴍʙᴏʟ ᴘʀᴇғɪx")
     else:
         ub_prefix = []
-        for prefix in message.command[1:]:
+        for prefix in html.escape(message.command[1:]):
             if prefix.lower() == "none":
                 ub_prefix.append("")
             else:
@@ -17,9 +17,7 @@ async def setprefix(client, message):
         try:
             ubot.set_prefix(message.from_user.id, ub_prefix)
             await set_pref(message.from_user.id, ub_prefix)
-            parsed_prefix = " ".join(
-                f"<code>{html.escape(prefix)}</code>" for prefix in ub_prefix
-            )
+            parsed_prefix = " ".join(f"<code>{prefix}</code>" for prefix in ub_prefix)
             return await Tm.edit(f"<b>✅ ᴘʀᴇғɪx ᴛᴇʟᴀʜ ᴅɪᴜʙᴀʜ ᴋᴇ: {parsed_prefix}</b>")
         except Exception as error:
             await Tm.edit(str(error))
