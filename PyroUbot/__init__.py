7d0b79bfcf2a140b7021058ffb47348918754f7f
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-30 20:35:21 +0700

    Update __init__.py

--- PyroUbot/__init__.py
+++ PyroUbot/__init__.py
@@ -68,7 +68,7 @@ class Ubot(Client):
         self._prefix[user_id] = prefix
 
     async def get_prefix(self, user_id):
-        return self._prefix.get(user_id, PREFIX)
+        return self._prefix.get(user_id, [".", ",", ":", ";", "!"])
 
     def cmd_prefix(self, cmd):
         command_re = re.compile(r"([\"'])(.*?)(?<!\\)\1|(\S+)")
@@ -120,7 +120,7 @@ class Ubot(Client):
         if handler:
             self._prefix[self.me.id] = handler
         else:
-            self._prefix[self.me.id] = PREFIX
+            self._prefix[self.me.id] = [".", ",", ":", ";", "!"]
         self._ubot.append(self)
         self._get_my_id.append(self.me.id)
         self._translate[self.me.id] = "id"
