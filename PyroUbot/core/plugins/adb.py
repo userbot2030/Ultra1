bb19dee176435bea9c97e7cd8726a649cbedba4f
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-30 09:30:47 +0700

    Update adb.py

--- PyroUbot/core/plugins/adb.py
+++ PyroUbot/core/plugins/adb.py
@@ -54,6 +54,8 @@ async def need_api(client, callback_query):
             """
 <b>✅ ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴜᴀᴛ ᴜsᴇʀʙᴏᴛ sɪᴀᴘᴋᴀɴ ʙᴀʜᴀɴ ʙᴇʀɪᴋᴜᴛ
 
+    • <code>ᴀᴘɪ_ɪᴅ</code>: ᴅᴀᴘᴀᴛᴋᴀɴ ᴅᴀʀɪ my.telegram.org
+    • <code>ᴀᴘɪ_ʜᴀsʜ</code>: ᴅᴀᴘᴀᴛᴋᴀɴ ᴅᴀʀɪ my.telegram.org
     • <code>ᴘʜᴏɴᴇ_ɴᴜᴍʙᴇʀ</code>: ɴᴏᴍᴇʀ ʜᴘ ᴀᴋᴜɴ ᴛᴇʟᴇɢʀᴀᴍ
 
 ☑️ ᴊɪᴋᴀ sᴜᴅᴀʜ ᴛᴇʀsᴇᴅɪᴀ sɪʟᴀʜᴋᴀɴ ᴋʟɪᴋ ᴛᴏᴍʙᴏɪ ᴅɪʙᴀᴡᴀʜ</b>
@@ -79,6 +81,34 @@ async def bikin_ubot(client, callback_query):
     user_id = callback_query.from_user.id
     try:
         await callback_query.message.delete()
+        api = await bot.ask(
+            user_id,
+            (
+                "<b>sɪʟᴀʜᴋᴀɴ ᴍᴀsᴜᴋᴋᴀɴ ᴀᴘɪ_ɪᴅ</b>\n"
+                "\n<b>ɢᴜɴᴀᴋᴀɴ /cancel ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴀᴛᴀʟᴋᴀɴ ᴘʀᴏsᴇs ᴍᴇᴍʙᴜᴀᴛ ᴜsᴇʀʙᴏᴛ</b>"
+            ),
+            timeout=300,
+        )
+    except asyncio.TimeoutError:
+        return await bot.send_message(user_id, "ᴘᴇᴍʙᴀᴛᴀʟᴀɴ ᴏᴛᴏᴍᴀᴛɪs")
+    if await is_cancel(callback_query, api.text):
+        return
+    api_id = api.text
+    try:
+        hash = await bot.ask(
+            user_id,
+            (
+                "<b>sɪʟᴀʜᴋᴀɴ ᴍᴀsᴜᴋᴋᴀɴ ᴀᴘɪ_ʜᴀsʜ</b>\n"
+                "\n<b>ɢᴜɴᴀᴋᴀɴ /cancel ᴜɴᴛᴜᴋ ᴘʀᴏsᴇs ᴍᴇᴍʙᴜᴀᴛ ᴜsᴇʀʙᴏᴛ</b>"
+            ),
+            timeout=300,
+        )
+    except asyncio.TimeoutError:
+        return await bot.send_message(user_id, "ᴘᴇᴍʙᴀᴛᴀʟᴀɴ ᴏᴛᴏᴍᴀᴛɪs")
+    if await is_cancel(callback_query, hash.text):
+        return
+    api_hash = hash.text
+    try:
         phone = await bot.ask(
             user_id,
             (
@@ -94,8 +124,8 @@ async def bikin_ubot(client, callback_query):
     phone_number = phone.text
     new_client = Ubot(
         name=str(callback_query.id),
-        api_id=API_ID,
-        api_hash=API_HASH,
+        api_id=api_id,
+        api_hash=api_hash,
         in_memory=False,
     )
     get_otp = await bot.send_message(user_id, "<b>ᴍᴇɴɢɪʀɪᴍ ᴋᴏᴅᴇ ᴏᴛᴘ...</b>")
@@ -191,8 +221,8 @@ async def bikin_ubot(client, callback_query):
         )
     await add_ubot(
         user_id=int(new_client.me.id),
-        api_id=API_ID,
-        api_hash=API_HASH,
+        api_id=api_id,
+        api_hash=api_hash,
         session_string=session_string,
     )
     await set_uptime(new_client.me.id, time())
