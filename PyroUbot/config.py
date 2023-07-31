1e1e6f45c6cb1a67168bb4e5518d3ed3d77e3d20
Tomi Setiawan <96681219+XtomiX@users.noreply.github.com>
2023-07-31 18:15:04 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -9,7 +9,7 @@ BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001969856888").spl
 MAX_BOT = int(os.getenv("MAX_BOT", "50"))
 RMBG_API = os.getenv("RMBG_API", "b5ZnjZ2nUUpbdEHfcrWdjWbC")
 OPENAI_KEY = os.getenv(
-    "OPENAI_KEY", "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"
+    "OPENAI_KEY", "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"
 )
 MONGO_URL = os.getenv(
     "MONGO_URL",
