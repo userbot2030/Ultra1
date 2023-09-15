907e13d5cd69a637ef1cfaef4b208d8383e7e9e7
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-09-15 17:00:37 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -1,11 +1,11 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "22039315"))
-API_HASH = os.getenv("API_HASH", "b9d878c0e74f6b2a2e2df3a1d9be968b")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6207982225:AAFqWP9qDE3fmvxH21W6Ppgui8yZwTdubAk")
-OWNER_ID = int(os.getenv("OWNER_ID", "1964437366"))
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1001969856888"))
-BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001969856888").split()))
+API_ID = int(os.getenv("API_ID", "17896688"))
+API_HASH = os.getenv("API_HASH", "947327cf5ff0053a66bf7951f9db5658")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6622316489:AAFu3gq8rm1ZZzvPxzg8jsHcosXs9vQ9UAg")
+OWNER_ID = int(os.getenv("OWNER_ID", "1948147616"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1001769286350"))
+BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001969856888, -1001571197486").split()))
 MAX_BOT = int(os.getenv("MAX_BOT", "50"))
 RMBG_API = os.getenv("RMBG_API", "b5ZnjZ2nUUpbdEHfcrWdjWbC")
 OPENAI_KEY = os.getenv(
@@ -13,5 +13,5 @@ OPENAI_KEY = os.getenv(
 )
 MONGO_URL = os.getenv(
     "MONGO_URL",
-    "mongodb+srv://PremUbot:sST5VL04n85JkcxS@cluster0.wixneuz.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://kjmenfes:fadhil123@cluster0.inkz42g.mongodb.net/?retryWrites=true&w=majority",
 )
