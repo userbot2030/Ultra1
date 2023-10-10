2496ce4e1bca967a73f7eb99e8c2fe6d1cf9b994
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-10-10 21:34:05 +0700

    Update stg.py

--- PyroUbot/core/plugins/stg.py
+++ PyroUbot/core/plugins/stg.py
@@ -1,5 +1,3 @@
-import html
-
 from PyroUbot import *
 
 
@@ -13,11 +11,55 @@ async def setprefix(client, message):
             if prefix.lower() == "none":
                 ub_prefix.append("")
             else:
-                ub_prefix.append(html.escape(prefix))
+                ub_prefix.append(prefix)
         try:
             ubot.set_prefix(message.from_user.id, ub_prefix)
             await set_pref(message.from_user.id, ub_prefix)
             parsed_prefix = " ".join(f"<code>{prefix}</code>" for prefix in ub_prefix)
             return await Tm.edit(f"<b>✅ ᴘʀᴇғɪx ᴛᴇʟᴀʜ ᴅɪᴜʙᴀʜ ᴋᴇ: {parsed_prefix}</b>")
         except Exception as error:
-            await Tm.edit(str(error))
+            return await Tm.edit(str(error))
+
+
+async def change_emot(client, message):
+    try:
+        msg = await message.reply("ᴍᴇᴍᴘʀᴏsᴇs...", quote=True)
+
+        if not client.me.is_premium:
+            return await msg.edit(
+                "<b>ᴜɴᴛᴜᴋ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ᴀᴋᴜɴ ᴀɴᴅᴀ ʜᴀʀᴜs ᴘʀᴇᴍɪᴜᴍ ᴛᴇʀʟᴇʙɪʜ</b>"
+            )
+
+        if len(message.command) < 3:
+            return await msg.edit("<b>ᴛᴏʟᴏɴɢ ᴍᴀsᴜᴋᴋᴀɴ ǫᴜᴇʀʏ ᴅᴀɴ ᴠᴀʟᴇᴜ ɴʏᴀ</b>")
+
+        query_mapping = {
+            "pong": "EMOJI_PING_PONG",
+            "uptime": "EMOJI_UPTIME",
+            "mention": "EMOJI_MENTION",
+        }
+        command, mapping, value = message.command[:3]
+
+        if mapping.lower() in query_mapping:
+            query_var = query_mapping[mapping.lower()]
+            emoji_id = None
+            if message.entities:
+                for entity in message.entities:
+                    if entity.custom_emoji_id:
+                        emoji_id = entity.custom_emoji_id
+                        break
+
+            if emoji_id:
+                await set_vars(client.me.id, query_var, emoji_id)
+                await msg.edit(
+                    f"<b>✅ <code>{query_var}</code> ʙᴇʀʜᴀsɪʟ ᴅɪ sᴇᴛᴛɪɴɢ ᴋᴇ:</b> <emoji id={emoji_id}>{value}</emoji>"
+                )
+            else:
+                await msg.edit(
+                    "<b>ᴜɴᴛᴜᴋ ᴍᴇɴɢɢᴜɴᴀᴋᴀɴ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ᴀᴋᴜɴ ᴀɴᴅᴀ ʜᴀʀᴜs ᴘʀᴇᴍɪᴜᴍ ᴛᴇʀʟᴇʙɪʜ</b>"
+                )
+        else:
+            await msg.edit("<b>ᴍᴀᴘᴘɪɴɢ ᴛɪᴅᴀᴋ ᴅɪᴛᴇᴍᴜᴋᴀɴ</b>")
+
+    except Exception as error:
+        await msg.edit(str(error))
