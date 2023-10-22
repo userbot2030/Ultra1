1a109722dace2084bd8ab180e5fccb93d7e59f73
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-10-23 03:46:10 +0700

    Update hlp.py

--- PyroUbot/core/plugins/hlp.py
+++ PyroUbot/core/plugins/hlp.py
@@ -30,7 +30,7 @@ async def help_cmd(client, message):
 
 
 async def menu_inline(client, inline_query):
-    msg = f"<b>✣ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a>\n\n★ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs sɪ ᴧꝛᴧʙ ᴘʀᴇᴍ: {len(HELP_COMMANDS)}</b>"
+    msg = f"<b>ᯤ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a>\n\nᝰ.ᐟ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs ᴜʙᴏᴛ: {len(HELP_COMMANDS)}</b>"
     await client.answer_inline_query(
         inline_query.id,
         cache_time=60,
