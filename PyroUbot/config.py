a6a78894146fdf43da3664f8a01c3806eb0aafc1
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-10-24 00:33:51 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -2,8 +2,8 @@ import os
 
 API_ID = int(os.getenv("API_ID", "17896688"))
 API_HASH = os.getenv("API_HASH", "947327cf5ff0053a66bf7951f9db5658")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6679967443:AAF71SynuLFGQFXM3SJLlBlaaYgc18Q4NFw")
-OWNER_ID = int(os.getenv("OWNER_ID", "1913872347"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6711664470:AAEXziGUPTozIbcole7tbOIL_W7_2NLg1rA")
+OWNER_ID = int(os.getenv("OWNER_ID", "1948147616"))
 LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1001887551242"))
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001969856888 -1001571197486").split()))
 MAX_BOT = int(os.getenv("MAX_BOT", "40"))
@@ -13,5 +13,5 @@ OPENAI_KEY = os.getenv(
 )
 MONGO_URL = os.getenv(
     "MONGO_URL",
-    "mongodb+srv://ubot2:abdat123@ubot2.74f7bpy.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://SpesialUbot:fadhil123@cluster0.hbjugoi.mongodb.net/?retryWrites=true&w=majority",
 )
