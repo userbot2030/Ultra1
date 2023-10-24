d5a3586958ce7d5f65b8455314588535452e7a18
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-10-24 08:20:11 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -81,3 +81,16 @@ class PY:
             return wrapped_func
 
         return wrapper
+        
+    def PRIVATE(func):
+        async def function(client, message):
+            user = message.from_user
+            rpk = f"<a href='tg://user?id={user.id}'>{user.first_name} {user.last_name or ''}</a>"
+            if not message.chat.type == ChatType.PRIVATE:
+                return await message.reply(
+                    f"<b>❌ ᴍᴀᴀғ {rpk}, ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ʜᴀɴʏᴀ ʙᴇʀғᴜɴɢsɪ ᴅɪ ᴘʀɪᴠᴀᴛᴇ.</b>",
+                    quote=True,
+                )
+            return await func(client, message)
+
+        return function
