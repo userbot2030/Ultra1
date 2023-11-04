dab99f08ca98d2b117362b348db28f3dc6bdad18
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-11-04 14:00:31 +0700

    Update variabel.py

--- PyroUbot/core/database/variabel.py
+++ PyroUbot/core/database/variabel.py
@@ -10,7 +10,7 @@ async def set_vars(bot_id, vars_name, value, query="vars"):
 
 async def get_vars(bot_id, vars_name, query="vars"):
     result = await varsdb.find_one({"_id": bot_id})
-    return result.get(query).get(vars_name) if result else None
+    return result.get(query, {}).get(vars_name, None) if result else None
 
 
 async def remove_vars(bot_id, vars_name, query="vars"):
@@ -25,6 +25,24 @@ async def all_vars(user_id, query="vars"):
 
 async def remove_all_vars(bot_id):
     await varsdb.delete_one({"_id": bot_id})
+
+
+async def get_list_from_vars(user_id, vars_name, query="vars"):
+    vars_data = await get_vars(user_id, vars_name, query)
+    return [int(x) for x in str(vars_data).split()] if vars_data else []
+
+
+async def add_to_vars(user_id, vars_name, value, query="vars"):
+    vars_list = await get_list_from_vars(user_id, vars_name, query)
+    vars_list.append(value)
+    await set_vars(user_id, vars_name, " ".join(map(str, vars_list)), query)
+
+
+async def remove_from_vars(user_id, vars_name, value, query="vars"):
+    vars_list = await get_list_from_vars(user_id, vars_name, query)
+    if value in vars_list:
+        vars_list.remove(value)
+        await set_vars(user_id, vars_name, " ".join(map(str, vars_list)), query)
 
 
 async def get_pm_id(user_id):
