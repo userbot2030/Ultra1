5aba285107a4e7fb3286a87485e95f69c23a0a15
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-11-04 14:18:49 +0700

    Update invite.py

--- PyroUbot/modules/invite.py
+++ PyroUbot/modules/invite.py
@@ -16,15 +16,18 @@ __HELP__ = """
 
 
 @PY.UBOT("invite")
+@PY.TOP_CMD
 async def _(client, message):
     await invite_cmd(client, message)
 
 
 @PY.UBOT("inviteall")
+@PY.TOP_CMD
 async def _(client, message):
     await inviteall_cmd(client, message)
 
 
 @PY.UBOT("cancel")
+@PY.TOP_CMD
 async def _(client, message):
     await cancel_cmd(client, message)
