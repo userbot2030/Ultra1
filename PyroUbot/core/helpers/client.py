e29d51b0c0c4661ac7a5f41c67f6bc18f57ebd19
SpaceX50 <140992454+SpaceX50@users.noreply.github.com>
2023-11-06 13:52:14 +0700

    Update client.py

--- PyroUbot/core/helpers/client.py
+++ PyroUbot/core/helpers/client.py
@@ -1,5 +1,8 @@
 from pyrogram import filters
 from pyrogram.enums import ChatType
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,
+                            InlineQueryResultArticle)
 
 from PyroUbot import *
 
@@ -121,6 +124,32 @@ class PY:
             top = await get_vars(bot.me.id, cmd, "modules")
             get = int(top) + 1 if top else 1
             await set_vars(bot.me.id, cmd, get, "modules")
+            return await func(client, message)
+
+        return function
+
+    def START(func):
+        async def function(client, message):
+            seved_users = await get_list_from_vars(client.me.id, "SAVED_USERS")
+            user_id = message.from_user.id
+            if user_id != OWNER_ID:
+                if user_id not in seved_users:
+                    await add_to_vars(client.me.id, "SAVED_USERS", user_id)
+                user_link = f"<a href=tg://user?id={message.from_user.id}>{message.from_user.first_name} {message.from_user.last_name or ''}</a>"
+                formatted_text = f"{user_link}\n\n<code>{message.text}</code>"
+                buttons = [
+                    [
+                        InlineKeyboardButton(
+                            "👤 ᴅᴀᴘᴀᴛᴋᴀɴ ᴘʀᴏғɪʟ 👤",
+                            callback_data=f"profil {message.from_user.id}",
+                        ),
+                    ]
+                ]
+                await client.send_message(
+                    OWNER_ID,
+                    formatted_text,
+                    reply_markup=InlineKeyboardMarkup(buttons),
+                )
             return await func(client, message)
 
         return function
