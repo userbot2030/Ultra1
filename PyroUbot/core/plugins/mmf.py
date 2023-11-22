a1f5599cc4151ec8947ec1e55ebd4a59027eaf12
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-11-22 15:50:00 +0700

    Update mmf.py

--- PyroUbot/core/plugins/mmf.py
+++ PyroUbot/core/plugins/mmf.py
@@ -11,8 +11,8 @@ async def memify_cmd(client, message):
     if not reply_message.media:
         return await message.reply("ʙᴀʟᴀs ᴋᴇ ᴘᴇsᴀɴ ꜰᴏᴛᴏ ᴀᴛᴀᴜ sᴛɪᴄᴋᴇʀ")
     file = await client.download_media(reply_message)
-    proses = await get_vars(client.me.id, "EMOJI_PROSES") or "5215484787325676090"
-    Tm = await message.reply(f"<emoji id={proses}>🔍</emoji> ᴘʀᴏᴄᴇssɪɴɢ ɴɪʜ ʙʀᴇᴇ . . .")
+    proses = await get_vars(client.me.id, "EMOJI_PROSES") or "6248838379551591559"
+    Tm = await message.reply(f"<emoji id={proses}>⏳</emoji> ᴘʀᴏᴄᴇssɪɴɢ ɴɪʜ ʙʀᴇᴇ . . .")
     text = get_arg(message)
     if len(text) < 1:
         return await Tm.edit(f"ʜᴀʀᴀᴘ ᴋᴇᴛɪᴋ {PREFIX[0]}mmf ᴛᴇxᴛ")
