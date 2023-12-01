10b87084e5f5f6b237996790c582b6ed122b58ab
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-01 15:08:14 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -2,9 +2,9 @@ import os
 
 API_ID = int(os.getenv("API_ID", "17896688"))
 API_HASH = os.getenv("API_HASH", "947327cf5ff0053a66bf7951f9db5658")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "6885113809:AAFv0w3Pn2gl6ICX0eNWXnEwmz1n4zWZEMw")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "6679967443:AAF71SynuLFGQFXM3SJLlBlaaYgc18Q4NFw")
 OWNER_ID = int(os.getenv("OWNER_ID", "1948147616"))
-LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1001887551242"))
+LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002092876451"))
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001969856888 -1001571197486").split()))
 MAX_BOT = int(os.getenv("MAX_BOT", "40"))
 RMBG_API = os.getenv("RMBG_API", "b5ZnjZ2nUUpbdEHfcrWdjWbC")
