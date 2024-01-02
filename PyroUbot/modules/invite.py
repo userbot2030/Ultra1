614f37ae21e21ae3cce0893fc1dcab91bb4ae2ab
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-01-03 03:25:46 +0700

    Update invite.py

--- PyroUbot/modules/invite.py
+++ PyroUbot/modules/invite.py
@@ -15,19 +15,19 @@ __HELP__ = """
   """
 
 
-@PY.UBOT("invite")
+@PY.UBOT("invite", sudo=True)
 @PY.TOP_CMD
 async def _(client, message):
     await invite_cmd(client, message)
 
 
-@PY.UBOT("inviteall")
+@PY.UBOT("inviteall", sudo=True)
 @PY.TOP_CMD
 async def _(client, message):
     await inviteall_cmd(client, message)
 
 
-@PY.UBOT("cancel")
+@PY.UBOT("cancel", sudo=True)
 @PY.TOP_CMD
 async def _(client, message):
     await cancel_cmd(client, message)
