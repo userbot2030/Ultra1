fab822f31fe149196e7788faa649c26e55512b64
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-01-03 03:51:15 +0700

    Update youtube.py

--- PyroUbot/modules/youtube.py
+++ PyroUbot/modules/youtube.py
@@ -12,13 +12,13 @@ __HELP__ = """
 """
 
 
-@PY.UBOT("vsong")
+@PY.UBOT("vsong", sudo=True)
 @PY.TOP_CMD
 async def _(client, message):
     await vsong_cmd(client, message)
 
 
-@PY.UBOT("song")
+@PY.UBOT("song", sudo=True)
 @PY.TOP_CMD
 async def _(client, message):
     await song_cmd(client, message)
