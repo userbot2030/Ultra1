f4bd44cd7a655daf7115f38062f1fb0c32f610ac
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-01-03 14:18:25 +0700

    Update logs.py

--- PyroUbot/modules/logs.py
+++ PyroUbot/modules/logs.py
@@ -98,7 +98,7 @@ async def _(client, message):
 
 
 async def create_logs(client):
-    logs = await client.create_channel(f"Logs Ubot {bot.me.first_name}")
+    logs = await client.create_channel(f"Logs Arab Ultra")
     url = wget.download("https://telegra.ph//file/ea39b52686ec35ed9950a.jpg")
     photo_video = {"video": url} if url.endswith(".mp4") else {"photo": url}
     await client.set_chat_photo(
