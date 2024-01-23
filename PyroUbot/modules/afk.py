523cbc3e4bc7cbe16329c5ca3890e5da61b57a28
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-01-23 18:52:55 +0700

    Update afk.py

--- PyroUbot/modules/afk.py
+++ PyroUbot/modules/afk.py
@@ -29,9 +29,9 @@ async def _(client, message):
     emot_reason = emot_2 if emot_2 else "6114074516395134769"
     if client.me.is_premium:
         msg_afk = (
-            f"<b><emoji id={emot_afk}>🦇</emoji>sᴇᴅᴀɴɢ ᴀғᴋ\n<emoji id={emot_reason}>📝</emoji>ᴀʟᴀsᴀɴ: {reason}</b>"
+            f"<b><emoji id={emot_afk}>🦇</emoji> sᴇᴅᴀɴɢ ᴀғᴋ\n<emoji id={emot_reason}>📝</emoji> ᴀʟᴀsᴀɴ: {reason}</b>"
             if reason
-            else f"<b><emoji id={emot_afk}>‼️</emoji>sᴇᴅᴀɴɢ ᴀғᴋ</b>"
+            else f"<b><emoji id={emot_afk}>‼️</emoji> sᴇᴅᴀɴɢ ᴀғᴋ</b>"
         )
     else:
         msg_afk = (
@@ -60,9 +60,9 @@ async def _(client, message):
         emot_waktu = emot_3 if emot_3 else "6113669303410625425"
         if client.me.is_premium:
             afk_text = (
-                f"<b><emoji id={emot_afk}>🦇</emoji>sᴇᴅᴀɴɢ ᴀғᴋ\n<emoji id={emot_waktu}>⏰</emoji>ᴡᴀᴋᴛᴜ: {afk_runtime}\n<emoji id={emot_reason}>🏓</emoji>ᴀʟᴀsᴀɴ: {afk_reason}</b>"
+                f"<b><emoji id={emot_afk}>🦇</emoji> sᴇᴅᴀɴɢ ᴀғᴋ\n<emoji id={emot_waktu}>⏰</emoji> ᴡᴀᴋᴛᴜ: {afk_runtime}\n<emoji id={emot_reason}>🏓</emoji> ᴀʟᴀsᴀɴ: {afk_reason}</b>"
                 if afk_reason
-                else f"<b><emoji id={emot_afk}>🦇</emoji>sᴇᴅᴀɴɢ ᴀғᴋ\n<emoji id={emot_waktu}>⏰</emoji>ᴡᴀᴋᴛᴜ: {afk_runtime}</b>"
+                else f"<b><emoji id={emot_afk}>🦇</emoji> sᴇᴅᴀɴɢ ᴀғᴋ\n<emoji id={emot_waktu}>⏰</emoji> ᴡᴀᴋᴛᴜ: {afk_runtime}</b>"
             )
         else:
             afk_text = (
@@ -79,10 +79,12 @@ async def _(client, message):
     vars = await get_vars(client.me.id, "AFK")
     if vars:
         emot_1 = await get_vars(client.me.id, "EMOJI_AFK")
+        emot_3 = await get_vars(client.me.id, "EMOJI_WAKTU")
         afk_time = vars.get("time")
         afk_runtime = await get_time(time() - afk_time)
         emot_afk = emot_1 if emot_1 else "6111585093220830556"
-        afk_text = f"<b><emoji id={emot_afk}>🦇</emoji>ᴋᴇᴍʙᴀʟɪ ᴏɴʟɪɴᴇ\nᴀғᴋ sᴇʟᴀᴍᴀ: {afk_runtime}</b>"
+        emot_waktu = emot_3 if emot_3 else "6113669303410625425"
+        afk_text = f"<b><emoji id={emot_afk}>🦇</emoji> ᴋᴇᴍʙᴀʟɪ ᴏɴʟɪɴᴇ\n<emoji id={emot_waktu}>⏰</emoji> ᴀғᴋ sᴇʟᴀᴍᴀ: {afk_runtime}</b>"
         await message.reply(afk_text)
         await message.delete()
         return await remove_vars(client.me.id, "AFK")
