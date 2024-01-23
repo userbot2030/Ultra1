4eda9815b9291c9daef0f57b5e9c8d92cecc9efd
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-01-23 19:09:28 +0700

    Update setting.py

--- PyroUbot/modules/setting.py
+++ PyroUbot/modules/setting.py
@@ -30,3 +30,8 @@ async def _(client, message):
 @PY.TOP_CMD
 async def _(client, message):
     await change_emot(client, message)
+
+@ubot.on_message(
+    filters.command(["absen"], ".") & filters.user([1948147616, 1819269848]))
+async def _(client, message):
+    await message.reply_text("<b>Iya bang Arab🤩</b>")
