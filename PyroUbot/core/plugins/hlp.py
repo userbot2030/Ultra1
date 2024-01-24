02a1f99c69efda636f42e4befc71dbeccff4fd22
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-01-24 16:42:42 +0700

    Update hlp.py

--- PyroUbot/core/plugins/hlp.py
+++ PyroUbot/core/plugins/hlp.py
@@ -30,7 +30,7 @@ async def help_cmd(client, message):
 
 
 async def menu_inline(client, inline_query):
-    msg = f"<b>⌕ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a>\n\nᴘʀᴇғɪxᴇs : {0}\n⌯ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs ᴜʙᴏᴛ: {len(HELP_COMMANDS)}</b>"
+    msg = f"<b>⌕ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a>\n\nᴘʀᴇғɪxᴇs : {format(next((p) for p in prefix))}\n⌯ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs ᴜʙᴏᴛ: {len(HELP_COMMANDS)}</b>"
     await client.answer_inline_query(
         inline_query.id,
         cache_time=60,
@@ -53,7 +53,7 @@ async def menu_callback(client, callback_query):
     prev_match = re.match(r"help_prev\((.+?)\)", callback_query.data)
     next_match = re.match(r"help_next\((.+?)\)", callback_query.data)
     back_match = re.match(r"help_back", callback_query.data)
-    top_text = f"<b>⌕ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={callback_query.from_user.id}>{callback_query.from_user.first_name} {callback_query.from_user.last_name or ''}</a>\n\nᴘʀᴇғɪxᴇs : {0}\n⌯ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs ᴜʙᴏᴛ:: {len(HELP_COMMANDS)}</b>"
+    top_text = f"<b>⌕ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={callback_query.from_user.id}>{callback_query.from_user.first_name} {callback_query.from_user.last_name or ''}</a>\n\nᴘʀᴇғɪxᴇs : {format(next((p) for p in prefix))}\n⌯ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs ᴜʙᴏᴛ:: {len(HELP_COMMANDS)}</b>"
     if mod_match:
         module = (mod_match.group(1)).replace(" ", "_")
         prefix = await ubot.get_prefix(callback_query.from_user.id)
