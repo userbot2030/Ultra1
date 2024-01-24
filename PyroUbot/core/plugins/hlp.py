9fcb0c97f3254fa16370c5f0bc54767258cb2634
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-01-24 16:43:54 +0700

    Update hlp.py

--- PyroUbot/core/plugins/hlp.py
+++ PyroUbot/core/plugins/hlp.py
@@ -30,6 +30,7 @@ async def help_cmd(client, message):
 
 
 async def menu_inline(client, inline_query):
+    prefix = await ubot.get_prefix(client.me.id)
     msg = f"<b>⌕ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={inline_query.from_user.id}>{inline_query.from_user.first_name} {inline_query.from_user.last_name or ''}</a>\n\nᴘʀᴇғɪxᴇs : {format(next((p) for p in prefix))}\n⌯ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs ᴜʙᴏᴛ: {len(HELP_COMMANDS)}</b>"
     await client.answer_inline_query(
         inline_query.id,
@@ -53,6 +54,7 @@ async def menu_callback(client, callback_query):
     prev_match = re.match(r"help_prev\((.+?)\)", callback_query.data)
     next_match = re.match(r"help_next\((.+?)\)", callback_query.data)
     back_match = re.match(r"help_back", callback_query.data)
+    prefix = await ubot.get_prefix(client.me.id)
     top_text = f"<b>⌕ ᴍᴇɴᴜ ɪɴʟɪɴᴇ <a href=tg://user?id={callback_query.from_user.id}>{callback_query.from_user.first_name} {callback_query.from_user.last_name or ''}</a>\n\nᴘʀᴇғɪxᴇs : {format(next((p) for p in prefix))}\n⌯ ᴛᴏᴛᴀʟ ᴍᴏᴅᴜʟᴇs ᴜʙᴏᴛ:: {len(HELP_COMMANDS)}</b>"
     if mod_match:
         module = (mod_match.group(1)).replace(" ", "_")
