35f27db7020ce804896527b5421a83896e31b1c0
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-02-24 12:18:45 +0700

    Update gcs.py

--- PyroUbot/core/plugins/gcs.py
+++ PyroUbot/core/plugins/gcs.py
@@ -37,7 +37,7 @@ async def broadcast_group_cmd(client, message):
     gagal = await get_vars(client.me.id, "EMOJI_GAGAL") or "6113891550788324241"
     sukses = await get_vars(client.me.id, "EMOJI_SUKSES") or "6114011655253790197"
     gcast_done = await get_vars(client.me.id, "GCAST_DONE") or "6114074516395134769"
-    await message.reply(f"<b>ᴘᴇꜱᴀɴ ɢɪᴋᴇꜱ ʟᴜ ᴛᴇʟᴀʜ ꜱᴇʟᴇꜱᴀɪ ᴅɪʟᴀᴋᴜᴋᴀɴ <emoji id={gcast_done}>⚠️</emoji>\n<emoji id={sukses}>✅</emoji> ᴘᴇsᴀɴ ᴛᴇʀᴋɪʀɪᴍ ᴋᴇ <code>{sent}</code> ɢʀᴏᴜᴘ\n<emoji id={gagal}>❌</emoji> ɢᴀɢᴀʟ ᴛᴇʀᴋɪʀɪᴍ ᴋᴇ <code>{failed}</code> ɢʀᴏᴜᴘ</b>")
+    await message.reply(f"<b>ɢɪᴋᴇꜱ ʟᴜ ᴛᴇʟᴀʜ ꜱᴇʟᴇꜱᴀɪ <emoji id={gcast_done}>⚠️</emoji>\n\n<emoji id={sukses}>✅</emoji> ᴘᴇsᴀɴ ᴛᴇʀᴋɪʀɪᴍ ᴋᴇ <code>{sent}</code> ɢʀᴏᴜᴘ\n<emoji id={gagal}>❌</emoji> ɢᴀɢᴀʟ ᴛᴇʀᴋɪʀɪᴍ ᴋᴇ <code>{failed}</code> ɢʀᴏᴜᴘ</b>")
 
 
 async def broadcast_users_cmd(client, message):
