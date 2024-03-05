26565a38c084df5b0381d4e8fe785924552c9e09
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-03-05 13:05:57 +0700

    fix

--- PyroUbot/modules/spamG.py
+++ PyroUbot/modules/spamG.py
@@ -67,9 +67,9 @@ async def SpamGcast(client, message, send):
 @PY.UBOT("spamg")
 @PY.TOP_CMD
 async def _(client, message):
-    proses = await get_vars(client.me.id, "EMOJI_PROSES") or "6248838379551591559"
-    gagal = await get_vars(client.me.id, "EMOJI_GAGAL") or "6247033234861853924"
-    sukses = await get_vars(client.me.id, "EMOJI_SUKSES") or "5895231943955451762"
+    proses = await get_vars(client.me.id, "EMOJI_PROSES") or "5960640164114993927"
+    gagal = await get_vars(client.me.id, "EMOJI_GAGAL") or "5438630285635757876"
+    sukses = await get_vars(client.me.id, "EMOJI_SUKSES") or "5787188704434982946"
     gcast_done = await get_vars(client.me.id, "GCAST_DONE") or "6289678459065077018"
     send_done = await get_vars(client.me.id, "SEND_DONE") or "6296577138615125756"
     r = await message.reply(f"<b><emoji id={proses}>⏳</emoji> ꜱᴇᴅᴀɴɢ ᴍᴇᴍᴘʀᴏꜱᴇꜱ....</b>")
