b21e8c0cbad8abd6207548797138afcc35a63902
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-03-05 13:34:04 +0700

    Update tph.py

--- PyroUbot/core/plugins/tph.py
+++ PyroUbot/core/plugins/tph.py
@@ -4,7 +4,7 @@ from PyroUbot import *
 
 
 async def tg_cmd(client, message):
-    proses = await get_vars(client.me.id, "EMOJI_PROSES") or "6248838379551591559"
+    proses = await get_vars(client.me.id, "EMOJI_PROSES") or "5960640164114993927"
     XD = await message.reply(f"<b><emoji id={proses}>⏳</emoji><code>ᴍᴇᴍᴘʀᴏsᴇs . . .</code>")
     if not message.reply_to_message:
         return await XD.edit("<b>ᴍᴏʜᴏɴ ʙᴀʟᴀs ᴋᴇ ᴘᴇsᴀɴ, ᴜɴᴛᴜᴋ ᴍᴇɴᴅᴀᴘᴀᴛᴋᴀɴ ʟɪɴᴋ ᴅᴀʀɪ ᴛᴇʟᴇɢʀᴀᴘʜ.</b>")
@@ -15,7 +15,7 @@ async def tg_cmd(client, message):
             media_url = upload_file(m_d)
         except exceptions.TelegraphException as exc:
             return await XD.edit(f"<code>{exc}</code>")
-        sukses = await get_vars(client.me.id, "EMOJI_SUKSES") or "5895231943955451762"
+        sukses = await get_vars(client.me.id, "EMOJI_SUKSES") or "5787188704434982946"
         U_done = f"<b><emoji id={sukses}>✅</emoji> ʙᴇʀʜᴀsɪʟ ᴅɪᴜᴘʟᴏᴀᴅ ᴋᴇ</b> <a href='https://telegra.ph/{media_url[0]}'>ᴛᴇʟᴇɢʀᴀᴘʜ</a>"
         await XD.edit(U_done)
     elif message.reply_to_message.text:
@@ -26,6 +26,6 @@ async def tg_cmd(client, message):
             response = telegraph.create_page(page_title, html_content=page_text)
         except exceptions.TelegraphException as exc:
             return await XD.edit(f"<code>{exc}</code>")
-        sukses = await get_vars(client.me.id, "EMOJI_SUKSES") or "5895231943955451762"
+        sukses = await get_vars(client.me.id, "EMOJI_SUKSES") or "5787188704434982946"
         wow_graph = f"<b><emoji id={sukses}>✅</emoji> ʙᴇʀʜᴀsɪʟ ᴅɪᴜᴘʟᴏᴀᴅ ᴋᴇ</b> <a href='https://telegra.ph/{response['path']}'>ᴛᴇʟᴇɢʀᴀᴘʜ</a>"
         await XD.edit(wow_graph)
