5e0bb1a2de632df80368c4ba8d78cb41237acdb8
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-03-08 02:22:22 +0700

    Update openAi.py

--- PyroUbot/core/helpers/openAi.py
+++ PyroUbot/core/helpers/openAi.py
@@ -1,5 +1,5 @@
-import asyncio
 
+import asyncio
 import google.generativeai as genai
 import openai
 
@@ -15,7 +15,7 @@ class OpenAi:
         request by: Dhilnihnge.t.me
         """
         try:
-            genai.configure(api_key=AI_GOOGLE_API)
+            genai.configure(api_key)
             model = genai.GenerativeModel(model_name="gemini-1.0-pro")
             convo = model.start_chat(history=[])
             convo.send_message(question)
