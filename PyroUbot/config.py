a08dc4c59caa65c3d8a69f7bc56c2249ba938f20
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-03-08 02:08:11 +0700

    Update config.py

--- PyroUbot/config.py
+++ PyroUbot/config.py
@@ -8,7 +8,7 @@ LOGS_MAKER_UBOT = int(os.getenv("LOGS_MAKER_UBOT", "-1002092876451"))
 BLACKLIST_CHAT = list(map(int, os.getenv("BLACKLIST_CHAT", "-1001969856888 -1001571197486").split()))
 MAX_BOT = int(os.getenv("MAX_BOT", "40"))
 RMBG_API = os.getenv("RMBG_API", "b5ZnjZ2nUUpbdEHfcrWdjWbC")
-OPENAI_KEY = os.getenv("OPENAI_KEY", "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")
+AI_GOOGLE_API = os.getenv("AI_GOOGLE_API", "AIzaSyAM4A7L0Qj3loDZDupt0X74PDne6Tx2YLA")
 MONGO_URL = os.getenv(
     "MONGO_URL",
     "mongodb+srv://MongoFwb:arab123@cluster0.x4azcc8.mongodb.net/?retryWrites=true&w=majority",
