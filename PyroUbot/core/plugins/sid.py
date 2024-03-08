bc67d80b49563c50d082c265973165d391ff1aa3
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-03-08 13:59:08 +0700

    Update sid.py

--- PyroUbot/core/plugins/sid.py
+++ PyroUbot/core/plugins/sid.py
@@ -4,21 +4,21 @@ from PyroUbot import *
 
 
 async def id_cmd(client, message):
-    text = f"<b><emoji id=5319302904308309833>🔸</emoji><a href={message.link}>ᴍᴇssᴀɢᴇ ɪᴅ:</a></b> <code>{message.id}</code>\n"
+    text = f"<b><emoji id=5319302904308309833>◎</emoji><a href={message.link}>ᴍᴇssᴀɢᴇ ɪᴅ:</a></b> <code>{message.id}</code>\n"
 
     if message.chat.type == ChatType.CHANNEL:
-        text += f"<b><emoji id=5319139772860472979>🔹</emoji><a href=https://t.me/{message.chat.username}>ᴄʜᴀᴛ ɪᴅ:</a></b> <code>{message.sender_chat.id}</code>\n"
+        text += f"<b><emoji id=5319139772860472979>◎</emoji><a href=https://t.me/{message.chat.username}>ᴄʜᴀᴛ ɪᴅ:</a></b> <code>{message.sender_chat.id}</code>\n"
     else:
-        text += f"<b><emoji id=5318967574736676420>🔸</emoji><a href=tg://user?id={message.from_user.id}>ʏᴏᴜʀ ɪᴅ:</a></b> <code>{message.from_user.id}</code>\n\n"
+        text += f"<b><emoji id=5318967574736676420>◎</emoji><a href=tg://user?id={message.from_user.id}>ʏᴏᴜʀ ɪᴅ:</a></b> <code>{message.from_user.id}</code>\n\n"
 
         if len(message.command) > 1:
             try:
                 user = await client.get_chat(message.text.split()[1])
-                text += f"<b><emoji id=5321238272406461248>🔸</emoji><a href=tg://user?id={user.id}>ᴜsᴇʀ ɪᴅ:</a></b> <code>{user.id}</code>\n\n"
+                text += f"<b><emoji id=5321238272406461248>◎</emoji><a href=tg://user?id={user.id}>ᴜsᴇʀ ɪᴅ:</a></b> <code>{user.id}</code>\n\n"
             except BaseException:
                 return await message.reply("Pengguna tidak ditemukan.")
 
-        text += f"<b><emoji id=5319139772860472979>🔹</emoji> <a href=https://t.me/{message.chat.username}>ᴄʜᴀᴛ ɪᴅ:</a></b> <code>{message.chat.id}</code>\n\n"
+        text += f"<b><emoji id=5319139772860472979>◎</emoji> <a href=https://t.me/{message.chat.username}>ᴄʜᴀᴛ ɪᴅ:</a></b> <code>{message.chat.id}</code>\n\n"
 
     if message.reply_to_message:
         id_ = (
@@ -28,10 +28,10 @@ async def id_cmd(client, message):
         )
         file_info = get_file_id(message.reply_to_message)
         if file_info:
-            text += f"<b><a href={message.reply_to_message.link}>ᴍᴇᴅɪᴀ ɪᴅ:</a> <code>{file_info.file_id}</code>\n\n"
+            text += f"<b><emoji id=5318967574736676420>◎</emoji><a href={message.reply_to_message.link}>ᴍᴇᴅɪᴀ ɪᴅ:</a> <code>{file_info.file_id}</code>\n\n"
         text += (
-            f"<b><a href={message.reply_to_message.link}>ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:</a></b> <code>{message.reply_to_message.id}</code>\n"
-            f"<b><a href=tg://user?id={id_}>ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:</a></b> <code>{id_}</code>"
+            f"<b><emoji id=5321238272406461248>◎</emoji><a href={message.reply_to_message.link}>ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:</a></b> <code>{message.reply_to_message.id}</code>\n"
+            f"<b><emoji id=5319139772860472979>◎</emoji><a href=tg://user?id={id_}>ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:</a></b> <code>{id_}</code>"
         )
 
     return await message.reply(text, disable_web_page_preview=True)
