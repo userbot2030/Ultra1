38c100385481039f9ede06bb09583f4fd02fa731
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-03-17 17:37:39 +0700

    Update str.py

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -22,13 +22,13 @@ async def ping_cmd(client, message):
     if client.me.is_premium:
         _ping = f"""
 <b><emoji id={ping1}>🏓</emoji> —ᴘɪᴡᴡ!! :</b> <code>{delta_ping} ms</code>
-<b><emoji id={ping2}>⏰</emoji> —ᴇxᴘɪʀᴇᴅ :</b> <code>{expired_date.strftime('%d-%m-%Y')}</code></b>
+<b><emoji id={ping2}>⏰</emoji> —ʜɪᴅᴜᴘ ᴅᴀʀɪ :</b> <code>{uptime}</code></b>
 <b><emoji id={ping3}>👑</emoji> —ɪ'ᴍ <a href=tg://user?id={client.me.id}>{client.me.first_name} {client.me.last_name or ''}</a></b>
 """
     else:
         _ping = f"""
 <b>❏ —ᴘɪᴡᴡ!! :</b> <code>{delta_ping} ms</code></b> 
-<b>├ —ᴇxᴘɪʀᴇᴅ :</b> <code>{expired_date.strftime('%d-%m-%Y')}</code></b>
+<b>├ —ʜɪᴅᴜᴘ ᴅᴀʀɪ :</b> <code>{uptime}</code></b>
 <b>╰ —ɪ'ᴍ <a href=tg://user?id={client.me.id}>{client.me.first_name} {client.me.last_name or ''}</a></b>
 """
     await message.reply(_ping)
