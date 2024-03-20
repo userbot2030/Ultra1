b8c47e42715f4b496daed841d29212b0235f488b
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-03-20 23:39:35 +0700

    Update gcs.py

--- PyroUbot/core/plugins/gcs.py
+++ PyroUbot/core/plugins/gcs.py
@@ -12,6 +12,7 @@ async def broadcast_group_cmd(client, message):
     proses = await get_vars(client.me.id, "EMOJI_PROSES") or "5960640164114993927"
     msg = await message.reply(f"<emoji id={proses}>⏳</emoji> ꜱᴇᴅᴀɴɢ ᴍᴇᴍᴘʀᴏꜱᴇꜱ ɢɪᴋᴇꜱ....")
     async for dialog in client.get_dialogs(limit=None):
+        await client.slowmode(2)
         if dialog.chat.type in (ChatType.GROUP, ChatType.SUPERGROUP):
             if message.reply_to_message:
                 send = message.reply_to_message
