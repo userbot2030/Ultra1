038ba5baab5bf4dec58ff2698b3dfdfe11610492
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-03-21 16:34:04 +0700

    Update staff.py

--- PyroUbot/modules/staff.py
+++ PyroUbot/modules/staff.py
@@ -1,12 +1,5 @@
 from PyroUbot import *
 
-__MODULE__ = "staff"
-__HELP__ = """
-<b>◖ ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ sᴛᴀꜰꜰ ◗</b>
-
-  <b>❑ ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}staff</code>
-  <b>➠ ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇɴɢᴇᴛᴀʜᴜɪ ᴅᴀꜰᴛᴀʀ sᴇᴍᴜᴀ ᴀᴅᴍɪɴ ᴅɪᴅᴀʟᴀᴍ ɢʀᴜᴘ
-"""
 
 
 @PY.UBOT("staff")
