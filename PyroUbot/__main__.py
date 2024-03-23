6d48426acde3d459e75c50e9a7dc7e67735a34f4
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2024-03-23 20:02:41 +0700

    Update __main__.py

--- PyroUbot/__main__.py
+++ PyroUbot/__main__.py
@@ -1,42 +1,27 @@
 import asyncio
 
 from pyrogram import idle
-from pyrogram.errors import RPCError
-from uvloop import install
-
 from PyroUbot import *
 
 
-async def start_ubot(user_id, _ubot):
-    ubot_ = Ubot(**_ubot)
-    try:
-        await asyncio.wait_for(ubot_.start(), timeout=30)
-    except asyncio.TimeoutError:
-        await remove_ubot(user_id)
-        await add_prem(user_id)
-        await rm_all(user_id)
-        await rem_pref(user_id)
-        await rem_uptime(user_id)
-        for X in await get_chat(user_id):
-            await remove_chat(user_id, X)
-        await sending_user(user_id)
-        print(f"[𝗜𝗡𝗙𝗢] - ({user_id}) 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
-    except RPCError:
-        await remove_ubot(user_id)
-        await rm_all(user_id)
-        await rem_pref(user_id)
-        await rem_uptime(user_id)
-        await rem_expired_date(user_id)
-        for X in await get_chat(user_id):
-            await remove_chat(user_id, X)
-        print(f"✅ {user_id} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦")
-
-
 async def main():
-    tasks = [asyncio.create_task(start_ubot(int(_ubot["name"]), _ubot)) for _ubot in await get_userbots()]
-    install()
-    await asyncio.gather(*tasks, bot.start())
+    await bot.start()
+    for _ubot in await get_userbots():
+        ubot_ = Ubot(**_ubot)
+        try:
+            await asyncio.wait_for(ubot_.start(), timeout=10)
+            await ubot_.join_chat("teikoxsupport")
+        except asyncio.TimeoutError:
+            await remove_ubot(int(_ubot["name"]))
+            await rem_expired_date(int(_ubot["name"]))
+            print(f"[𝗜𝗡𝗙𝗢]: {int(_ubot['name'])} 𝗧𝗜𝗗𝗔𝗞 𝗗𝗔𝗣𝗔𝗧 𝗠𝗘𝗥𝗘𝗦𝗣𝗢𝗡")
+        except Exception:
+            await remove_ubot(int(_ubot["name"]))
+            await rem_expired_date(int(_ubot["name"]))
+            print(f"[𝗜𝗡𝗙𝗢]: {int(_ubot['name'])} 𝗕𝗘𝗥𝗛𝗔𝗦𝗜𝗟 𝗗𝗜𝗛𝗔𝗣𝗨𝗦")
+    await bash("rm -rf *session*")
     await asyncio.gather(loadPlugins(), installPeer(), expiredUserbots(), idle())
+
 
 
 if __name__ == "__main__":
