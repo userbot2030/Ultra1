8c1c10bc08260e3ffbeac63134b7cdd338c3c704
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2024-03-23 21:32:34 +0700

    Update str.py

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -32,6 +32,66 @@ async def ping_cmd(client, message):
     await message.reply(_ping)
 
 
+async def stats_ubot(client, message):
+    start = datetime.now()
+    await client.invoke(Ping(ping_id=0))
+    end = datetime.now()
+    delta_ping = (end - start).microseconds / 1000
+    delta_ping_formatted = round(delta_ping, 3)
+    uptime = await get_time((time() - start_time))
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    buttons = [[InlineKeyboardButton("ʀᴇғʀᴇsʜ", callback_data="kontol")]]
+    _ping = f"""
+<b>🖥️ [SYSTEM UBOT]
+PING: {str(delta_ping_formatted).replace('.', ',')} ms
+UBOT: {len(ubot._ubot)} user
+UPTIME: {uptime}
+OWNER:<b/> @Arabnihnge
+
+<b>📊 [STATUS SERVER]
+CPU: {cpu}%
+RAM: {mem}%
+DISK: {disk}%
+MEMORY: {round(process.memory_info()[0] / 1024 ** 2)} MB</b>
+"""
+    await message.reply(_ping, reply_markup=InlineKeyboardMarkup(buttons))
+
+
+async def cb_stats(client, callback_query):
+    await callback_query.answer("ʀᴇғʀᴇsʜɪɴɢ...")
+    start = datetime.now()
+    await client.invoke(Ping(ping_id=0))
+    end = datetime.now()
+    delta_ping = (end - start).microseconds / 1000
+    delta_ping_formatted = round(delta_ping, 3)
+    uptime = await get_time((time() - start_time))
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+    process = psutil.Process(os.getpid())
+    _ping = f"""
+<b>🖥️ [SYSTEM UBOT]
+PING: {str(delta_ping_formatted).replace('.', ',')} ms
+UBOT: {len(ubot._ubot)} user
+UPTIME: {uptime}
+OWNER:</b> @Arabnihnge
+
+<b>📊 [STATUS SERVER]
+CPU: {cpu}%
+RAM: {mem}%
+DISK: {disk}%
+MEMORY: {round(process.memory_info()[0] / 1024 ** 2)} MB</b>
+"""
+    buttons = [[InlineKeyboardButton("ʀᴇғʀᴇsʜ", callback_data="kontol")]]
+    try:
+        await callback_query.message.edit(_ping, reply_markup=InlineKeyboardMarkup(buttons))
+    except:
+        return
+
+
 async def start_cmd(client, message):
     if len(message.command) < 2:
         buttons = Button.start(message)
