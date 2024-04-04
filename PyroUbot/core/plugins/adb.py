6e4d22dd1827dfa4b594d9ae0a63e06b640785b6
ohmyshin <146281749+ohmyshin@users.noreply.github.com>
2024-04-04 12:49:34 +0700

    Update adb.py

--- PyroUbot/core/plugins/adb.py
+++ PyroUbot/core/plugins/adb.py
@@ -353,7 +353,6 @@ async def hapus_ubot(client, callback_query):
             await X.unblock_user(bot.me.username)
             for chat in await get_chat(X.me.id):
                 await remove_chat(X.me.id, chat)
-            await rm_all(X.me.id)
             await rem_pref(X.me.id)
             await remove_ubot(X.me.id)
             await rem_uptime(X.me.id)
