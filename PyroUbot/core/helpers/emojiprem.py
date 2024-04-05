d1ab7828193928ffe2292e43ec01353d1e4946d5
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-04-05 16:58:33 +0700

    Update emojiprem.py

--- PyroUbot/core/helpers/emojiprem.py
+++ PyroUbot/core/helpers/emojiprem.py
@@ -1,6 +1,6 @@
 class EMO:
     async def PING1(client):
-        emot_1 = await get_vars(client.me.id, "PING1")
+        emot_1 = await get_vars(client.me.id, "EMOJI_PING1")
         ping1 = emot_1 if emot_1 else "5219943216781995020"
         if client.me.is_premium:
             _ping1 = f"<emoji id={ping1}>🏓</emoji>"
@@ -10,7 +10,7 @@ class EMO:
 
 
     async def PING2(client):
-        emot_2 = await get_vars(client.me.id, "PING2")
+        emot_2 = await get_vars(client.me.id, "EMOJI_PING2")
         ping2 = emot_2 if emot_2 else "5301096984617166561"
         if client.me.is_premium:
             _ping2 = f"<emoji id={ping2}>🤖</emoji>"
@@ -19,8 +19,8 @@ class EMO:
         return _ping2
 
 
-    async def UBOT(client):
-        emot_3 = await get_vars(client.me.id, "PING3")
+    async def PING3(client):
+        emot_3 = await get_vars(client.me.id, "EMOJI_PING3")
         ping3 = emot_3 if emot_3 else "5289940334619406906"
         if client.me.is_premium:
             _ping3 = f"<emoji id={ping3}>👑</emoji>"
