2e93f771fbab6506fe25b47c1a3df150c9efcbf0
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-04-05 17:19:24 +0700

    Update str.py

--- PyroUbot/core/plugins/str.py
+++ PyroUbot/core/plugins/str.py
@@ -10,7 +10,7 @@ from pyrogram.raw.functions import Ping
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from PyroUbot import *
-from PyroUbot import emojiprem
+from PyroUbot.core.helpers import emojiprem
 
 async def ping_cmd(client, message):
     start = datetime.now()
