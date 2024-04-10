947defe3d3a6a271197863006bb00a15b944cb2d
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-04-11 02:36:12 +0700

    Update __init__.py

--- PyroUbot/core/plugins/__init__.py
+++ PyroUbot/core/plugins/__init__.py
@@ -27,7 +27,6 @@ from PyroUbot.core.plugins.str import *
 from PyroUbot.core.plugins.tll import *
 from PyroUbot.core.plugins.tny import *
 from PyroUbot.core.plugins.tpc import *
-from PyroUbot.core.plugins.tph import *
 from PyroUbot.core.plugins.trl import *
 from PyroUbot.core.plugins.ytp import *
 from PyroUbot.core.plugins.fnt import *
