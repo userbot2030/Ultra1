46eec542a37682af76018fa0b9713ed0ed12e119
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-04-11 02:39:38 +0700

    Update topcmd.py

--- PyroUbot/modules/topcmd.py
+++ PyroUbot/modules/topcmd.py
@@ -2,5 +2,25 @@ from PyroUbot import *
 
 
 @PY.UBOT("topcmd", FILTERS.ME_OWNER)
-async def _(client, message):
-    await get_top_module(client, message)
+async def get_top_module(client, message):
+    vars = await all_vars(bot.me.id, "modules")
+    sorted_vars = sorted(vars.items(), key=lambda item: item[1], reverse=True)
+
+    command_count = 999
+    text = message.text.split()
+
+    if len(text) == 2:
+        try:
+            command_count = min(max(int(text[1]), 1), 10)
+        except Exception:
+            pass
+
+    total_count = sum(count for _, count in sorted_vars[:command_count])
+
+    txt = "<b>📊 ᴛᴏᴘ ᴄᴏᴍᴍᴀɴᴅ</b>\n"
+    for command, count in sorted_vars[:command_count]:
+        txt += f"<b> ├ {command}:</b> <code>{count}</code>\n"
+
+    txt += f"<b> ╰ ᴛᴏᴛᴀʟ ᴄᴏᴍᴍᴀɴᴅ:</b> <code>{total_count}</code>"
+
+    await message.reply(txt, quote=True)
