1e4489dedfa1138e8e5adc6fff7aab84fbae1067
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-04-12 13:50:53 +0700

    Update tagall.py

--- PyroUbot/modules/tagall.py
+++ PyroUbot/modules/tagall.py
@@ -7,7 +7,7 @@ __HELP__ = """
   <b>❑ ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}tagall</code> [ᴛʏᴘᴇ ᴍᴇssᴀɢᴇ/ʀᴇᴘʟʏ ᴍᴇssᴀɢᴇ] 
   <b>➠ ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍᴇɴᴛɪᴏɴ sᴇᴍᴜᴀ ᴀɴɢɢᴏᴛᴀ ɢʀᴜᴘ ᴅᴇɴɢᴀɴ ᴘᴇsᴀɴ ʏᴀɴɢ ᴀɴᴅᴀ ɪɴɢɪɴᴋᴀɴ
 
-  <b>❑ ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}cancel</code>
+  <b>❑ ᴘᴇʀɪɴᴛᴀʜ:</b> <code>{0}stop</code>
   <b>➠ ᴘᴇɴᴊᴇʟᴀsᴀɴ:</b> ᴜɴᴛᴜᴋ ᴍᴇᴍʙᴀᴛᴀʟᴋᴀɴ ᴍᴇᴍᴇɴᴛɪᴏɴ ᴀɴɢɢᴏᴛᴀ ɢʀᴜᴘ
 """
 
@@ -18,7 +18,7 @@ async def _(client, message):
     await tagall_cmd(client, message)
 
 
-@PY.UBOT("cancel")
+@PY.UBOT("stop")
 @PY.TOP_CMD
 async def _(client, message):
     await batal_cmd(client, message)
