d37986633f17d40a9ec60c76eec5d5d55a73309f
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-04-12 13:46:26 +0700

    Update tagall.py

--- PyroUbot/modules/tagall.py
+++ PyroUbot/modules/tagall.py
@@ -1,5 +1,3 @@
-import asyncio
-from random import shuffle
 from PyroUbot import *
 
 __MODULE__ = "tagall"
@@ -14,38 +12,13 @@ __HELP__ = """
 """
 
 
-
-tagallgcid = []
-
-
 @PY.UBOT("tagall")
 @PY.TOP_CMD
-async def tagall_cmd(client, message):
-    if message.chat.id in tagallgcid:
-        return
-    tagallgcid.append(message.chat.id)
-    text = message.text.split(None, 1)[1] if len(message.text.split()) != 1 else ""
-    users = [member.user.mention async for member in message.chat.get_members() if not (member.user.is_bot or member.user.is_deleted)]
-    shuffle(users)
-    m = message.reply_to_message or message
-    for output in [users[i : i + 5] for i in range(0, len(users), 5)]:
-        if message.chat.id not in tagallgcid:
-            break
-        await asyncio.sleep(1.5)
-        await m.reply_text("\n".join([f"{emoji.TELEPHONE} {output[i]}" for i in range(len(output))]) + "\n\n" + text, quote=bool(message.reply_to_message))
-    try:
-        tagallgcid.remove(message.chat.id)
-    except Exception:
-        pass
+async def _(client, message):
+    await tagall_cmd(client, message)
 
 
 @PY.UBOT("cancel")
 @PY.TOP_CMD
-async def batal_cmd(client, message):
-    if message.chat.id not in tagallgcid:
-        return await message.reply_text("sedang tidak ada perintah: <code>tagall</code> yang digunakan")
-    try:
-        tagallgcid.remove(message.chat.id)
-    except Exception:
-        pass
-    await message.reply_text("ok tagall berhasil dibatalkan")
+async def _(client, message):
+    await batal_cmd(client, message)
