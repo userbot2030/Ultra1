9541397541f817c4ea9d3e9856e6b8347354d8d8
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-04-19 20:36:40 +0700

    Update tll.py

--- PyroUbot/core/plugins/tll.py
+++ PyroUbot/core/plugins/tll.py
@@ -1,24 +1,41 @@
 import asyncio
-from random import shuffle
-
-from PyroUbot import *
+from random import choice, shuffle
+from pyrogram import emoji
+from PyroUbot import get_arg 
 
 tagallgcid = []
 
 
 async def tagall_cmd(client, message):
-    if message.chat.id in tagallgcid:
-        return
+    msg = await message.reply("sɪʟᴀʜᴋᴀɴ ᴛᴜɴɢɢᴜ", quote=True)
+
     tagallgcid.append(message.chat.id)
-    text = message.text.split(None, 1)[1] if len(message.text.split()) != 1 else ""
-    users = [member.user.mention async for member in message.chat.get_members() if not (member.user.is_bot or member.user.is_deleted)]
-    shuffle(users)
+    emoji_list = [value for key, value in emoji.__dict__.items() if not key.startswith("__")]
+    user_tagged = [f"<a href=tg://user?id={user.user.id}>{choice(emoji_list)}</a>" async for user in message.chat.get_members() if not (user.user.is_bot or user.user.is_deleted)]
     m = message.reply_to_message or message
-    for output in [users[i : i + 5] for i in range(0, len(users), 5)]:
+    count = []
+    for output in [user_tagged[i : i + 5] for i in range(0, len(user_tagged), 5)]:
         if message.chat.id not in tagallgcid:
             break
-        await asyncio.sleep(1.5)
-        await m.reply_text("\n".join([f"{emoji.TELEPHONE} {output[i]}" for i in range(len(output))]) + "\n\n" + text, quote=bool(message.reply_to_message))
+        try:
+            await m.reply(
+                f"{get_arg(message)}\n\n{' '.join(output)}",
+                quote=bool(message.reply_to_message),
+            )
+            await asyncio.sleep(3)
+            count += output
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            await m.reply(
+                f"{get_arg(message)}\n\n{' '.join(output)}",
+                quote=bool(message.reply_to_message),
+            )
+            await asyncio.sleep(3)
+            count += output
+
+    await msg.delete()
+    await message.reply(f"<b>✅ <code>{len(count)}</code> ᴀɴɢɢᴏᴛᴀ ʙᴇʀʜᴀsɪʟ ᴅɪ ᴛᴀɢ</b>")
+
     try:
         tagallgcid.remove(message.chat.id)
     except Exception:
