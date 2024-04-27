415bb2261412dcbed74913c040f863badbe1929b
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-04-28 06:25:53 +0700

    Update eval.py

--- PyroUbot/modules/eval.py
+++ PyroUbot/modules/eval.py
@@ -1,13 +1,13 @@
 from PyroUbot import *
 
-@PY.BOT("sh", FILTERS.DEVS)
-@PY.UBOT("sh", FILTERS.ME_DEVS)
+@PY.BOT("sh", FILTERS.OWNER)
+@PY.UBOT("sh", FILTERS.ME_OWNER)
 async def _(client, message):
     await shell_cmd(client, message)
 
 
-@PY.BOT("eval", FILTERS.DEVS)
-@PY.UBOT("eval", FILTERS.ME_DEVS)
+@PY.BOT("eval", FILTERS.OWNER)
+@PY.UBOT("eval", FILTERS.ME_OWNER)
 async def _(client, message):
     await evalator_cmd(client, message)
 
