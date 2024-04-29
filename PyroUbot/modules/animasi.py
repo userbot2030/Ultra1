615afb3b6e7b342cb47af7358e1e4c8221d4fd74
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2024-04-29 12:17:11 +0700

    Create animasi.py

---
+++ PyroUbot/modules/animasi.py
@@ -0,0 +1,112 @@
+from PyroUbot import *
+
+__MODULE__ = "animasi"
+__HELP__ = """
+ <b>ʙᴀɴᴛᴜᴀɴ ᴜɴᴛᴜᴋ ᴀɴɪᴍᴀꜱɪ</b>
+
+• <b>ᴘᴇʀɪɴᴛᴀʜ:</b> <ᴄᴏᴅᴇ>{0}kntl ᴏʀ {0}penis</ᴄᴏᴅᴇ> 
+• <b>ᴘᴇɴᴊᴇʟᴀꜱᴀɴ:</b> <i>ᴄᴏʙᴀ ꜱᴇɴᴅɪʀɪ.</i>  
+
+• <b>ᴘᴇʀɪɴᴛᴀʜ:</b> <ᴄᴏᴅᴇ>{0}ajg ᴏʀ {0}babi</ᴄᴏᴅᴇ> 
+• <b>ᴘᴇɴᴊᴇʟᴀꜱᴀɴ:</b> <i>ᴄᴏʙᴀ ꜱᴇɴᴅɪʀɪ.</i>
+
+• <b>ᴘᴇʀɪɴᴛᴀʜ:</b> <ᴄᴏᴅᴇ>{0}ɴᴀʜ ᴏʀ {0}ʏ</ᴄᴏᴅᴇ> 
+• <b>ᴘᴇɴᴊᴇʟᴀꜱᴀɴ:</b> <i>ᴄᴏʙᴀ ꜱᴇɴᴅɪʀɪ.</i>
+
+• <b>ᴘᴇʀɪɴᴛᴀʜ:</b> <ᴄᴏᴅᴇ>{0}ᴀɴɢᴇ ᴏʀ {0}ʟɪᴘᴋᴏʟ</ᴄᴏᴅᴇ> 
+• <b>ᴘᴇɴᴊᴇʟᴀꜱᴀɴ:</b> <i>ᴄᴏʙᴀ ꜱᴇɴᴅɪʀɪ.</i>  
+
+• <b>ᴘᴇʀɪɴᴛᴀʜ:</b> <ᴄᴏᴅᴇ>{0}ɴᴀᴋᴀʟ ᴏʀ {0}ᴘɪꜱꜱ</ᴄᴏᴅᴇ> 
+• <b>ᴘᴇɴᴊᴇʟᴀꜱᴀɴ:</b> <i>ᴄᴏʙᴀ ꜱᴇɴᴅɪʀɪ.</i>  
+
+• <b>ᴘᴇʀɪɴᴛᴀʜ:</b> <ᴄᴏᴅᴇ>{0}ᴋᴏᴄᴏᴋ</ᴄᴏᴅᴇ> 
+• <b>ᴘᴇɴᴊᴇʟᴀꜱᴀɴ:</b> <i>ᴄᴏʙᴀ ꜱᴇɴᴅɪʀɪ.</i>
+"""
+
+"""
+@PY.UBOT("loveyou")
+async def _(client, message):
+    await lopeyo(client, message)
+
+@PY.UBOT("hmm")
+async def _(client, message):
+    await hmmm(client, message)
+
+
+@PY.UBOT("nah")
+async def _(client, message):
+    await nahlove(client, message)
+  
+@PY.UBOT("heli")
+async def _(client, message):
+    await helikopter(client, message)
+
+@PY.UBOT("piss")
+async def _(client, message):
+    await peace(client, message)
+
+@PY.UBOT("tembak")
+async def _(client, message):
+    await dornembak(client, message)
+
+@PY.UBOT("spongebob")
+async def _(client, message):
+    await spongebobss(client, message)
+
+@PY.UBOT("bundir")
+async def _(client, message):
+    await ngebundir(client, message)
+
+@PY.UBOT("awk")
+async def _(client, message):
+    await awikwok(client, message)
+
+@PY.UBOT("tank")
+async def _(client, message):
+    await tank(client, message)
+"""
+
+@PY.UBOT("babi")
+async def _(client, message):
+    await babi(client, message)
+
+@PY.UBOT("ajg")
+async def _(client, message):
+    await anjg(client, message)
+
+@PY.UBOT("y")
+async def _(client, message):
+    await ysaja(client, message)
+
+@PY.UBOT("ange")
+async def _(client, message):
+    await piciieess(client, message)
+
+@PY.UBOT("lipkol")
+async def _(client, message):
+    await lipkoll(client, message)
+
+@PY.UBOT("nakal")
+async def _(client, message):
+    await nakall(client, message)
+
+@PY.UBOT("piss")
+async def _(client, message):
+    await peace(client, message)
+
+
+@PY.UBOT("kocok")
+async def _(client, message):
+    await kocokk(client, message)
+
+@PY.UBOT("nah")
+async def _(client, message):
+    await nahlove(client, message)
+
+@PY.UBOT("kntl|kontol")
+async def _(client, message):
+    await kntl(client, message)
+
+@PY.UBOT("penis|titid")
+async def _(client, message):
+    await pns(client, message)
